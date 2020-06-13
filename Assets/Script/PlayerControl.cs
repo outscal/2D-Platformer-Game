928f73007bbe81ebdc7a2aa3d5219234e5199434
P Joy Kumar <joy.officialdev@gmail.com>
2020-06-13 13:38:53 +0530

    Animations Idle and running implemented with interaction of D button.

---
+++ Assets/Script/PlayerControl.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public GameObject player_Object;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            player_Object.GetComponent<Animator>().SetFloat("speed",0.6f);
+        }
+        else if(Input.GetKeyUp(KeyCode.D))
+        {
+            player_Object.GetComponent<Animator>().SetFloat("speed", 0.3f);
+        }
+    }
+}
