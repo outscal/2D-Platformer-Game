60d059903e2213361926462f7747ad21767c5c80
DESKTOP-64MNA6F\Raj <parmarj123@gmail.com>
2020-07-01 03:10:09 +0530

    navigation feature

---
+++ Assets/Scripts/BackwardScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BackwardScript : MonoBehaviour
+{
+    public void previousScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
---
+++ Assets/Scripts/ForwardScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ForwardScript : MonoBehaviour
+{
+    public void nextScene(string sceneName)
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
