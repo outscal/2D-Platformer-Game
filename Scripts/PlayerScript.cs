fa78a0a8b6b322d698a25b672b74df9a563e635f
rahulkumar1711 <60355624+rahulkumar1711@users.noreply.github.com>
2020-07-02 15:40:56 +0530

    Player commit

---
+++ Scripts/PlayerScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+	private Animator anim;
+	public float speed;
+	private void Awake()
+	{
+		anim = GetComponent<Animator>();
+	}
+	private void Update()
+	{ 
+		anim.SetFloat("Speed", speed);
+	}
+}
