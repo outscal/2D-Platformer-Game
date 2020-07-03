75ba0e51cd3991a944521023dde058dd41fb81b1
prakhya24 <prakhyabolla@gmail.com>
2020-07-03 21:06:48 +0530

    Rgid body was added to player character and a platform was created. Created collision between player and platform

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private void Awake()
+    {
+        Debug.Log("Player COntroller is awake");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("collision: " + collision.gameObject.name);
+    }
+
+}
