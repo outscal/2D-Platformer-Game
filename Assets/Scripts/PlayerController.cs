a88b0e46ede5e4b569fc9269b838172472089684
Viditv68 <viditrawatv68@gmail.com>
2020-07-03 20:35:15 +0530

    Crouch and Jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,21 +6,63 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
-    // Start is called before the first frame update
-    void Start()
+    private Rigidbody2D rigidbody;
+    private BoxCollider2D collider;
+    public float jump;
+
+    private bool isInGround;
+
+
+    private Vector2 startingColliderSize;
+    private Vector2 startingColliderOffset;
+
+    private void Awake()
+    {
+        rigidbody = gameObject.GetComponent<Rigidbody2D>();
+        collider = gameObject.GetComponent<BoxCollider2D>();
+    }
+    private void Start()
     {
         
+        startingColliderSize = collider.size;
+        startingColliderOffset = collider.offset;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float speed = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("Speed",  Mathf.Abs(speed));
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Jump");
+        Move(horizontal,vertical);
+
+        if(Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            animator.SetBool("Crouch", true);
+            
+        }
+        else if(Input.GetKey(KeyCode.LeftControl))
+        {
+            collider.size = new Vector2(1.0f, 1.4f);
+            collider.offset = new Vector2(-0.12f, 0.62f);
+
+        }
+        else
+        {
+            animator.SetBool("Crouch", false);
+            collider.size = startingColliderSize;
+            collider.offset = startingColliderOffset;
+        }
+
+    }
+
+    private void Move(float horizontal,float vertical)
+    {
+        // Moving horizontal
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
 
         Vector2 scale = transform.localScale;
 
-        if(speed < 0)
+        if (horizontal < 0)
         {
             scale.x = -1f * Mathf.Abs(scale.x);
         }
@@ -30,5 +72,49 @@ public class PlayerController : MonoBehaviour
         }
 
         transform.localScale = scale;
+
+
+        // Jump
+        if (vertical > 0 && isInGround)
+        {
+            
+            animator.SetBool("Jump", true);
+            rigidbody.AddForce(new Vector2(0, jump), ForceMode2D.Force);
+            
+        }
+
+           
+        else
+        {
+            animator.SetBool("Jump", false);
+            collider.offset = startingColliderOffset;
+        }
+
+
+        /*if(Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetBool("Jump", true);
+            rigidbody.AddForce(new Vector2(0, jump), ForceMode2D.Force);
+        }
+        if(Input.GetKeyUp(KeyCode.Space))
+        {
+            animator.SetBool("Jump", false);
+        }*/
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)          
+    {
+        if(other.gameObject.CompareTag("Ground"))
+        {
+            isInGround = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if(other.gameObject.CompareTag("Ground"))
+        {
+            isInGround = false;
+        }
     }
 }
