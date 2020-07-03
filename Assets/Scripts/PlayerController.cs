8d2acbcd7e00a361044e4d13f78635c477c5d552
Viditv68 <viditrawatv68@gmail.com>
2020-07-03 13:25:05 +0530

    Assignment 2 - Horizontal Input from user and run animation movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator animator;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Speed",  Mathf.Abs(speed));
+
+        Vector2 scale = transform.localScale;
+
+        if(speed < 0)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        else
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+
+        transform.localScale = scale;
+    }
+}
