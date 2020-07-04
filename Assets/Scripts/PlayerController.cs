f84f397d6036e1124e321f53925ca0cfa1ff9107
aamir <aamir.iqbal040@gmail.com>
2020-07-04 17:25:41 +0530

    New Script And Added Rigid Body
    Created a New Script "PlayerController",
    and added Rigid Bidy to Player.
    Added Platform also as in the material and performed some scripting also Like running on keyboard command to and flip.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator animator;
+
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        Debug.Log("Player controller awake");
+    }
+
+    // Update is called once per frame   
+    private void Update()
+    {
+        float speed = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("Speed", Mathf.Abs(speed));
+
+        Vector3 scale = transform.localScale;
+        if(speed < 0)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        else if (speed > 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+
+        transform.localScale = scale;
+    }
+}
