6099c89add2a1816af3994aa5a17919cf2bb7894
ShubhamJain <shubhamjain631@gmail.com>
2020-07-05 17:50:57 +0530

    Player now responds to horizontal input by the user.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    public Animator animator;
+   
     void Update()
     {
-        
+        float inputHorizontal = Input.GetAxis("Horizontal");
+        //Debug.Log(inputHorizontal);
+        animator.SetFloat("Speed", Mathf.Abs(inputHorizontal));
+        Vector3 scale = transform.localScale;
+        if (inputHorizontal > 0)
+        {
+            animator.SetBool("isRunning", true);
+            transform.localScale = new Vector3(Mathf.Abs(scale.x), scale.y, scale.z);
+        }
+        else if (inputHorizontal < 0)
+        {
+            animator.SetBool("isRunning", true);
+            transform.localScale = new Vector3(-1 * Mathf.Abs(scale.x), scale.y, scale.z);
+        }
+        else {
+            animator.SetBool("isRunning", false);
+        }
     }
 }
