e675d7fb4c4a73c46e17515ee676e3a990c409c4
aamir <aamir.iqbal040@gmail.com>
2020-07-07 06:28:33 +0530

    Movement-Horizontally and Jump vertical

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,58 +6,88 @@ public class PlayerController : MonoBehaviour
 {
     public Animator animator;
     public BoxCollider2D boxcollider;
+    public float speed;
+    public float jump;
+    private Rigidbody2D rb2d;
+    private float size = 1.22f;
+    private float offset = 0.59f;
+    private float size1 = 0.98f;
+    private float offset1 = 2.011f;
 
     // Start is called before the first frame update
     private void Awake()
     {
         Debug.Log("Player controller awake");
-    }
-    private void Start()
-    {
-        boxcollider.GetComponent<BoxCollider2D>();
+        rb2d=gameObject.GetComponent<Rigidbody2D>();
+        boxcollider=gameObject.GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame   
     private void Update()
     {
-        float speed = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("Speed", Mathf.Abs(speed));
-       
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Jump");
+        MoveCharacter(horizontal,vertical);
+        PlayMovementAnimation(horizontal, vertical);
+
+    }
+
+    
+
+    private void MoveCharacter(float horizontal,float vertical)
+    {
+        speed = 4;
+        //Move Character Horizontally
+        Vector3 position = transform.position;
+        position.x += horizontal * speed * Time.deltaTime;
+        transform.position = position;
+
+        //Move Character Vertically
+        if(vertical > 0)
+        {
+            jump = 30;
+            rb2d.AddForce( new Vector2(0f,jump), ForceMode2D.Force);
+        }
+    }
+
+    private void PlayMovementAnimation(float horizontal,float vertical)
+    {
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
+
 
         Vector3 scale = transform.localScale;
 
-        if(speed < 0)
+        if (horizontal < 0)
         {
             scale.x = -1f * Mathf.Abs(scale.x);
         }
-        else if (speed > 0)
+        else if (horizontal > 0)
         {
             scale.x = Mathf.Abs(scale.x);
         }
 
         transform.localScale = scale;
-        if (Input.GetKey(KeyCode.LeftControl)== true)
-        {
-            animator.SetBool("isSit", true);
-            boxcollider.size = new Vector2(boxcollider.size.x,1.22f);
-            boxcollider.offset = new Vector2(boxcollider.offset.x, 0.59f);
-            
-        }
-        else
-        {
-            animator.SetBool("isSit", false);
-            boxcollider.offset = new Vector2(boxcollider.offset.x, 0.98f);
-            boxcollider.size = new Vector2(boxcollider.size.x, 2.011f);
-        }
-        float vertical = Input.GetAxisRaw("Jump");
-        
-        if (vertical > 0) 
+
+        if (vertical > 0)
         {
             animator.SetBool("Jump", true);
         }
         else
         {
             animator.SetBool("Jump", false);
+        }
+        if (Input.GetKey(KeyCode.LeftControl) == true)
+        {
+            animator.SetBool("isSit", true);
+            boxcollider.size = new Vector2(boxcollider.size.x, size);
+            boxcollider.offset = new Vector2(boxcollider.offset.x, offset);
+
+        }
+        else
+        {
+            animator.SetBool("isSit", false);
+            boxcollider.offset = new Vector2(boxcollider.offset.x, size1);
+            boxcollider.size = new Vector2(boxcollider.size.x, offset1);
         }
     }
 }
