ffbc5513cde8338b77f907c005e46f566a21d17e
ShubhamJain <shubhamjain631@gmail.com>
2020-07-10 23:27:16 +0530

    Added tile map

---
+++ Assets/LevelCompleteDetector.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelCompleteDetector : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.GetComponent<PlayerController>() != null)
+        {
+            Debug.Log("level complete");
+        }
+    }
+}
