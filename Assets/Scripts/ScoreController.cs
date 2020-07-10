4b72ab8d62c3c67f357b271b4a5c486a5663a53c
arunteja-dev <aruntejagurrapu@gmail.com>
2020-07-10 22:49:39 +0530

    Added collectibles and increases score when player  picks up collectibles

---
+++ Assets/Scripts/ScoreController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class ScoreController : MonoBehaviour
+{
+    private TextMeshProUGUI scoreText;
+    private int Score;
+
+    private void Awake()
+    {
+        scoreText = GetComponent<TextMeshProUGUI>();
+    }
+    private void Start()
+    {
+        RefreshUI();
+    }
+    public void IncreaseScore(int increment)
+    {
+        Score += increment;
+        RefreshUI();
+    }
+
+    private void RefreshUI()
+    {
+        scoreText.text = "Score: " + Score;
+    }
+}
