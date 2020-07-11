f2636f48c4ff10660639c776fb4de7581ca1b2ef
Sujeet singh <singhsujeet772@gmail.com>
2020-07-11 14:37:42 +0530

    Player Movement Right-left Player Movement along x-axis Player Jump animation Player Jump with Force

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,33 +4,68 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Animator animator;
+     public Animator animator;
+
+    public float speed;
+    public float jump;
+    private Rigidbody2D rigidbody2D;
+
      private void Awake()
      {
          Debug.Log("PlayerController Awake");
+        rigidbody2D = gameObject.GetComponent<Rigidbody2D>();
      }
      private void OnCollisionEnter2D(Collision2D collision)
      {
          Debug.Log("Collision :" + collision.gameObject.name);
      }
      private void Update()
-     {
-         float speed = Input.GetAxisRaw("Horizontal");
-         animator.SetFloat("speed", Mathf.Abs(speed));
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Jump");
 
-         Vector3 scale = transform.localScale;
-         if (speed < 0)
-         {
-             scale.x = -1f * Mathf.Abs(scale.x);
-         }
-         else if (speed > 0)
-         {
-             scale.x = Mathf.Abs(scale.x);
-         }
-         transform.localScale = scale;
+        MoveCharacter(horizontal,vertical);
+        PlayerMovementAnimation(horizontal,vertical);
+    }
+    private void MoveCharacter(float horizontal, float vertical) {
+        //move Character Horizontally
+        Vector3 position = transform.position;
+        position.x += horizontal * speed * Time.deltaTime;
+        transform.position = position;
 
-         //Jump
-         Input.GetAxisRaw("Vertical");
-         Input.GetKeyDown(KeyCode.Space);
-     }
+        //Move Character Vertically
+        if (vertical > 0)
+        {
+            rigidbody2D.AddForce(new Vector2(0f, jump), ForceMode2D.Force);
+        }
+
+    }
+
+    private void PlayerMovementAnimation(float horizontal,float vertical)
+
+    {
+        Debug.Log("jump:" +vertical);
+        animator.SetFloat("speed", Mathf.Abs(horizontal));
+
+        Vector3 scale = transform.localScale;
+        if (horizontal < 0)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        else if (horizontal > 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
+
+        //Jump
+        
+        if(vertical>0){
+            animator.SetBool("jump", true);
+        }
+        else
+        {
+            animator.SetBool("jump", false);
+        }
+    }
 }
