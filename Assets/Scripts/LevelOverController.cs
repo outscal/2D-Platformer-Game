64f74c5600e25cf7ba8bea855bbdd238a49f3ee9
Sujeet singh <singhsujeet772@gmail.com>
2020-07-11 19:43:26 +0530

    Tilemap,Tilemap collidor2d LevelOverController Scripting File

---
+++ Assets/Scripts/LevelOverController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelOverController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>()!=null)
+        {
+            //level is over
+            Debug.Log("Level finished by the player");
+        }
+    }
+}
