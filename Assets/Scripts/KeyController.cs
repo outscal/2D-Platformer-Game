d32b68d19b6319d521faad4a353931a065ad84b0
Sujeet singh <singhsujeet772@gmail.com>
2020-07-12 22:31:38 +0530

    Key Animation and Prefab Text Update Score

---
+++ Assets/Scripts/KeyController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyController : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.gameObject.GetComponent<PlayerController>()!=null)
+        {
+            PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
+            playerController.PickUpkey();
+            Destroy(gameObject);
+        }
+    }
+}
