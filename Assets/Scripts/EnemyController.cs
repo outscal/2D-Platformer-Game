4dbc19fb346fac3d82e4b47d9fc433fa69ebc484
arunteja-dev <aruntejagurrapu@gmail.com>
2020-07-13 10:50:57 +0530

    added Enemy Movement

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,33 +1,37 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
     public float speed;
-
+    private float direction = 1.0f;
 
     private void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Jump");
         EnemyMovement();
     }
-
     private void EnemyMovement()
     {
         Vector2 position = transform.position;
-        position.x += speed * Time.deltaTime;
+        position.x += direction * speed * Time.deltaTime;
         transform.position = position;
-        Debug.Log("Enemy Move");
     }
-
     private void OnCollisionEnter2D(Collision2D collision)
     {
        if(collision.gameObject.GetComponent<PlayerController>() != null)
         {
+            Debug.Log("collided");
             PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
             playerController.killPlayer();
         }
+    }
+    public void ChangeDirection()
+    {
+        direction = -direction;
+        Vector2 scale = transform.localScale;
+        scale.x = -scale.x;
+        transform.localScale = scale;
     }
 }
