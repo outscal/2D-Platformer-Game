23c6020d2a1c77c91b7a1e1048a6e907b06a380c
arunteja-dev <aruntejagurrapu@gmail.com>
2020-07-13 09:15:10 +0530

    added Enemy and player death when both collides and  will reload the scene

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float speed;
+
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Jump");
+        EnemyMovement();
+    }
+
+    private void EnemyMovement()
+    {
+        Vector2 position = transform.position;
+        position.x += speed * Time.deltaTime;
+        transform.position = position;
+        Debug.Log("Enemy Move");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+       if(collision.gameObject.GetComponent<PlayerController>() != null)
+        {
+            PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
+            playerController.killPlayer();
+        }
+    }
+}
