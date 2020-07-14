cfeaffe786e29b20240d97b18d486105b1f07910
Sujeet singh <singhsujeet772@gmail.com>
2020-07-14 20:02:32 +0530

    GameOver Text Button

---
+++ Assets/Scripts/GameOverController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverController : MonoBehaviour
+{
+    public Button buttonRestart;
+    private void Awake()
+    {
+        buttonRestart.onClick.AddListener(ReloadLevel);
+    }
+    public void PlayerDied()
+    {
+        gameObject.SetActive(true);
+    }
+    private void ReloadLevel()
+    {
+        Debug.Log("Reloading the scene");
+        SceneManager.LoadScene(0);
+    }
+}
