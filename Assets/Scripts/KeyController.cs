6f60516d950acab84c8154fab1f68a04c35d1ca9
Sujeet singh <singhsujeet772@gmail.com>
2020-07-14 10:42:47 +0530

    key Animation and Text Score update

--- Assets/Scripts/KeyController.cs
+++ Assets/Scripts/KeyController.cs
@@ -6,7 +6,6 @@ public class KeyController : MonoBehaviour
 {
     private void OnCollisionEnter2D(Collision2D collision)
     {
-
         if (collision.gameObject.GetComponent<PlayerController>()!=null)
         {
             PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
