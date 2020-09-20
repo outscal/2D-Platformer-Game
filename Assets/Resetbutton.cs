3821b394679685eb867d98ee5587fc1e8e77bd1e
Suvamjyoti_Panda <likun54@gmail.com>
2020-09-20 19:35:56 +0530

    unity1-Chalenge

---
+++ Assets/Resetbutton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+
+public class Resetbutton : MonoBehaviour
+{
+    public Button button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button.onClick.AddListener(OnButtonClick);
+    }
+
+    // Update is called once per frame
+    private void OnButtonClick()
+    {
+        SceneManager.LoadScene("MasterScene");
+    }
+}
