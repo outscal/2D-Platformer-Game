9481fb05effbd3832fd68fab80399edf7a6c825e
karishmakrishnan <karishmakrishnan321@gmail.com>
2020-09-23 15:53:25 +0530

    PlayerController

---
+++ Assets/script/PlayerControll.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControll : MonoBehaviour
+{
+    private void Awake(){
+        Debug.Log("Player controller");
+    }
+    private void OnCollisionEnter(Collision collision)
+
+    {
+        
+        Debug.Log("collision:"+collision.gameObject.name);
+        if (collision is null)
+        {
+            throw new System.ArgumentNullException(nameof(collision));
+        }
+       
+    }
+}
