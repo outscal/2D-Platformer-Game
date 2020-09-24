f0222f1cba5f2bd6bb03673eb53382023c6fdff2
karishmakrishnan <karishmakrishnan321@gmail.com>
2020-09-24 21:24:54 +0530

    scripting animation & crouch challenge

--- Assets/script/PlayerControll.cs
+++ Assets/script/PlayerControll.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class PlayerControll : MonoBehaviour
 {
+    float sf=0.25f;
+    Rigidbody2D rigidbody;
+    BoxCollider2D collider2D;
     public Animator animator;
     private void Awake(){
         Debug.Log("Player controller");
@@ -19,16 +22,45 @@ public class PlayerControll : MonoBehaviour
         }
        
     }
-    private void Update(){
-        float speed=Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("speed",Mathf.Abs(speed));
-        Vector3 scale =transform.localScale;
-        if (speed<0){
-            
-            scale.x=-1f*Mathf.Abs(scale.x);
-        }else if(speed>0){
-            scale.x=Mathf.Abs(scale.x);
-        }
-        transform.localScale=scale;
+    private void Start(){
+            rigidbody=GetComponent<Rigidbody2D>();
+            collider2D=GetComponent<BoxCollider2D>();
     }
+    private void Update(){
+      
+     float speed=Input.GetAxisRaw("Horizontal");
+     if(Input.GetKeyDown(KeyCode.LeftControl)){
+         sf=2.0f;
+         }
+         else
+         sf=0.0f;
+     //speed=speed*sf; 
+     animator.SetFloat("speed",Mathf.Abs(speed));
+    //     //speed=9.0f;
+ 
+    //    // animator.SetFloat("jump",Mathf.Abs(jump));
+       
+       Vector3 scale =transform.localScale;
+         if (speed<0){
+            
+             scale.x=-1f*Mathf.Abs(scale.x);
+         }else if(speed>0){
+             scale.x=Mathf.Abs(scale.x);
+         }
+         transform.localScale=scale;
+         float jmp=Input.GetAxisRaw("Vertical");
+        bool v = Input.GetKeyDown(KeyCode.W);
+        if(v==true)
+        animator.SetBool("IsJump",v);
+        else
+        animator.SetBool("IsJump",false);
+        float X=0.55462f;
+        float Y=2.2113f;
+      collider2D.size=new Vector2(X,collider2D.size.x);
+      collider2D.size=new Vector2(Y,collider2D.size.y);
+
+    
+}
+
+  
 }
