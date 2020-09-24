48f2a56ceb32a37f1fd8e97cc65cc48a7a3707de
abhishekjoshigamerz <69464702+abhishekjoshigamerz@users.noreply.github.com>
2020-09-24 11:18:31 +0530

    Chapter 3 - Mini Assignment  2 - Scripting Animation & Flipping

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-
-    void Awake(){
-        
-    }
+    
+    public Animator Animator;
+   
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +17,35 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float speed = Input.GetAxisRaw("Horizontal");
+        Animator.SetFloat("Speed", Mathf.Abs(speed));
+         Vector3 scale = transform.localScale; 
+        if(speed < 0){   
+            scale.x = - 1 * Mathf.Abs(scale.x);
+            Debug.Log(speed);
+        } else if(speed > 0){
+               scale.x = Mathf.Abs(scale.x); 
+               Debug.Log(speed);
+        }
+        transform.localScale = scale;
+
+        if(Input.GetKeyDown(KeyCode.W)){
+            Animator.SetBool("IsJump",true);   
+        }
+       
+
+        if(Input.GetKeyUp(KeyCode.W)){
+           Animator.SetBool("IsJump",false); 
+           
+        }
         
+        // if(Input.GetKey(KeyCode.W)){
+        //     Animator.SetBool("IsJump",true);
+        //     // jumped=true;
+        //     // if(jumped==true){
+        //     //     Animator.SetBool("IsJump",false);
+        //     //     jumped=false;
+        //     // }
+        // }
     }
 }
