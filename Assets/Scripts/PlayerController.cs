9f5a5cd972993ed9b5c248824e69e738c6a354e9
Vikramad98 <gaikwadvikramaditya@gmail.com>
2020-09-24 17:37:46 +0530

    commit 9

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,13 +6,14 @@ public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     public Animator animator;
+    BoxCollider2D Collider;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("collision " + collision.gameObject.name);
     }
     void Start()
     {
-        
+        Collider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -30,13 +31,21 @@ public class PlayerController : MonoBehaviour
         }
         transform.localScale = scale;
 
-        if(Input.GetKey(KeyCode.C))
+        if(Input.GetKey(KeyCode.LeftControl))
         {
             animator.SetBool("isCrouch", true);
+            Collider.offset = new Vector2(-0.013f, 0.64f);
+            Collider.size = new Vector2(0.95f, 1.33f);
+            
         }
-        else if (Input.GetKeyUp(KeyCode.C))
+        else if (Input.GetKeyUp(KeyCode.LeftControl))
         {
             animator.SetBool("isCrouch", false);
+            Collider.offset = new Vector2(0.013f, 0.983f);
+            Collider.size = new Vector3(0.6f, 2.1f);
         }
+        
     }
 }
+
+
