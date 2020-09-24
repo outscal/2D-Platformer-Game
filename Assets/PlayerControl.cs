67bf882ee2b1b26d58294867517bea017f177266
Suvamjyoti_Panda <likun54@gmail.com>
2020-09-24 11:15:48 +0530

    script for playerControll

---
+++ Assets/PlayerControl.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+
+    public Animator animator;
+
+    void Update()
+    {
+        float speed = Input.GetAxisRaw("Horizontal");
+
+
+        animator.SetFloat("Speed", Mathf.Abs(speed));
+
+
+        Vector3 scale = transform.localScale;
+
+        if (speed < 0)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+            
+        }
+        else if (speed >= 0)
+        {
+            scale.x =  Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
+    }
+}
