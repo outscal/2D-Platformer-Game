6e8ebdab2dbd5f859ae9aeea2776bbd149ba8599
Prashant Dombale <prashant.dombale@gmail.com>
2020-09-25 18:04:11 +0530

    Added crouching on user input and reducing collider size while crouching, jump animation with user input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
     public Animator animator;
+    public BoxCollider2D player;
     void Start()
     {
         
@@ -14,9 +15,9 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Running and turning
         float speed = Input.GetAxis("Horizontal");
         //Debug.Log("Horizaontal Speed = " speed);
-
         animator.SetFloat("Speed", Mathf.Abs(speed));
         Vector3 scale = transform.localScale;
         if (speed < 0)
@@ -27,6 +28,36 @@ public class PlayerController : MonoBehaviour
             scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
+
+
+        //Crouching
+        player = player.GetComponent<BoxCollider2D>();
+        if (Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.RightControl))
+        {
+            animator.SetBool("IsCrouch", true);
+            player.size = new Vector2(player.size.x, (player.size.y) * 0.6f);
+            player.offset = new Vector2(player.offset.x, (player.offset.y) *0.6f);
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftControl) || Input.GetKeyUp(KeyCode.RightControl))
+        {
+            animator.SetBool("IsCrouch", false);
+            player.size = new Vector2(player.size.x, (player.size.y) / 0.6f);
+            player.offset = new Vector2(player.offset.x, (player.offset.y) / 0.6f);
+        }
+
+        //Jumping
+        float jump = Input.GetAxis("Vertical");
+        animator.SetFloat("Jump", jump);
+        /*if (jump > 0.3)
+        {
+            player.size = new Vector2(player.size.x, (player.size.y) * 0.6f);
+            player.offset = new Vector2(player.offset.x, (player.offset.y) / 0.6f);
+        }
+
+        else if(jump < 0.3 && jump >0){
+            player.size = new Vector2(player.size.x, (player.size.y) / 0.6f);
+            player.offset = new Vector2(player.offset.x, (player.offset.y) * 0.6f);
+        }*/
     }
-    
+   
 }   
