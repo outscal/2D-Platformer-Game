70693582fbac020d208c3a6bdf15ba3e29ec80f2
Manas Gangwani <manasjg1996@gmail.com>
2020-09-25 14:33:48 +0530

    Added Crouch Animation and Control

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,28 @@ namespace Player
     public class PlayerController : MonoBehaviour
     {
         public Animator animator;
-        
+        public BoxCollider2D StandingCollider;
+        public BoxCollider2D SittingCollider;
+        bool resetCollider = false;
+      
+        private void FixedUpdate()
+        {
+            if (resetCollider)
+            {
+                if (!SittingCollider.enabled)
+                {
+                    SittingCollider.enabled = true;
+                    StandingCollider.enabled = false;
+                }
+                else
+                {
+                    SittingCollider.enabled = false;
+                    StandingCollider.enabled = true;
+                }
+                resetCollider = false;
+            }
+        }
+
         void Update()
         {
             float speed = Input.GetAxisRaw("Horizontal");
@@ -22,6 +43,27 @@ namespace Player
                 scale.x = Mathf.Abs(scale.x);
             }
             transform.localScale = scale;
+            float jump = Input.GetAxisRaw("Vertical");
+            if (jump > 0)
+            {
+                animator.SetBool("Jump",true);
+            }
+            else
+            {
+                animator.SetBool("Jump", false);
+            }
+
+            if (Input.GetKeyDown(KeyCode.LeftControl))
+            {
+                animator.SetBool("Crouch", true);
+                resetCollider = true;
+            }
+
+            if (Input.GetKeyUp(KeyCode.LeftControl))
+            {
+                animator.SetBool("Crouch", false);
+                resetCollider = true;
+            }
         }
     }
 }
