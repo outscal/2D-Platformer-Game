4afb3a156160a949af1bcf6e715478ed160310eb
Vasudev Kulkarni <vdk5384@gmail.com>
2020-09-25 22:38:59 +0530

    changes to master scene

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+   public Animator animator;
+   private void Awake() {
+      Debug.Log("Player Controller awake");
+   }
+   // private void OnCollisionEnter2D(Collision2D collision) {
+   //    Debug.Log("Collision: " + collision.gameObject.name);
+   // }
+
+   private void Update() {
+      float speed = Input.GetAxisRaw("Horizontal");
+     
+   }
+  
+}
+
