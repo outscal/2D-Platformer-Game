e05c1d6f94518ddf158053a7be5607ec497e8ec5
Prashant Dombale <prashant.dombale@gmail.com>
2020-09-27 19:36:30 +0530

    Player no longers moves at one spot, horizontal movements and vertical movemetns during jump added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,38 +5,59 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Animator animator;
-    public BoxCollider2D player;
-    void Start()
+    [SerializeField]
+    private Animator animator;
+    [SerializeField]
+    private BoxCollider2D player;
+    private Rigidbody2D rb2d;
+    private void Awake()
     {
-        
+        player = gameObject.GetComponent<BoxCollider2D>();
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+
     }
+    void Start()
+    {}
 
     // Update is called once per frame
     void Update()
     {
         //Running and turning
+        LeftRight();
+        //Crouching
+        Crouch();
+        //Jumping
+        Jump();
+    }
+
+    void LeftRight() {
         float speed = Input.GetAxis("Horizontal");
-        //Debug.Log("Horizaontal Speed = " speed);
         animator.SetFloat("Speed", Mathf.Abs(speed));
         Vector3 scale = transform.localScale;
         if (speed < 0)
         {
             scale.x = -1f * Mathf.Abs(scale.x);
+            if (speed < -0.5 && !Input.GetKey(KeyCode.LeftControl))
+            {
+                transform.position += Vector3.right * 3 * speed * Time.deltaTime;
+            }
         }
-        else if(speed>0) {
+        else if (speed > 0)
+        {
             scale.x = Mathf.Abs(scale.x);
+            if (speed > 0.5 && !Input.GetKey(KeyCode.LeftControl))
+            {
+                transform.position += Vector3.right * 3 * speed * Time.deltaTime;
+            }
         }
         transform.localScale = scale;
-
-
-        //Crouching
-        player = player.GetComponent<BoxCollider2D>();
+    }
+    void Crouch() {
         if (Input.GetKeyDown(KeyCode.LeftControl) || Input.GetKeyDown(KeyCode.RightControl))
         {
             animator.SetBool("IsCrouch", true);
             player.size = new Vector2(player.size.x, (player.size.y) * 0.6f);
-            player.offset = new Vector2(player.offset.x, (player.offset.y) *0.6f);
+            player.offset = new Vector2(player.offset.x, (player.offset.y) * 0.6f);
         }
         else if (Input.GetKeyUp(KeyCode.LeftControl) || Input.GetKeyUp(KeyCode.RightControl))
         {
@@ -44,20 +65,28 @@ public class PlayerController : MonoBehaviour
             player.size = new Vector2(player.size.x, (player.size.y) / 0.6f);
             player.offset = new Vector2(player.offset.x, (player.offset.y) / 0.6f);
         }
-
-        //Jumping
-        float jump = Input.GetAxis("Vertical");
-        animator.SetFloat("Jump", jump);
-        /*if (jump > 0.3)
-        {
-            player.size = new Vector2(player.size.x, (player.size.y) * 0.6f);
-            player.offset = new Vector2(player.offset.x, (player.offset.y) / 0.6f);
-        }
-
-        else if(jump < 0.3 && jump >0){
-            player.size = new Vector2(player.size.x, (player.size.y) / 0.6f);
-            player.offset = new Vector2(player.offset.x, (player.offset.y) * 0.6f);
-        }*/
     }
-   
+    void Jump() {
+        /*float jump = Input.GetAxis("Jump");
+       if (jump > 0)
+       {
+           animator.SetBool("Jump", true);
+           rb2d.AddForce(new Vector2(0, jump*40), ForceMode2D.Force);
+       }
+       else {
+           animator.SetBool("Jump", false);
+       }*/
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb2d.AddForce(new Vector2(0, 20), ForceMode2D.Impulse);
+        }
+        else if (Input.GetKey(KeyCode.Space))
+        {
+            animator.SetBool("Jump", true);
+        }
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            animator.SetBool("Jump", false);
+        }
+    }
 }   
