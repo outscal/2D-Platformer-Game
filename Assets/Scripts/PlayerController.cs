e0f46c6de9f310151708ca0a9043ef427125d1a5
Vasudev Kulkarni <vdk5384@gmail.com>
2020-09-28 21:43:41 +0530

    rectifying jump animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
    public Animator animator;
+   public float speed;
+
    private void Awake() {
       Debug.Log("Player Controller awake");
    }
@@ -12,23 +14,45 @@ public class PlayerController : MonoBehaviour
    //    Debug.Log("Collision: " + collision.gameObject.name);
    // }
 
-   private void Update() {
-        float speed = Input.GetAxisRaw("Horizontal");
-      animator.SetFloat("Speed", Mathf.Abs(speed));
+   private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Jump");
+
+        MoveCharacter(horizontal);
+        PlayMovementAnimation(horizontal);
+
+    }
+
+    private void MoveCharacter(float horizontal){
+        Vector3 position  = transform.position;
+        position.x +=  horizontal *speed * Time.deltaTime;
+        transform.position = position;
+    }
+
+    private void PlayMovementAnimation(float horizontal, float vertical)
+    {
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
 
         Vector3 scale = transform.localScale;
-        if (speed < 0)
+        if (horizontal < 0)
         {
             scale.x = -1f * Mathf.Abs(scale.x);
         }
-        else if(speed > 0){
+        else if (horizontal > 0)
+        {
             scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
 
         
-   }
-  
+        if(vertical > 0){
+            animator.SetBool("Jump", true);
+        }
+        else{
+            animator.SetBool("Jump", false);
+        }
+    }
 }
 
      
