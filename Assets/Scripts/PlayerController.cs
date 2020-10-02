6814789f4cdffefeb81c8c4b4375537bc781c433
Sahil Shama <shamasahil240@gmail.com>
2020-10-02 15:50:35 +0530

    timing of jump and crouch is slow down

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,16 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using UnityEditorInternal;
 using UnityEngine;
+
+
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
-        
+        Debug.Log("Player Controllor Awake");
     }
-
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        Debug.Log("Collison : " + collision.gameObject.name);
     }
 }
