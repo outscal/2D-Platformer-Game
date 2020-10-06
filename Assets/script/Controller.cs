84f9de41ec2e327d0aead766eef857f040705547
karishmakrishnan <karishmakrishnan321@gmail.com>
2020-10-06 13:48:25 +0530

    added more enemies to the level

---
+++ Assets/script/Controller.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+    private void OnCollisionEnter2D(Collision2D collision){
+        if(collision.gameObject.GetComponent<PlayerControll>()!=null){
+            PlayerControll playerControll=collision.gameObject.GetComponent<PlayerControll>();
+            playerControll.KillPlayer();
+           
+             }
+         }
+  
+}
