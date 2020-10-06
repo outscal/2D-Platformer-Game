ac512d6b2e346335b05fc685cb1241eebac4377c
Sahil Shama <shamasahil240@gmail.com>
2020-10-07 03:42:36 +0530

    scripting run on horizontal input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,15 @@
-﻿using UnityEditorInternal;
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditorInternal;
 using UnityEngine;
+
 
 
 
 public class PlayerController : MonoBehaviour
 {
+    public Animator animator;
     private void Awake()
     {
         Debug.Log("Player Controllor Awake");
@@ -13,4 +18,20 @@ public class PlayerController : MonoBehaviour
     {
         Debug.Log("Collison : " + collision.gameObject.name);
     }
+
+    private void Update()
+    {
+        float speed = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("speed", Mathf.Abs(speed));
+        Vector3 scale = transform.localScale;
+        if (speed < 0)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        else if (speed > 0 )
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
+    } 
 }
