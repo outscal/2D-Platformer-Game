5e66d875ce83dae2cfcd362898f1083ab98d8ef5
karishmakrishnan <karishmakrishnan321@gmail.com>
2020-10-12 14:46:58 +0530

    added level locker to the level and unlock when previous level is complted

--- Assets/script/LevelController.cs
+++ Assets/script/LevelController.cs
@@ -1,19 +1,22 @@
 ﻿using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections;
 public class LevelController : MonoBehaviour
  {
-     public GameOverController game;
+     //public GameOverController game;
      private int nextSceneLoad;
      private void Awake(){
          nextSceneLoad=SceneManager.GetActiveScene().buildIndex+1;
      }
      
 private    void OnTriggerEnter2D(Collider2D collider){
-           
-            SceneManager.LoadScene(nextSceneLoad);
+           StartCoroutine(WaitSec());
+            
+     }
+     IEnumerator WaitSec(){
+         yield return new WaitForSeconds(1);
+         SceneManager.LoadScene(nextSceneLoad);
      }
     
-    // void MySceneLoader(int sceneLoader){
-    //     SceneManager.LoadScene(sceneLoader);
-    // }
-}
+   }
+
