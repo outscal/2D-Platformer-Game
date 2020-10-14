8bd0f317563510e40be6c08ded12dcab5e56cedc
Vasudev Kulkarni <vdk5384@gmail.com>
2020-10-14 12:39:59 +0530

    Adding ground check for jump animation to fix infinite jump

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>() != null)
+        {
+            PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
+            playerController.KillPlayer();
+        }
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,25 +1,50 @@
-﻿using System;
+﻿ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour
 {
-   public Animator animator;
-   public float speed;
-   public float jump;
+    public ScoreController scoreController;
+    public GameOverController gameOverController;
+    public Rigidbody2D rgbd2D;
+    public Animator animator;
+    public float speed;
+    public float jump;
+    private bool canJump;
+    public bool isGrounded;
+
+//    public float jumpSpeed  = 10f;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask groundLayer;
+    
+    
+
+    public void KillPlayer()
+    {
+        Debug.Log("Player Killed by enemy");
+        // Destroy(gameObject);
+        gameOverController.PlayerDied();
+        this.enabled = false;
+    }
+
    
 
-   private Rigidbody2D  rgbd2D;
-
-   private void Awake() {
+    private void Awake() {
       Debug.Log("Player Controller awake");
       rgbd2D = gameObject.GetComponent<Rigidbody2D>();
+   }
+
+   void Start() {
+        rgbd2D = GetComponent<Rigidbody2D>();
    }
 
     public void PickUpKey()
     {
         Debug.Log("Player picked up the key");
+        scoreController.IncreaseScore(10);
     }
 
     // private void OnCollisionEnter2D(Collision2D collision) {
@@ -33,7 +58,8 @@ public class PlayerController : MonoBehaviour
 
         MoveCharacter(horizontal, vertical);
         PlayMovementAnimation(horizontal, vertical);
-
+        checkSurroundings();
+        can_Jump();
     }
 
     private void MoveCharacter(float horizontal, float vertical){
@@ -41,10 +67,26 @@ public class PlayerController : MonoBehaviour
         Vector3 position  = transform.position;
         position.x +=  horizontal *speed * Time.deltaTime;
         transform.position = position;
+    }
 
+    void Jump(){
         //Move Vertically
-        if(vertical > 0){
-            rgbd2D.AddForce(new Vector2(0f, jump), ForceMode2D.Force);
+        if (canJump)
+        {
+            rgbd2D.AddForce(new Vector2(0f, jump), ForceMode2D.Impulse);
+        }
+    }
+
+    private void checkSurroundings(){
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+    }
+
+    private void can_Jump(){
+        if(isGrounded && rgbd2D.velocity.y <= 0){
+            canJump = true;
+        }
+        else{
+            canJump = false;
         }
     }
 
@@ -63,13 +105,17 @@ public class PlayerController : MonoBehaviour
         }
         transform.localScale = scale;
 
-        //Jump
+        // //Jump
         if(vertical > 0){
             animator.SetBool("Jump", true);
         }
         else{
             animator.SetBool("Jump", false);
         }
+
+        // if(Input.GetButtonDown("Space")){
+        //     Jump();
+        // }
 
         //crouch
         if(Input.GetKey(KeyCode.LeftControl)){
@@ -78,6 +124,10 @@ public class PlayerController : MonoBehaviour
         else{
             animator.SetBool("isCrouch", false);
         }
+    }
+
+    private void OnDrawGizmos() {
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
     }
 }
 
