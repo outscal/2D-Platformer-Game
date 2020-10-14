b2063013120921ffc7242eb285caf9eb94b10f1d
Sahil Shama <shamasahil240@gmail.com>
2020-10-15 02:08:44 +0530

    Level completion object

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+       if(collision.gameObject.GetComponent<PlayerController>() != null)
+        {
+            Debug.Log("Level is completed");
+        }
+    }
+}
