aa69fb5631517ad5cf8b337363827d210c7e6ea9
aydev <aayush.kumar@famepilot.com>
2020-12-26 02:15:11 +0530

    Feature 4 - Tile Map Setup

---
+++ Assets/Scripts/LevelOverController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelOverController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if (collision.gameObject.GetComponent<PlayerController>() != null){
+            Debug.Log("Level is completed");
+            SceneManager.LoadScene("NextScene");
+        }
+    }
+}
