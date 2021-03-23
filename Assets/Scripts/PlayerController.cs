f1f7a29ce70f7614bc236eb6a3c34ca07c17ba3a
Viral00 <80312069+Viral00@users.noreply.github.com>
2021-03-24 03:31:43 +0530

    Horizontal inputs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,9 +4,31 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    private void OnCollisionEnter20(Collision2D collision)
+    public Animator animator;
+    /*private void OnCollisionEnter20(Collision2D collision)
     {
         Debug.Log("collision" + collision.gameObject.name);
     }
-    
+    */
+
+    void Update()
+    {
+        float speed = Input.GetAxisRaw("Horizontal");
+        bool crouch = Input.GetKey(KeyCode.LeftControl);
+
+        animator.SetFloat("Speed", Mathf.Abs(speed));
+        animator.SetBool("crouch", crouch);
+
+        Vector3 scale = transform.localScale;
+
+        if (speed < 0)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        else if (speed > 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
+    }
 }
