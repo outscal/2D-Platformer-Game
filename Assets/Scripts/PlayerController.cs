0ea20026a47dcb576ad54a8406bbb3cb1bac260c
Viral00 <80312069+Viral00@users.noreply.github.com>
2021-03-23 20:49:50 +0530

    Collision Detection

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private void OnCollisionEnter20(Collision2D collision)
+    {
+        Debug.Log("collision" + collision.gameObject.name);
+    }
+    
+}
