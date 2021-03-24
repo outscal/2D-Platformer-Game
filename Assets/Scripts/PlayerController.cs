34d2e221d80efeda28dd10f5df9a38c31149c6fe
Viral00 <80312069+Viral00@users.noreply.github.com>
2021-03-24 18:59:29 +0530

    Jump and crouch Done.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,22 +5,27 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
+    public BoxCollider2D boxcollider;
+    
 
     void Update()
     {
-        float speed = Input.GetAxisRaw("Horizontal");
-        bool crouch = Input.GetKey(KeyCode.LeftControl);
 
-        animator.SetFloat("Speed", Mathf.Abs(speed));
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        bool crouch = Input.GetKey(KeyCode.LeftControl);
+        bool jump = Input.GetKey(KeyCode.Space);
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
         animator.SetBool("crouch", crouch);
+        animator.SetBool("jump", jump);
 
         Vector3 scale = transform.localScale;
 
-        if (speed < 0)
+        if (horizontal < 0)
         {
             scale.x = -1f * Mathf.Abs(scale.x);
         }
-        else if (speed > 0)
+        else if (horizontal > 0)
         {
             scale.x = Mathf.Abs(scale.x);
         }
