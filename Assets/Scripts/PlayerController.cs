502cbe34857b269ceb8e4e0a8580de46c42875a1
roshan98 <roshansingh1912@gmail.com>
2021-03-24 18:13:49 +0530

    Added jump and crouch on button presses

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,48 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    //Cached references
+    BoxCollider2D boxCollider;
+    private void Start()
+    {
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
     public Animator animator;
     private void OnCollisionEnter2D(Collision2D collision)
     {
+
         Debug.Log("Collision :" + collision.gameObject.name);   
     }
     private void Update()
     {
         float speed = Input.GetAxisRaw("Horizontal");
+        float vSpeed = Input.GetAxisRaw("Vertical");
+        bool jump = false;
         animator.SetFloat("Speed", Mathf.Abs(speed));
+        animator.SetBool("Jump", jump);
         Vector3 scale = transform.localScale;
-        if (speed<0)
+        if (vSpeed > 0)
+        {
+            animator.SetBool("Jump", true);
+        }
+        else if (vSpeed <= 0)
+        {
+            animator.SetBool("Jump", false);
+        }
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            animator.SetBool("isCrouch", true);
+            boxCollider.offset =  new Vector2(0.05f,0.77f);
+            boxCollider.size =  new Vector2(0.95f, 1.6f);
+        }
+
+        else
+        {
+            animator.SetBool("isCrouch", false);
+            boxCollider.offset = new Vector2(0.024f,1.01f);
+            boxCollider.size = new Vector2(0.62f,2.07f);
+        }
+        if (speed < 0)
         {
             scale.x = -1f * Mathf.Abs(scale.x);
         }
