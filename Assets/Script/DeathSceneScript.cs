e5067f9fee72d2cf26374beb51ed691b46804e20
pawarrahul1002 <pawar.rahul1002@gmail.com>
2021-06-15 22:25:11 +0530

    temp changes

---
+++ Assets/Script/DeathSceneScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathSceneScript : MonoBehaviour
+{
+    public string deathScene;
+    private void OnTriggerEnter2D(Collider2D collison)
+    {
+        // if (collision.gameobject.CompareTag("Player"))
+        if (collison.gameObject.GetComponent<PlayerController>() != null)
+        {
+            Debug.Log("Player is Dead.. ");
+            SceneManager.LoadScene(deathScene);
+        }
+    }
+
+
+}
