58166b02e6589a576ef428dd43f640914f6b5c57
DESKTOP-F4UBJGL\ABC <shubhamyadav74242@gmail.com>
2021-07-19 20:59:19 +0530

    Commit after adding PlayerController Script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Animator animator;
+    void Start()
+    {
+        Debug.Log("Script Attached");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs(speed));
+
+        Vector3 scale = transform.localScale;
+        if(speed < 0)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }else if(speed > 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
+    }
+}
