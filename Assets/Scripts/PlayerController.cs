c00435d7fcc8a427b432375b881f06a3ce38b26f
Honey Jain <hrjain911@gmail.com>
2021-07-19 22:39:07 +0530

    Added a new script player controller and rigid body component to the player character.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Game Start");
+        //Debug.Log("Game Start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
