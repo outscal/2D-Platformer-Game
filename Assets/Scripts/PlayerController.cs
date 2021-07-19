a797351881e2ff0f5675fd5b59d8133f6a8a4dfa
LAPTOP-9EDMHPG6\ashwi <ashwinrjprht@gmail.com>
2021-07-19 21:30:33 +0530

    Feature2-PlayerController Script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator animator;
+    private void Awake()
+    {
+        Debug.Log("Player Controller awake");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision: " + collision.gameObject.name);
+    }
+
+    private void Update()
+    {
+        float Speed = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs(Speed));
+
+        Vector3 scale = transform.localScale;
+        if (Speed < 0)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        } else if (Speed > 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
+    }
+    }
+
