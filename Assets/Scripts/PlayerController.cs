0d1c8d2caa6b77f020f7ba8e7a53edb9d9515a2f
Rahul Yadav <devmethyl@outlook.com>
2021-07-19 21:46:19 +0530

    Added PlayerController script, Added Walk/Run functionality, added Flip based on player direction

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public Animator Animator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = Input.GetAxis("Horizontal");
+        Debug.Log(speed);
+        Animator.SetFloat("Speed", Mathf.Abs(speed));
+
+        Vector3 scale = transform.localScale;
+        if (speed < 0)
+        {
+            
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+
+        else if (speed > 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+
+        transform.localScale = scale;
+    }
+}
