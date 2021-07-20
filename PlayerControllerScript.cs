1d16d098269b9d8963ead18d8bd63f707f2fca86
sachinlele7 <sachinlele12@gmail.com>
2021-07-20 20:00:34 +0530

    Flipping Code

--- PlayerControllerScript.cs
+++ PlayerControllerScript.cs
@@ -4,11 +4,26 @@ using UnityEngine;
 
 public class PlayerControllerScript : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    public Animator animator;
+    //private void OnCollisionEnter2D(Collision2D collision)
+    // {
+    //     Debug.Log("Collision; " + collision.gameObject.name);
+    // }
+    private void Update()
     {
-        Debug.Log("Collision; " + collision.gameObject.name);
+        float speed = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs(speed));
+        
+        Vector3 scale = transform.localScale;
+        if (speed < 0)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+       
+        else if (speed > 0)
+        { scale.x = Mathf.Abs(scale.x); }
+        transform.localScale = scale;
     }
-
 
 
 
