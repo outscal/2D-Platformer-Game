296d45f8ba786336fe4333109a8ade1a9f7abac9
_Justrishi <rishimud@gmail.com>
2021-07-20 20:08:47 +0530

    Jump and Crouch Animation
    Jump and Crouch Animation Added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,50 +6,63 @@ public class PlayerController : MonoBehaviour
 {
     // Update is called once per frame
 
-    //this enables us to add GameObjext/Prefab to the Script
+    //this enables us to add GameObject/Prefab to the Script
     public Animator animator;
+
+    
+
     private void Update()
     {
-        //Input.GetAxisRaw fetches whatever is inside the brackets, Horizontal in this case
-        //Horizontal comprises of Right and left arrow, A and D, Right and left Joystick
+
         float speed = Input.GetAxisRaw("Horizontal");
+        PlayCrouchAnimation();
+        PlayJumpAnimation();
+        PlayHorizontalAnimation(speed);
+    }
 
-        //tried crouch on my own
-        bool crouch = Input.GetKeyUp(KeyCode.LeftControl);
-
-        //Since, running can be varied, the Speed is a float value
-        //Mathf.Abs(speed) fetches the absolute value
-        animator.SetFloat("Speed", Mathf.Abs(speed));
-
-        //Vector 3 represents the 3 axes, scale is the size
-        // -1 is used for sign change, irrespective of the value of scale
-        // if speed is less, ie, the left, A or left joystick is pressed, the scale value will be reversed
-        // Hence the animation will flip, and stay there.
-
-        Vector3 scale = transform.localScale;
-        if (speed < 0)
-        {
-            scale.x = -1f * Mathf.Abs(scale.x);
-            transform.Translate(Vector3.left * 5 * Time.deltaTime, Space.World);
-        }
-        else if (speed > 0)
-        {
-            scale.x = Mathf.Abs(scale.x);
-            transform.Translate(Vector3.right * 5 * Time.deltaTime, Space.World);
-        }
-
+    private void PlayCrouchAnimation()
+    {
+        bool crouch = Input.GetKey(KeyCode.LeftControl);
         if (crouch == true)
         {
             animator.SetBool("Crouch", true);
+        }
+        else
+        {
+            animator.SetBool("Crouch", false);
+        }
+    }
+
+    private void PlayJumpAnimation()
+    {
+        bool jump = Input.GetKey(KeyCode.Space);
+        if (jump == true)
+        {
+            animator.SetBool("Jump", true);
             //transform.Translate(Vector3.up * 3 * Time.deltaTime, Space.World);
         }
-
         else
         {
             animator.SetBool("Jump", false);
         }
+    }
 
-
+    private void PlayHorizontalAnimation(float speed)
+    {
+        animator.SetFloat("Speed", Mathf.Abs(speed));
+        Vector3 scale = transform.localScale;
+        if (speed < 0)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        else if (speed > 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
         transform.localScale = scale;
     }
+        
+       
+
+    
 }
