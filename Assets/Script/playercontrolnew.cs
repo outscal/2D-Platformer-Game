f7622d8ac7902316e1ed95a8b0e18f3f773e4ee8
Shikhar Garg <shikherbest5@gmail.com>
2021-07-20 11:12:31 +0530

    playerinputrun
    player is running left and right

---
+++ Assets/Script/playercontrolnew.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playercontrolnew : MonoBehaviour
+{
+    public Animator animator;
+
+    private void Update()
+    {
+        float speed = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("speed", Mathf.Abs(speed));
+        Vector3 scale = transform.localScale;
+
+        if (speed < 0)
+        {
+            scale.x = -1 * Mathf.Abs(speed);
+        }
+        else if (speed > 0)
+        {
+            scale.x = Mathf.Abs(speed);
+        }
+        transform.localScale = scale;
+    }
+
+}
