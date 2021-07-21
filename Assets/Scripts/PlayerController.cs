81d16680399025f9bd5fa598fb73defbf7b38828
Honey Jain <hrjain911@gmail.com>
2021-07-21 13:04:21 +0530

    Added play jump and crouch animation. Edited size of crouch collider in animator using record button.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,19 @@
 ﻿using System.Diagnostics;
 using System.Collections;
 using UnityEngine;
+using System;
 
 public class PlayerController : MonoBehaviour
-{
+    {
 
     public Animator animator;
+  
+
     // Start is called before the first frame update
+    private void Awake()
+    {
+       
+    }
     void Start()
     {
         print("Game Start");
@@ -16,26 +23,62 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-    
-          float run = Input.GetAxisRaw("Horizontal"); // -1 to 1 
-          animator.SetFloat("speed",Mathf.Abs(run));
 
-          Vector3 scale = transform.localScale;
-          // scale = x=2.0f, y=2.0f,z=1.0f
-          // scale =(2.0f,2.0f,1.0f);
-          if (run < 0)
-          {
-              scale.x = -1f * Mathf.Abs(scale.x); 
-              //scale=(scale.x,2.0f,1.0f);
-              transform.localScale = scale; //transform.localScale=(scale.x,2.0f,1.0f);
-          }
+        float run = Input.GetAxisRaw("Horizontal"); // -1 to 1 0
+        animator.SetFloat("speed", Mathf.Abs(run));
 
-          else if (run > 0)
+        float vertical = Input.GetAxisRaw("Jump");
+        //crouch
+        bool crouchVertical = Input.GetKey(KeyCode.LeftControl);
+
+        MovementAnimation(run, vertical, crouchVertical);
+       
+    }
+
+    private void MovementAnimation(float run, float vertical, bool crouchVertical)
+    {
+
+        //Run Animation
+        Vector3 scale = transform.localScale;
+        // scale = x=2.0f, y=2.0f,z=1.0f
+        // scale =(2.0f,2.0f,1.0f);
+        if (run < 0)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+            //scale=(scale.x,2.0f,1.0f);
+            transform.localScale = scale; //transform.localScale=(scale.x,2.0f,1.0f);
+        }
+
+        else if (run > 0)
         {
             scale.x = Mathf.Abs(scale.x);
             transform.localScale = scale; //transform.localScale=(scale.x,2.0f,1.0f);
         }
 
+        //Jump Animation
+        if (vertical > 0)
+        {
+            animator.SetBool("jump", true);
+        }
+        else
+        {
+            animator.SetBool("jump", false);
+        }
 
+        //crouch Animation
+
+        if (crouchVertical == true)
+        {
+            animator.SetBool("crouch", true);
+
+        }
+        else
+        {
+            animator.SetBool("crouch",false);
+        }
+
+        
     }
+
+    
 }
