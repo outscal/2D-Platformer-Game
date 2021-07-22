a0cbbb7f3777a0d018fdb5132904001cbfeaa4a2
Honey Jain <hrjain911@gmail.com>
2021-07-22 17:49:21 +0530

    Added Death After falling from the platform and going outside the screen downwards.

---
+++ Assets/Scripts/DeathPointScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class DeathPointScript : MonoBehaviour
+{
+    public GameObject DeathPanel;
+    public Button btnRestartGame;
+    // Start is called before the first frame update
+    void Start()
+    {
+        btnRestartGame.onClick.AddListener(ReloadScene);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Debug.Log("Player is dead");
+            DeathPanel.SetActive(true);
+            Destroy(collision.gameObject);
+        }
+    }
+
+    void ReloadScene()
+    {
+        DeathPanel.SetActive(false);
+        SceneManager.LoadScene(0);
+    }
+}
