afa3eefbbbf461171f3ccee2c24f5e26d87af050
Honey Jain <hrjain911@gmail.com>
2021-07-22 17:27:03 +0530

    Added Next Level >>Level2

---
+++ Assets/Scripts/LevelOverController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelOverController : MonoBehaviour
+
+{
+
+    public Button btnNextLevel;
+
+    public GameObject levelCompletePanel;
+
+    public string sceneName;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        collision.gameObject.GetComponent<PlayerController>();
+        levelCompletePanel.SetActive(true);
+        Debug.Log("Level Complete");
+
+        btnNextLevel.onClick.AddListener(ButtonClick);
+
+        void ButtonClick()
+        {
+            SceneManager.LoadScene(sceneName);
+
+        }
+    }
+}
