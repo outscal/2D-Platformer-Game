186fb112e16030c68de15a3955716cf4c8db8703
_Justrishi <rishimud@gmail.com>
2021-07-23 23:40:34 +0530

    Background updated
    need to update the flickering of the tilemaps

--- Assets/Scripts/LevelFailing.cs
+++ Assets/Scripts/LevelFailing.cs
@@ -4,14 +4,11 @@ using UnityEngine;
 
 public class LevelFailing : MonoBehaviour
 {
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-      
-
-        if (collision.gameObject.GetComponent<PlayerController>() != null)
-        {
-            Debug.Log("Level Failed");
-            
-        }
-    }
+ private void OnTriggerEnter2D(Collider2D collision)
+ {
+  if (collision.gameObject.GetComponent<PlayerController>() != null)
+   {
+    Debug.Log("Level Failed");
+   }
+ }
 }
