d30fd2928ccf27f81624c58aa11b4fa85fe9f0bc
Rahul Yadav <devmethyl@outlook.com>
2021-08-06 20:56:28 +0530

    Added LevelManagerScript

--- Assets/Scripts/Levels/LevelLoader.cs
+++ Assets/Scripts/Levels/LevelLoader.cs
@@ -20,6 +20,22 @@ namespace Assets.Scripts.Levels
 
         private void onClick()
         {
+            LevelStatus levelStatus = LevelManager.Instance.GetLevelStatus(LevelName);
+            switch(levelStatus)
+            {
+                case LevelStatus.Locked:
+                    Debug.Log("can't Play this level, till you unlock it");
+                    break;
+
+                case LevelStatus.Unlocked:
+                    Debug.Log(LevelName);
+                    SceneManager.LoadScene(LevelName);
+                    break;
+
+                case LevelStatus.Completed:
+                    SceneManager.LoadScene(LevelName);
+                    break;
+            }
             SceneManager.LoadScene(LevelName);
             //throw new NotImplementedException();
         }
---
+++ Assets/Scripts/Levels/LevelManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Levels
+{
+    public class LevelManager : MonoBehaviour
+    {
+        private static LevelManager instance;
+        public static LevelManager Instance { get { return instance;  } }
+
+
+        private void Awake()
+        {
+            if(instance == null)
+            {
+                instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+
+            else
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        public LevelStatus GetLevelStatus(string level)
+        {
+            LevelStatus levelStatus =  (LevelStatus)PlayerPrefs.GetInt(level, 0);
+            return levelStatus;
+        }
+
+        public void SetLevelStatus(string level, LevelStatus levelStatus)
+        {
+            PlayerPrefs.SetInt(level, (int)levelStatus);
+        }
+    }
+}
