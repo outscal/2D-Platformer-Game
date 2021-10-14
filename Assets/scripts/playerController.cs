9d58dae33a2f0b1fd9f5f59e5c0061286d64e5d1
nannapuraju Karthikeya <karthik.suraj2619@gmail.com>
2021-10-14 21:14:48 +0530

    crouch added

--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
+    [SerializeField] SpriteRenderer sr;
+    [SerializeField] Rigidbody2D rb;
+    [SerializeField] Animator anmi;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,20 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        anmi.SetFloat("run",0);
+        transform.position += new Vector3(Input.GetAxis("Horizontal"),  Input.GetAxis("Vertical"),0) *5*Time.deltaTime;
+        if(Input.GetAxis("Horizontal")>0)
+        {
+            sr.flipX = false;
+            anmi.SetFloat("run" ,0.6f);
+        }
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            sr.flipX = true;
+            anmi.SetFloat("run", 0.6f);
+        }
+        if()
+
+
     }
 }
