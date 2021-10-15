8984d6a1ee423765a5614af2f80fae0e182550df
Shivkamal <59872823+kamal-shiv@users.noreply.github.com>
2021-10-15 20:21:53 +0530

    Rigid and Run

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator animator;
+    // Start is called before the first frame update
+    private  void Update()
+    {
+        float speed=Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Speed",Mathf.Abs(speed));
+        // for run into left direction
+
+        Vector3 scale=transform.localScale;
+        if(speed<0){
+            scale.x=-1f*Mathf.Abs(scale.x);
+        }else if(speed>0){
+            scale.x=Mathf.Abs(scale.x);
+        }
+        transform.localScale=scale;
+    }
+}
