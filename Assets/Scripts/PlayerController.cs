9aa6d0aa384ef07da18d5f535f27f055f14c0988
dsm231 <deepmann.singh@gmail.com>
2021-10-16 22:17:05 +0530

    Player Controller(horizontal input,controlling run animation)

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator animator;
+    private void Awake()
+    {
+    Debug.Log("Player controller awake");    
+    }
+
+    // private void OnCollisionEnter2D(Collision2D collision)
+    // {
+    //  Debug.Log("Collision:" + collision.gameObject.name);  
+    // }
+
+    private void Update()
+    {
+        float speed = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs(speed));
+
+        Vector3 scale= transform.localScale;
+          
+        
+         if(speed<0){
+            scale.x = -1;
+        }
+        else if(speed>0){
+            //scale.x = Mathf.Abs(scale.x);
+            scale.x = 1;
+        }
+        transform.localScale = scale;
+    }
+}
