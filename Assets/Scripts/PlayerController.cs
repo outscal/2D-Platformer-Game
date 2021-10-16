94955f2d53991e6930c3a35d4d526fc904ed700e
Shivkamal <59872823+kamal-shiv@users.noreply.github.com>
2021-10-16 20:39:49 +0530

    player control script
    Crouch and Jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
+    
     // Start is called before the first frame update
     private  void Update()
     {
         float speed=Input.GetAxisRaw("Horizontal");
         animator.SetFloat("Speed",Mathf.Abs(speed));
-        // for run into left direction
-
+        // for run in flip
+        
         Vector3 scale=transform.localScale;
         if(speed<0){
             scale.x=-1f*Mathf.Abs(scale.x);
@@ -19,5 +20,14 @@ public class PlayerController : MonoBehaviour
             scale.x=Mathf.Abs(scale.x);
         }
         transform.localScale=scale;
+        
+        bool isCrouching= Input.GetKey(KeyCode.DownArrow);
+        animator.SetBool("Crouch",isCrouching);
+        
+        bool jump=Input.GetKey(KeyCode.UpArrow);
+        // animator.SetFloat("Speed",Mathf.Abs(speed1));
+        animator.SetBool("Jump",jump);
+           
     }
+
 }
