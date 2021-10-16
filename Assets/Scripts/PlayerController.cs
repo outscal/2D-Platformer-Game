3a926018d4338fae0b18f255ee97d1b9fc7ea9c0
Parva <parvahdesai10@gmail.com>
2021-10-16 19:12:27 +0530

    Playing Jump and Crouch animation on user input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,7 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Animator animator; 
+    public Animator playerAnimator;
+    public float speed;
+
+    private Vector2 standingColliderOffset = new Vector2(0f, 0.98f);
+    private Vector2 standingColliderSize = new Vector2(0.45f, 2.05f);
+    private Vector2 crouchingColliderOffset = new Vector2(-0.15f, 0.6f);
+    private Vector2 crouchingColliderSize = new Vector2(0.75f, 1.3f);
 
     // Start is called before the first frame update
     void Start()
@@ -15,19 +21,55 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float speed = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("speed", Mathf.Abs(speed));
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Jump");
 
-        Vector3 scale = transform.localScale;
-        if (speed < 0)
-        {
-            scale.x = -1f * Mathf.Abs(scale.x); 
-        }
-        else if(speed > 0)
-        {
-            scale.x = Mathf.Abs(scale.x); 
-        }
-        transform.localScale = scale; 
+        PlayerMovementAnimation(horizontal, vertical);
     }
 
+    private void PlayerMovementAnimation(float horizontal, float vertical)
+    {
+
+        //Move horizontally to Walk/Run
+        playerAnimator.SetFloat("speed", Mathf.Abs(horizontal));
+
+        Vector3 scale = transform.localScale;
+        if (horizontal < 0)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        else if (horizontal > 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
+        
+
+        //Jump
+        if (vertical > 0)
+        {
+            playerAnimator.SetBool("jump", true);
+        }
+        else
+        {
+            playerAnimator.SetBool("jump", false);
+        }
+
+        //Crouch
+        if (Input.GetKey(KeyCode.LeftControl) || Input.GetKey(KeyCode.RightControl))
+        {
+            playerAnimator.SetBool("crouch", true);
+
+            GetComponent<BoxCollider2D>().offset = crouchingColliderOffset;
+            GetComponent<BoxCollider2D>().size = crouchingColliderSize;
+        }
+        else
+        {
+            playerAnimator.SetBool("crouch", false);
+
+            GetComponent<BoxCollider2D>().offset = standingColliderOffset;
+            GetComponent<BoxCollider2D>().size = standingColliderSize;
+        }
+
+    }
 }
