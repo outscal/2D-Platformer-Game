6366d27e8a7649097b1874cabb3e3bfb7f73c726
nannapuraju Karthikeya <karthik.suraj2619@gmail.com>
2021-10-16 11:24:07 +0530

    crouch added

--- Assets/scripts/playerController.cs
+++ Assets/scripts/playerController.cs
@@ -7,6 +7,7 @@ public class playerController : MonoBehaviour
     [SerializeField] SpriteRenderer sr;
     [SerializeField] Rigidbody2D rb;
     [SerializeField] Animator anmi;
+    bool crouch = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +17,7 @@ public class playerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+       
         anmi.SetFloat("run",0);
         transform.position += new Vector3(Input.GetAxis("Horizontal"),  Input.GetAxis("Vertical"),0) *5*Time.deltaTime;
         if(Input.GetAxis("Horizontal")>0)
@@ -28,7 +30,23 @@ public class playerController : MonoBehaviour
             sr.flipX = true;
             anmi.SetFloat("run", 0.6f);
         }
-        if()
+        if(Input.GetAxis("Vertical")>0)
+        {
+            
+            anmi.SetBool("jump", true);
+
+        }
+        else
+        {
+            anmi.SetBool("jump", false);
+        }
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            crouch = !crouch;
+            anmi.SetBool("crouch", crouch);
+           
+
+        }
 
 
     }
