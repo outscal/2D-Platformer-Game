12f61475d06ed73454f84c4ec19ca199276c2451
Shivkamal <kamal692103@gmail.com>
2021-10-17 20:00:26 +0530

    Update script
    Flipping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+    public Animator animator;
     // Update is called once per frame
     void Update()
     {
+       float horizontal= Input.GetAxisRaw("Horizontal");
+       playAnimationMovement(horizontal); 
+    }
+    private void playAnimationMovement(float horizontal){
+        animator.SetFloat("Speed",Mathf.Abs(horizontal));
         
+        // Flipping
+        Vector3 scale=transform.localScale;
+        if(horizontal<0){
+            scale.x=-1f*Mathf.Abs(scale.x);
+        }
+        if(horizontal>0){
+            scale.x=Mathf.Abs(scale.x);
+        }
+        transform.localScale=scale;
     }
 }
