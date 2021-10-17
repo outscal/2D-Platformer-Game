3b77cd0ef27ef8d20dd9b970ca8709f30295dedc
Harvinder2115 <harvinder2194@gmail.com>
2021-10-17 16:34:46 +0530

    Player movement (Left & Right Run) by clicking (A & D key ) VscodePlayerMovement Done

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,5 +4,18 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    update
+    public Animator animator;
+
+    private void Update() 
+    {
+        float speed = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Speed",Mathf.Abs (speed));
+        Vector3 scale = transform.localScale;
+        if(speed < 0) {
+            scale.x = -1f * Mathf.Abs (scale.x); }
+        else if(speed > 0) {
+            scale.x = Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
+    }
 }
