7d4e6dba9341561deea58f2e92e9eab60e117d77
Pranat1 <pranat@lucknowmarbles.com>
2021-10-19 19:47:10 +0530

    Jump animation and TileMap

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,7 +5,13 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
+    public float speed = 2f;
+    private Rigidbody2D rB2d;
+    public float jumpForce;
     // Start is called before the first frame update
+    private void Awake(){
+        rB2d = gameObject.GetComponent<Rigidbody2D>();
+    }
     void Start()
     {
         
@@ -14,16 +20,49 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float speed = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("Speed", Mathf.Abs(speed));
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float verticle = Input.GetAxisRaw("Jump");
+        
+        MoveCharector(horizontal, verticle);
+        methodAnimationRun(horizontal, verticle);
+
+        if (Input.GetButton("Fire1")){
+            animator.SetBool("Crowch", true);
+        }
+        else{
+            animator.SetBool("Crowch", false);
+        }
+    
+    }
+
+    private void methodAnimationRun(float horizontal, float verticle){
+        
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
         Vector3 scale = transform.localScale;
-        if (speed < 0 && scale.x > 0)
+        if (horizontal < 0 && scale.x > 0)
         {
             scale.x = -1f* scale.x;
         }
-        else if(speed > 0){
+        else if(horizontal > 0){
             scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
+        
+        if (verticle > 0)
+        {
+            animator.SetBool("Jump", true);
+        }
+        else{
+            animator.SetBool("Jump", false);
+        }
+    }
+    private void MoveCharector(float horizontal, float verticle){
+        Vector3 position = transform.position;
+        position.x += horizontal * speed * Time.deltaTime;
+        transform.position = position;
+
+        if (verticle > 0){
+        rB2d.AddForce(new Vector2(0f,jumpForce), ForceMode2D.Force);
+        }
     }
 }
