4b9d139ddbaaccf6a77eb145389bb274340d4a67
punitp59 <punitp59@gmail.com>
2021-10-25 18:46:28 +0530

    Movement Assignment

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,36 +5,71 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
-
+    public float speed;
+    public float jump;
+    private Rigidbody2D rb2d;
     private void Awake()
     {
         Debug.Log("Player Controller Awake");
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
     }
-
-
-    /*private void OnCollisionEnter2D(Collision2D collision)
-    {
-        Debug.Log("Collision Detected with " + collision.gameObject.name);
-    }*/
     
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
     // Update is called once per frame
     private void Update()
     {
-        float Speed = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("AnimSpeed", Mathf.Abs(Speed));
+        float Horizontal = Input.GetAxisRaw("Horizontal");
+        float Vertical = Input.GetAxisRaw("Jump");
+        PlayerMovementAnimation(Horizontal,Vertical);
+        MoveCharacter(Horizontal,Vertical);
+    }
+    private void PlayerMovementAnimation(float Horizontal,float Vertical)
+    {
+        animator.SetFloat("AnimSpeed", Mathf.Abs(Horizontal));
 
+        //Horizontal Animator Panel
         Vector3 scale = transform.localScale;
-        if (Speed < 0){
+        if (Horizontal < 0)
+        {
             scale.x = -1f * Mathf.Abs(scale.x);
-        }else if(Speed > 0){
+        }
+        else if(Horizontal > 0)
+        {
             scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
+
+        //Vertical Animator Panel
+        if (Vertical > 0)
+        {
+            animator.SetBool("AnimJump", true);
+        }
+        /*else if(Vertical < 0)
+        {
+            animator.SetBool("Crouch", true);
+        }*/
+        else
+        {
+            animator.SetBool("AnimJump", false);
+            //animator.SetBool("AnimCrouch", false);
+        }
     }
+    private void MoveCharacter(float Horizontal,float Vertical)
+    {
+        //move character horizontally
+        Vector3 position = transform.position;
+        position.x += Horizontal*speed*Time.deltaTime;
+        transform.position = position;
+
+        //move character vertically
+        if(Vertical > 0)
+        {
+            rb2d.AddForce(new Vector2(0f,jump), ForceMode2D.Force);
+        }
+    }
+    
 }
