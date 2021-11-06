e8da6be8656df4e281defbb91427cc8edefbf2d5
Kathiravan Thirumurthy <tkathiravan1976@gmail.com>
2021-11-06 23:45:52 +0530

    Crouch and resize collider

--- Assets/Scripts/Playercontroller.cs
+++ Assets/Scripts/Playercontroller.cs
@@ -12,18 +12,30 @@ public class Playercontroller : MonoBehaviour
     private float jump;
     private Rigidbody2D rb;
     private bool isGrounded;
+    [SerializeField]
+    private BoxCollider2D Player_Collider;
+    private SpriteRenderer sr;
+    private float m_ScaleX, m_ScaleY, m_ScaleZ;
     // Start is called before the first frame update
     void Awake()
     {
         rb = gameObject.GetComponent<Rigidbody2D>();
+        Player_Collider = GetComponent<BoxCollider2D>();
+        sr = GetComponent<SpriteRenderer>();
         isGrounded = true;
+        
+        m_ScaleX = Player_Collider.size.x;
+        m_ScaleY = Player_Collider.size.y;
+        // m_ScaleZ = Player_Collider.size.z;
+        Debug.Log("Current Sprite Size before: " + sr.sprite.bounds.size);
+        Debug.Log("Current BoxCollider Size before: " + Player_Collider.size);
+
     }
         // Update is called once per frame
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Jump");
-        Debug.Log(vertical);
         // playerInput();
         movePlayer(horizontal,vertical);
         playerMovementAnimation(horizontal, vertical);
@@ -33,17 +45,58 @@ public class Playercontroller : MonoBehaviour
         animator.SetFloat("Speed", Mathf.Abs(horizontal));
         flipPlayer(horizontal);
         jumpPlayer(vertical);
+        if (Input.GetKeyDown(KeyCode.RightControl) || Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            
+            animator.SetBool("crouch", true);
+            print("Control key was pressed");
+            print("Sprite bound size:"+sr.sprite.bounds.size/2);
+                    
+            /* m_ScaleX = sr.sprite.bounds.size.x;
+             m_ScaleY = sr.sprite.bounds.size.y;*/
+
+            //Player_Collider.size = new Vector3(m_ScaleX, m_ScaleY);
+            Player_Collider.size = new Vector3(1.13f, 1.64f);
+            Player_Collider.offset = new Vector3(0f,0.63f);
+            Debug.Log("Current BoxCollider Size : " + Player_Collider.size);
+            /*Destroy(Player_Collider);
+            gameObject.AddComponent<BoxCollider2D>();
+            x=1.07,y=1.2 offx=-0.13,y=0.63
+            jump x=1.07y=1.8 , xoff=0.07547355 yoff=1.6
+             */
+
+        }
+        else if (Input.GetKeyUp(KeyCode.RightControl) || Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            animator.SetBool("crouch", false);
+            Player_Collider.size = new Vector3(0.6f, 2.3f);
+            Player_Collider.offset = new Vector3(0f, 0.98f);
+            // print("Control key was release");
+        }
+       
+    }
+    private void crouchPlayer(float vertical)
+    {
+        animator.SetBool("crouch", true);
+        print("Control key was pressed");
     }
     private void jumpPlayer(float vertical)
     {
         if (vertical > 0)
         {
              animator.SetBool("jump", true);
-           
+            Player_Collider.size = new Vector3(1f, 1.8f);
+            Player_Collider.offset = new Vector3(0f, 1.6f);
+
         }
             
-        else 
+        else
+        {
             animator.SetBool("jump", false);
+            Player_Collider.size = new Vector3(0.6f, 2.3f);
+            Player_Collider.offset = new Vector3(0f, 0.98f);
+        }
+            
     }
     private void movePlayer(float horizontal,float vertical)
     {
@@ -55,9 +108,10 @@ public class Playercontroller : MonoBehaviour
             if(isGrounded)
             {
                 rb.AddForce(new Vector2(0f, jump), ForceMode2D.Force);
-               // rb.AddForce(Vector2.up * jump * Time.deltaTime);
+                // rb.AddForce(Vector2.up * jump * Time.deltaTime);
                 isGrounded = false;
             }
+            
 
         }
 
