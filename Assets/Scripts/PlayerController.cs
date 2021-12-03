0b61d0f65132d3bd7ba456672e1390697c7d12f0
DESKTOP-QC0Q37G\rupam <rdebmail@gmail.com>
2021-12-04 00:22:07 +0530

    Added Jump animation and movement for player

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,30 +7,61 @@ public class PlayerController : MonoBehaviour
     public Animator animator;
 
     public float speed;
+    public float jump;
+
+    private Rigidbody2D rBody;
+    
+
+    private void Awake()
+    {
+        rBody = gameObject.GetComponent<Rigidbody2D>();
+        gameObject.GetComponent<SpriteRenderer>();
+
+    }
 
     // Start is called before the first frame update
     private void Start()
     {
-        
-    }
+         
+    } 
 
     // Update is called once per frame
     private void Update()
     {
+        //horizontal movement
         float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Jump");
+        
+        PlayerMovementAnimation(horizontal, vertical);
+        MoveCharacter(horizontal, vertical);
 
-        MoveCharacter(horizontal);
-        PlayerMovementAnimation(horizontal);
+        //vertical movement
+        if (vertical > 0)
+        {
+            rBody.AddForce(new Vector2(0f, jump), ForceMode2D.Force);
+        }
+
+
     }
 
-    private void MoveCharacter(float horizontal)
+    private void FixedUpdate()
     {
+
+
+    }
+
+
+
+    private void MoveCharacter(float horizontal, float vertical)
+    {
+        //horizontal player movement
         Vector3 position = transform.position;
         position.x += horizontal * speed * Time.deltaTime;
         transform.position = position;
+
     }
 
-    private void PlayerMovementAnimation(float horizontal)
+    private void PlayerMovementAnimation(float horizontal, float vertical)
     {
         animator.SetFloat("Speed", Mathf.Abs(horizontal));
 
@@ -46,5 +77,17 @@ public class PlayerController : MonoBehaviour
         }
 
         transform.localScale = scale;
+
+        //Jump code
+        //float vertical = Input.GetAxisRaw("Jump");
+        if (vertical > 0)
+        {
+            animator.SetBool("Jump", true);
+
+        }
+        else
+        {
+            animator.SetBool("Jump", false);
+        }
     }
 }
