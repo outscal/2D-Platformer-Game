31b83ef2ac73521e5ddbe8ec0f7357e2d01b7957
DESKTOP-QC0Q37G\rupam <rdebmail@gmail.com>
2021-12-05 18:31:53 +0530

    Restarted the level on player falling from the platform before reaching the goal-door, Added RestartLevel.cs script

---
+++ Assets/Scripts/RestartLevel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartLevel : MonoBehaviour
+{
+    //Level Over on Trigger
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>() != null)
+        {
+            Debug.Log("Player is Dead");
+            Debug.Log("Restart Level..........");
+            SceneManager.LoadScene("NewScene");
+        }
+    }
+}
