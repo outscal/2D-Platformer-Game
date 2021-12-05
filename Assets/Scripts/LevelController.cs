1d65355e5f1140e034e1f8b92980d9351d2a0e1f
DESKTOP-QC0Q37G\rupam <rdebmail@gmail.com>
2021-12-05 18:20:09 +0530

    Door created at end of level with OnTriggerEnter, the game should run the code for next level, LevelController.cs script file added.

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    //Level Over on Trigger
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>() != null)
+        {
+            Debug.Log("Level Finished by the player");
+            Debug.Log("Run Next Level..........");
+        }
+    }
+}
