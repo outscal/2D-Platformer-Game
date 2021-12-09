02f1d877fa120c2db18cfd93c47b4fd2544d8090
mppavan05 <mppavan05@gmail.com>
2021-12-09 22:02:35 +0530

    Create Player_controller.cs
    script

---
+++ Assets/Player_controller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_controller : MonoBehaviour
+{
+    public Animator animator;
+    private void Awake()
+    {
+        Debug.Log(" player controller awake ");
+
+    }
+
+    private void Update()
+    {
+        float speed = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs (speed));
+
+         Vector3 scale = transform.localScale;
+        if (speed < 0)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        else if (speed > 0){
+            scale.x = Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
+    }
+}
