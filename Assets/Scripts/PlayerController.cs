9d4c4831d52a5ff92b9ff3a37815b6e21142e58e
Mukunda Penke <mukunda.penke@gmail.com>
2021-12-10 03:23:07 -0800

    commit_Walk

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,23 +6,39 @@ public class PlayerController : MonoBehaviour
 {
     public Animator animator;
 
+    public float speed;
+
+
 
     // Update is called once per frame//
     void Update()
     {
-        float speed = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("Speed", Mathf.Abs(speed));
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        PlayMovementAnimation(horizontal);
+        MoveCharacter(horizontal);
+
+    }
+    private void MoveCharacter(float horizontal)
+    {
+        Vector3 position = transform.position;
+        position.x += horizontal * speed * Time.deltaTime;
+        transform.position = position;
+
+    }
+
+    private void PlayMovementAnimation(float horizontal)
+    {
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
 
         Vector3 scale = transform.localScale;
-        if (speed < 0)
+        if (horizontal < 0)
         {
-            scale.x = -1f *Mathf.Abs(scale.x);
+            scale.x = -1f * Mathf.Abs(scale.x);
         }
-        else if (speed > 0)
+        else if (horizontal > 0)
         {
             scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
-
     }
 }
