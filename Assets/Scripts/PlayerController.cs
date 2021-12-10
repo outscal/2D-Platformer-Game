628728a66f85dcf6b2be44af6a21659d78997ab2
Mukunda Penke <mukunda.penke@gmail.com>
2021-12-10 09:12:58 -0800

    commit_Jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,32 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
+    private Rigidbody2D rb2d;
+
 
     public float speed;
+    public float jump;
+    public bool isCrouching = false;
 
-
+    private void Start()    
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+        animator = gameObject.GetComponent<Animator>();
+    }
 
     // Update is called once per frame//
     void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
-        PlayMovementAnimation(horizontal);
-        MoveCharacter(horizontal);
+        float verticle = Input.GetAxisRaw("Jump");
+        PlayMovementAnimation(horizontal,verticle);
+        MoveCharacter(horizontal,verticle);
 
     }
-    private void MoveCharacter(float horizontal)
+    private void MoveCharacter(float horizontal,float verticle )
     {
         Vector3 position = transform.position;
         position.x += horizontal * speed * Time.deltaTime;
         transform.position = position;
 
-    }
 
-    private void PlayMovementAnimation(float horizontal)
+        if (verticle > 0)
+            {
+            rb2d.AddForce(new Vector2(0f, jump), ForceMode2D.Force);
+            }
+    }
+   
+
+    private void PlayMovementAnimation(float horizontal,float verticle)
     {
         animator.SetFloat("Speed", Mathf.Abs(horizontal));
 
@@ -40,5 +56,12 @@ public class PlayerController : MonoBehaviour
             scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
+       
+        if (verticle > 0)
+            animator.SetBool("Jump", true);
+        else
+        {
+            animator.SetBool("Jump", false);
+        }
     }
 }
