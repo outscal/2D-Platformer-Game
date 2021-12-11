67ba7accec5655c5cf0b1620b1caf2ea6f34cf73
Avinashilingam <64300843+Avinashilingam@users.noreply.github.com>
2021-12-11 18:37:31 +0530

    Feature2 commit 3 Flipping
    Added the logic for Flipping in the script "playercontroller".

--- 2D_Platformer_game/Assets/Scripts/PlayerController.cs
+++ 2D_Platformer_game/Assets/Scripts/PlayerController.cs
@@ -14,5 +14,16 @@ public class PlayerController : MonoBehaviour
     {
         float speed = Input.GetAxisRaw("Horizontal");
         animator.SetFloat("Speed", Mathf.Abs(speed));
+
+        Vector3 scale = transform.localScale;
+        if(speed < 0)
+        {
+          scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        else if(speed > 0)
+        {
+          scale.x = Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
     }
 }
