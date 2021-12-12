493e01197802c5ce7ec6b6b2a264a4bc1748bbce
mppavan05 <mppavan05@gmail.com>
2021-12-12 12:03:01 +0530

    color and end message
    color and end message

---
+++ Assets/script/LevelOverController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelOverController : MonoBehaviour
+{
+    //  public int iLevelToLoad;
+    // public string sLevelToLad;
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        if (collision.gameObject.GetComponent<Player_controller>() != null)
+        {
+        //level is completed
+          Debug.Log("Level 1 is finished by player");
+    }
+    // {
+    // if (collision.gameObject.CompareTag("Player"))
+    // if (collision.gameObject.GetComponent<Player_controller>() != null)
+    //  {
+    // level is completed
+    // Debug.Log("Level 1 is finished by player");
+}
+      }
+
