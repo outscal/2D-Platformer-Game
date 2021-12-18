ce48dcca9964f64ecee13beb6c524e9db56d56e6
Jay Thanki <thankijay15@gmail.com>
2021-12-18 15:15:42 +0530

    Feature 7
    Feature 7 - Lobby Screen Setup

---
+++ Assets/Scripts/MenuController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject LevelUI;
+
+    public void GameStart()
+    {
+        LevelUI.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+[RequireComponent(typeof(Button))]
+public class SceneLoader : MonoBehaviour
+{
+    private Button button;
+
+    public string Levelname;
+
+    private void Awake()
+    {
+        button = GetComponent<Button>();
+        button.onClick.AddListener(onClick);
+    }
+
+    private void onClick()
+    {
+        SceneManager.LoadScene(Levelname);
+    }
+}
