667b78fd19f84fef09940fa6264103a1331b236c
Ashutosh Sharma <sharma.aashu1208@gmail.com>
2022-01-16 23:39:35 +0530

    Chapter-2 all parts commited

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerController : MonoBehaviour
 {
+
+    public Animator anim;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +15,24 @@ public class NewBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        float speed = Input.GetAxisRaw("Horizontal");
+
+        if (speed>0)
+        {
+            anim.SetFloat("Speed", speed);
+        }
+
+        else if (speed<0)
+        {
+            anim.SetFloat("-Speed", speed);
+        }
+
+        else
+        {
+
+
+        }
         
     }
 }
