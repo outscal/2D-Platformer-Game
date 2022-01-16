37774acf12f979669c37b9225b1614656d5b63bb
ambrollins <gauravas079@gmail.com>
2022-01-16 16:00:16 +0530

    PlayerController
    added player controller script ,
    added rigid body 2d and box collider 2D to player .
    added empty game object (platform )
    added box collider 2D to empty game object

---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Animator animator;
+
+
+    private void Awake() 
+    {
+      Debug.Log("Player controller awake");
+    }
+  // private void OnCollisionEnter2D(Collision2D collision)
+   //{
+    //  Debug.Log("Collision:" + collision.gameObject.name);
+ // }
+ private void Update()
+  {
+     float speed = Input.GetAxisRaw("Horizontal");
+     animator.SetFloat("Speed", Mathf.Abs(speed));
+
+     {
+         Vector3 scale = transform.localScale;
+         if(speed < 0)
+         {
+         scale.x = -1f * Mathf.Abs(scale.x);
+     }
+     else if(speed > 0)
+     {
+         scale.x = Mathf.Abs(scale.x);
+     }
+     transform.localScale = scale;
+ }
+
+   }
+}
