e137525c09721f8de1ad4be76402ae5134012e30
ambrollins <gauravas079@gmail.com>
2022-01-19 16:12:02 +0530

    added player controller movements
    added crouch, Jump input and assigned it to "S" and "J"

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -4,34 +4,75 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Animator animator;
+public Animator animator;
+public float speed;
+public float jumpSpeed;
 
-
-    private void Awake() 
-    {
+public Rigidbody2D RB2D;
+private void Awake() 
+   {
       Debug.Log("Player controller awake");
-    }
-  // private void OnCollisionEnter2D(Collision2D collision)
-   //{
-    //  Debug.Log("Collision:" + collision.gameObject.name);
- // }
- private void Update()
-  {
-     float speed = Input.GetAxisRaw("Horizontal");
-     animator.SetFloat("Speed", Mathf.Abs(speed));
-
-     {
-         Vector3 scale = transform.localScale;
-         if(speed < 0)
-         {
-         scale.x = -1f * Mathf.Abs(scale.x);
-     }
-     else if(speed > 0)
-     {
-         scale.x = Mathf.Abs(scale.x);
-     }
-     transform.localScale = scale;
- }
-
+      RB2D = gameObject.GetComponent<Rigidbody2D>();
    }
+private void Update()
+{
+   float horizontal = Input.GetAxisRaw("Horizontal");
+   float vertical = Input.GetAxisRaw("Jump");
+
+   MoveCharacter(horizontal,vertical);
+   PlayMovementAnimation(horizontal,vertical); 
+}
+private void MoveCharacter(float horizontal , float vertical)
+{
+   Vector2 position = transform.position;
+   position.x = position.x + horizontal * speed * Time.deltaTime;
+   transform.position = position;
+
+   if (vertical > 0)
+   {
+      RB2D.AddForce(new Vector2(0f , jumpSpeed),ForceMode2D.Impulse);
+   }
+}
+private void PlayMovementAnimation(float horizontal, float vertical)
+{
+
+animator.SetFloat("speed", Mathf.Abs(horizontal));
+
+Vector2 scale = transform.localScale;
+if(horizontal < 0)
+   {
+     scale.x = -1f * Mathf.Abs(scale.x);
+   }
+else if(horizontal > 0)
+   {
+      scale.x = Mathf.Abs(scale.x);
+   }
+   transform.localScale = scale;
+
+   //to jump
+   // if (vertical>0)
+   // {
+   //    animator.SetBool("Jump",true);
+   // } 
+   // else
+   // {
+   //    animator.SetBool("Jump",false);
+   // }
+   //if (Input.GetButtonDown("Jump"))
+   //if (vertical > 0)
+   if (Input.GetKeyDown(KeyCode.W))
+   {
+      animator.SetTrigger("Jump");
+      RB2D.velocity = new Vector2(RB2D.velocity.x,jumpSpeed);
+   }
+
+   if (Input.GetKeyDown(KeyCode.S))
+   {
+      animator.SetBool("Crouch",true);
+   }
+   else if(Input.GetKeyUp(KeyCode.S))
+   {
+      animator.SetBool("Crouch",false);
+   }
+} 
 }
