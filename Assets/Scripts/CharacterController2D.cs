e7e80d5fb1d8dc41696443026500f05a06033148
Amit8844 <amitkrsingh1995.sup@gmail.com>
2022-01-20 21:37:18 +0530

    Movement
    Added Jump
    Added Crouch
    Added Physics Material 2D
    Added Capsul Collider
    Added Camera Movement
    Added Ground

---
+++ Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(CapsuleCollider2D))]
+
+public class CharacterController2D : MonoBehaviour
+{
+    // Move player in 2D space
+    public float maxSpeed = 3.4f;
+    public float jumpHeight = 6.5f;
+    public float gravityScale = 1.5f;
+    public Camera mainCamera;
+
+    bool facingRight = true;
+    float moveDirection = 0;
+    bool isGrounded = false;
+    Vector3 cameraPos;
+    Rigidbody2D r2d;
+    CapsuleCollider2D mainCollider;
+    Transform t;
+
+    // Use this for initialization
+    void Start()
+    {
+        t = transform;
+        r2d = GetComponent<Rigidbody2D>();
+        mainCollider = GetComponent<CapsuleCollider2D>();
+        r2d.freezeRotation = true;
+        r2d.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+        r2d.gravityScale = gravityScale;
+        facingRight = t.localScale.x > 0;
+
+        if (mainCamera)
+        {
+            cameraPos = mainCamera.transform.position;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Movement controls
+        if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) && (isGrounded || Mathf.Abs(r2d.velocity.x) > 0.01f))
+        {
+            moveDirection = Input.GetKey(KeyCode.A) ? -1 : 1;
+        }
+        else
+        {
+            if (isGrounded || r2d.velocity.magnitude < 0.01f)
+            {
+                moveDirection = 0;
+            }
+        }
+
+        // Change facing direction
+        if (moveDirection != 0)
+        {
+            if (moveDirection > 0 && !facingRight)
+            {
+                facingRight = true;
+                t.localScale = new Vector3(Mathf.Abs(t.localScale.x), t.localScale.y, transform.localScale.z);
+            }
+            if (moveDirection < 0 && facingRight)
+            {
+                facingRight = false;
+                t.localScale = new Vector3(-Mathf.Abs(t.localScale.x), t.localScale.y, t.localScale.z);
+            }
+        }
+
+        // Jumping
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        {
+            r2d.velocity = new Vector2(r2d.velocity.x, jumpHeight);
+        }
+
+        // Camera follow
+        if (mainCamera)
+        {
+            mainCamera.transform.position = new Vector3(t.position.x, cameraPos.y, cameraPos.z);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Bounds colliderBounds = mainCollider.bounds;
+        float colliderRadius = mainCollider.size.x * 0.4f * Mathf.Abs(transform.localScale.x);
+        Vector3 groundCheckPos = colliderBounds.min + new Vector3(colliderBounds.size.x * 0.5f, colliderRadius * 0.9f, 0);
+        // Check if player is grounded
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheckPos, colliderRadius);
+        //Check if any of the overlapping colliders are not player collider, if so, set isGrounded to true
+        isGrounded = false;
+        if (colliders.Length > 0)
+        {
+            for (int i = 0; i < colliders.Length; i++)
+            {
+                if (colliders[i] != mainCollider)
+                {
+                    isGrounded = true;
+                    break;
+                }
+            }
+        }
+
+        // Apply movement velocity
+        r2d.velocity = new Vector2((moveDirection) * maxSpeed, r2d.velocity.y);
+
+        // Simple debug
+        Debug.DrawLine(groundCheckPos, groundCheckPos - new Vector3(0, colliderRadius, 0), isGrounded ? Color.green : Color.red);
+        Debug.DrawLine(groundCheckPos, groundCheckPos - new Vector3(colliderRadius, 0, 0), isGrounded ? Color.green : Color.red);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+public class PlayerController : MonoBehaviour
+{
+  public Animator animator;
+  public Rigidbody2D rb2D;
+  public float moveSpeed;
+  public float jumpSpeed;
+  public bool isGrounded;
+  public bool isCrouching;
+   public Camera mainCamera;
+ 
+   Vector3 cameraPos;
+ 
+  private void Awake() 
+  {
+    Debug.Log("Player controller awake");  
+  }
+   void Start()
+   {
+       if (mainCamera)
+        {
+            cameraPos = mainCamera.transform.position;
+        }
+   }
+  
+  private void Update()
+  {
+    float speed = Input.GetAxisRaw("Horizontal");
+    animator.SetFloat("Speed", Mathf.Abs(speed));
+    if(isCrouching == true)
+    {
+      speed = 0;
+    }
+    // rb2D.velocity = new Vector2(speed * moveSpeed,rb2D.velocity.y);
+    transform.localPosition += Vector3.right * speed * moveSpeed * Time.deltaTime;
+    
+    if(speed < 0)
+    {
+      transform.rotation = Quaternion.Euler(transform.rotation.x, 180, transform.rotation.z);
+    }
+    else if(speed > 0)
+    {
+      transform.rotation = Quaternion.Euler(transform.rotation.x, 0, transform.rotation.z);
+    }
+    //Camera
+     if (mainCamera)
+        {
+            mainCamera.transform.position = new Vector3(transform.position.x, cameraPos.y, cameraPos.z);
+        }
+  
+    // Jump
+    if(Input.GetKeyDown(KeyCode.W) && isGrounded)
+    {
+      animator.SetTrigger("Jump");
+      rb2D.velocity = new Vector2(rb2D.velocity.x, jumpSpeed);
+    }
+    // Crouch
+    if(Input.GetKeyDown(KeyCode.S))
+    {
+      animator.SetBool("Crouch", true);
+      isCrouching = true;
+    }
+    else if (Input.GetKeyUp(KeyCode.S))
+    {
+      animator.SetBool("Crouch", false);
+      isCrouching = false;
+    }
+    
+  }
+ 
+  void OnCollisionEnter2D(Collision2D other) 
+  {
+    if (other.gameObject.tag == "Ground")
+    {
+      isGrounded = true;
+    }
+  }
+  void OnCollisionExit2D(Collision2D other) 
+  {
+    if (other.gameObject.tag == "Ground")
+    {
+      isGrounded = false;
+    }
+  }
+  
+}
+ 
+ 
