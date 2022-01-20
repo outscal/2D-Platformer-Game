1e99c1c8cc5c5bc458442ecdd00d6601125202a9
Yogesh Sonawane <96642408+Itachi206@users.noreply.github.com>
2022-01-20 15:58:35 +0530

    Added vetical input for jump with the finite jump

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,21 +5,37 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
-    public float speed;    
+    public float speed = 5; 
+    public float jumpVertical;  
+    private Rigidbody2D rbd2d; 
 
+    public bool isGrounded = false;
+
+    private void Awake(){ 
+        rbd2d = gameObject.GetComponent<Rigidbody2D>();
+
+    }
     private void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");  
-        Debug.Log("horizontal = " + horizontal);
-        MoveCharacter(horizontal); 
+        float vertical = Input.GetAxisRaw("Jump");
+        bool crouch = Input.GetKeyDown(KeyCode.LeftControl);
+        
+        MoveCharacter(horizontal, vertical); 
         PlayerMovementAnimation(horizontal); 
+        playerJumpAnimation(vertical);
+        PlayerCrouchAnimation(crouch);
     }
 
-    private void MoveCharacter(float horizontal){
+    private void MoveCharacter(float horizontal, float vertical){
         Vector3 position = transform.position;
         //(distance / time)  * (1 / frames per second) 
-        position.x += horizontal * speed * Time.deltaTime;
+        position.x += horizontal * speed * Time.deltaTime;       
         transform.position = position;         
+
+        if(vertical > 0 && isGrounded == true){
+            rbd2d.AddForce(new Vector2(0,jumpVertical), ForceMode2D.Force);
+        }
     }
 
     private void PlayerMovementAnimation(float horizontal)
@@ -33,5 +49,29 @@ public class PlayerController : MonoBehaviour
             scale.x = Mathf.Abs(scale.x);
         }         
         transform.localScale = scale;
+        
+    }
+
+    private void playerJumpAnimation(float vertical)
+    {
+        if(vertical > 0)
+        {
+            animator.SetBool("Jump", true);
+        } else
+        {
+            animator.SetBool("Jump", false);
+        }
+    }
+
+    private void PlayerCrouchAnimation(bool crouch)
+    {
+        if(crouch)
+        {
+            animator.SetBool("Crouch", true);
+        } else
+        {
+            animator.SetBool("Crouch", false);
+        }
+
     }
 }
---
+++ Assets/Scripts/isGround.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class isGround : MonoBehaviour
+{
+    GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = gameObject.transform.parent.gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision){
+        if(collision.collider.tag == "Ground"){
+            player.GetComponent<PlayerController>().isGrounded = true;
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision){
+        player.GetComponent<PlayerController>().isGrounded = false;
+    }
+}
