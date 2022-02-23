cc5205ec1c0dabd3051c5b9f3be8fa57efac6d04
Nikhil2194 <ndahake21@gmail.com>
2022-02-23 17:21:39 +0530

    Player Controller Script, Animation implemneted Jump, Crouch

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float JumpForce;
+
+    private Animator animator;
+    private void Start() 
+    {
+        rb = GetComponent<Rigidbody2D>();
+      animator = GetComponent<Animator>();  
+    }
+      void Update()
+    {
+       float speed = Input.GetAxisRaw("Horizontal");
+       animator.SetFloat("Speed", Mathf.Abs(speed));
+
+       Vector3 scale = transform.localScale;
+       if(speed<0)
+       {
+           scale.x = -1f * Mathf.Abs(scale.x);
+       }
+       else if (speed>0)
+       {
+           scale.x = Mathf.Abs(scale.x);
+       }
+       transform.localScale =scale;
+
+       if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(new Vector2(0,JumpForce),ForceMode2D.Impulse);
+            setJump();
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+           setCrouch();
+        }
+    }
+
+    void setJump () 
+    {
+        animator.SetTrigger("Jump");
+    }
+
+    void setCrouch ()
+    {
+        animator.SetTrigger("Crouch");
+    }
+}
