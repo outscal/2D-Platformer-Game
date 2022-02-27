2035d7fe0abb95fcc3ae66bb635e58691cb6fd75
Mukunda Penke <mukunda.penke@gmail.com>
2022-02-27 06:59:30 -0800

    Moving

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,23 +9,54 @@ public class PlayerController : MonoBehaviour
 {
 
     public Animator animator;
+    public float speed;
+    bool crouch;
 
 
     private void Update()
     {
-        float speed = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("Speed", Mathf.Abs(speed));
+        float horizontal = Input.GetAxisRaw("Horizontal");
+       // float jump = Input.GetAxisRaw("Verticle");
+        PlayerMovementAnimation(horizontal);
+        MoveCharacter(horizontal);
+    }
 
+    private void MoveCharacter(float horizontal)
+    {
+        Vector3 position = transform.position;
+        position.x += horizontal * speed * Time.deltaTime;
+        transform.position = position;
+    }
+
+    void PlayerMovementAnimation(float horizontal)
+    {
+
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
+
+        //crouching
+        if (Input.GetKeyDown(KeyCode.CapsLock))
+        {
+            crouch = true;
+            animator.SetBool("Crouch", crouch);
+        }
+        if (Input.GetKeyUp(KeyCode.CapsLock))
+        {
+            crouch = false;
+            animator.SetBool("Crouch", crouch);
+        }
+
+
+        //positioning
         Vector3 scale = transform.localScale;
-        if(speed < 0)
+        if (horizontal < 0)
         {
             scale.x = -1f * Mathf.Abs(scale.x);
         }
-        else if(speed > 0)
+        else if (horizontal > 0)
         {
             scale.x = Mathf.Abs(scale.x);
         }
-            
         transform.localScale = scale;
+
     }
 }
