427dfff528534757cfc900de14da0f030c4fc1ef
arindambharati <arindambharati@gmail.com>
2022-03-05 18:11:41 +0530

    Enemy gameObject, EnemyController, patrol added

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    private bool moveRight=true;
+    [SerializeField] private Transform groundDetection;
+
+
+    void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        RaycastHit2D groundInfo = Physics2D.Raycast(groundDetection.position, Vector2.down, 2f);
+
+        if (groundInfo.collider == false)
+        {
+            if (moveRight == true)
+            {
+                transform.eulerAngles = new Vector3(0, -180, 0);
+                moveRight = false;
+            }
+            else
+            {
+                transform.eulerAngles = new Vector3(0, 0, 0);
+                moveRight = true;
+            }
+        }
+    }
+}
