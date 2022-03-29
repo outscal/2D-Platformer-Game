9c350b2ba27394b281c2935c6f1d60a0c349f752
Godfatherkrish <100038747+Godfatherkrish@users.noreply.github.com>
2022-03-29 16:08:46 +0530

    LevelOverController and Next Level

---
+++ LevelOverController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelOverController : MonoBehaviour
+{
+   private void OnTriggerEnter2D(Collider2D collision)
+   {
+       if (collision.gameObject.GetComponent<PlayerController>() != null )
+       {
+           //Level is over
+           Debug.Log("Level Finished by the Player");
+           Debug.Log(" Loading the next level");
+       }
+   }
+}
