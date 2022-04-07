32b69d8f99771d58fe8dde44ff40b79b997f90bb
Godfatherkrish <100038747+Godfatherkrish@users.noreply.github.com>
2022-04-07 17:27:03 +0530

    Score Controller

---
+++ ScoreController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+
+public class ScoreController : MonoBehaviour
+{
+    private TextMeshProUGUI scoreText;
+
+    private int score = 0;
+
+
+    /// Awake is called when the script instance is being loaded.
+
+    private void Awake()
+    {
+        scoreText = GetComponent<TextMeshProUGUI>();
+    }
+
+/// Start is called on the frame when a script is enabled just before
+/// any of the Update methods is called the first time.
+
+    private void Start()
+    {
+        RefreshUI();
+    }
+
+    public void IncreaseScore(int increment)
+    {
+        score += increment;
+        RefreshUI();
+    }
+
+    private void RefreshUI()
+    {
+        scoreText.text = " Score " +  score;
+    }
+
+
+}
