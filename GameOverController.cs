8a94e48ba58918abaca46dd5fde1a9093164b864
Godfatherkrish <100038747+Godfatherkrish@users.noreply.github.com>
2022-04-11 15:29:13 +0530

    GameOverController
    Lobby Screen

---
+++ GameOverController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameOverController : MonoBehaviour
+{
+    public Button buttonRestart;
+
+     void Awake()
+    {
+        buttonRestart.onClick.AddListener(ReloadLevel);
+    }
+
+  public void PlayerDied()
+  {
+      gameObject.SetActive(true);
+  }
+  private void ReloadLevel()
+    {
+        Debug.Log("Reloading scene 1");
+        SceneManager.LoadScene(1);
+
+    }
+
+
+}
