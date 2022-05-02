74f64c33d70c154352c409213f70d0b47b407a45
Shakil-Hassan <shakilhassan.bcm@gmail.com>
2022-05-02 23:54:02 +0530

    Revert "Added Lots of Things like enemy controller, health system, death and reload scene"
    This reverts commit 6d1359c7d86a332cbdef8f0aa8a2f8150f7fcf7c.

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,44 +9,6 @@ public class PlayerController : MonoBehaviour
     private Rigidbody2D rb2d;
     public float speed;
     public float jump;
-<<<<<<< Updated upstream
-=======
-    
-    public isGrounded isPlayerGrounded;//is Player Grounded
-    /*private bool falling = false;
-    private bool playerDead = false;*/
-    public ScoreController scoreController;//Score Controller
-    public HealthController _healthController;//Health Controller
-    private SpriteRenderer spriteR;
-
-
-    public void PickUpKey()
-    {
-        //Debug.Log("Player Picked Up the Key");
-        scoreController.IncreaseScore(10);
-    }
-    //Player death
-    public void playerDeathAnimation()
-    {
-        //Debug.Log("Player Dead");
-        animator.SetBool("Fall", true);
-        
-        //Camera.main.transform.parent = null; // unchild from player. Keeps same position in world
-        //Destroy(gameObject); // player kills itself
-    }
-
-  
-    public BoxCollider2D collider;
-
-    public Vector2 standingSize;
-    public Vector2 sCrouchingOffset;
-    public Vector2 crouchingSize;
-    public Vector2 crouchingOffset;
-
-
-
-
->>>>>>> Stashed changes
 
 
     private void Awake()
@@ -57,16 +18,7 @@ public class PlayerController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-<<<<<<< Updated upstream
         
-=======
-        playerRigidBody = GetComponent<Rigidbody2D>();
-        collider = GetComponent<BoxCollider2D>();
-        
-        
-
-
->>>>>>> Stashed changes
     }
 
    
@@ -74,24 +26,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-<<<<<<< Updated upstream
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Jump");
         MoveCharacter(horizontal, vertical);
         PlayerAnimationMovement(horizontal, vertical);
-=======
-        if(_healthController.playerHealth > 0)
-        {
-            float horizontal = Input.GetAxisRaw("Horizontal");
-            float vertical = Input.GetAxisRaw("Jump");
-            MoveCharacter(horizontal, vertical);
-            PlayerAnimationMovement(horizontal, vertical);
-            PlayerCrouch();
-        }
-        
-        
-        //Debug.Log("Vertical: " +vertical);
->>>>>>> Stashed changes
 
        
 
@@ -106,16 +44,10 @@ public class PlayerController : MonoBehaviour
         transform.position = position;
 
         //vertical
-       if(vertical > 0 && isPlayerGrounded.isOnGrounded == true)
+       if(vertical > 0)
        {
            rb2d.AddForce(new Vector2(0f, jump), ForceMode2D.Force);
        }
-
-       //FallDeath
-       /*if(isOnDG.isOnDeathGrounded == true)
-       {
-           Application.LoadLevel(0);
-       }*/
     }
     //Player Movement Animation 
     private void PlayerAnimationMovement(float horizontal, float vertical)
@@ -134,7 +66,7 @@ public class PlayerController : MonoBehaviour
         transform.localScale = scale;
 
          //Jump animation along y axis
-        if(vertical > 0 )
+        if(vertical > 0)
         {
             animator.SetBool("Jump", true);
         }
@@ -142,97 +74,5 @@ public class PlayerController : MonoBehaviour
         {
             animator.SetBool("Jump", false);
         }
-<<<<<<< Updated upstream
     }
-=======
-
-       /* //Death if player falling from ground
-        if(transform.position.y < 0)
-        {
-            animator.SetBool("Fall", true);
-        }
-        else
-        {
-            animator.SetBool("Fall", false);
-        }*/
-
-    }
-
-    //Crouch
-
-    private void PlayerCrouch()
-    {
-        if(Input.GetKeyDown(KeyCode.C))
-        {
-
-            // spriteR.sprite = Crouching;
-            animator.SetBool("Crouch", true);
-            collider.size = crouchingSize;
-            collider.offset = crouchingOffset;
-        }
-
-        if(Input.GetKeyUp(KeyCode.C))
-        {
-            animator.SetBool("Crouch", false);
-            //spriteR.sprite = Standing;
-            collider.size = standingSize;
-            collider.offset = sCrouchingOffset;
-        }
-    }
-
-    // //isGrounded
-
-    // private void isGrounded()
-    // {
-    //     public bool isOnGrounded = false;
-    //     private void OnTriggerEnter2D(Collider2D other)
-    //     {
-    //         if(other.gameObject.tag == "floor")
-    //         {
-    //             isOnGrounded = true;
-    //         }
-    //     }
-
-    //     private void OnCollisionExit2D(Collision2D col)
-    //     {
-    //         isOnGrounded = false;
-    //     }
-    // }
-
-
-
-//var falling: boolean = false; // tells when the player is falling private var lastY: float; // last grounded height private var character: CharacterController;
-
-// function Start()
-// { 
-// character = GetComponent(CharacterController); 
-// lastY = transform.position.y; 
-// }
-
-// function Update()
-//     { 
-//         if (character.isGrounded == false)
-//         { 
-//         // if character not grounded... 
-//         falling = true; 
-//         // assume it's falling 
-//         } 
-//         else 
-//         { 
-            // if character grounded... 
-//             if (falling)
-//             { 
-//                 // but was falling last update... 
-//                 var hFall = lastY - transform.position.y; 
-//                 // calculate the fall height... 
-//                 if (hFall > 8)
-//                 { 
-//                     // then check the damage/death 
-//                     // player is dead 
-//                 } 
-//             } 
-//             lastY = transform.position.y; // update lastY when character grounded 
-//         } 
-//     }
->>>>>>> Stashed changes
 }
