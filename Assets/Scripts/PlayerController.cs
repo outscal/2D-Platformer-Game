109fa7581558ace1f73b0f1ee26a9225e0abcfc1
Aviral Goel <aviralgoel@rocketmail.com>
2022-06-06 16:28:30 +0400

    added jump and crouch animation
    collider changes when crouching, jump is based on vertical get axis raw.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
-    public float speed;
+    public BoxCollider2D collider2d;
+    //public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,7 +17,8 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {   
-        float horizontal = Input.GetAxisRaw("Horizontal") * speed;
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        
         animator.SetFloat("Speed", Mathf.Abs(horizontal));
         Vector3 scale = transform.localScale;
         if (horizontal < 0)
@@ -27,6 +30,41 @@ public class PlayerController : MonoBehaviour
             scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
+        CheckForCrouch();
+        CheckForJump();
+
+    }
+
+    private void CheckForJump()
+    {
+        float vertical = Input.GetAxisRaw("Vertical");
+       //if(Input.GetKeyDown(KeyCode.Space))
+        if(vertical > 0)
+        {
+            animator.SetTrigger("JumpTrigger");
+            //animator.SetBool("Jump", true);
+        }
+        /*else
+        {
+            //animator.SetBool("Jump", false);
+        }*/
+    }
+
+    private void CheckForCrouch()
+    {
+     
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            animator.SetBool("Crouch", true);
+            collider2d.size = new Vector2(0.6311399f, 1.327474f);
+            collider2d.offset = new Vector2(0.01034069f, 0.5875177f);
+        }
+        else
+        {
+            animator.SetBool("Crouch", false);
+            collider2d.size = new Vector2(0.6311399f,2.108599f);
+            collider2d.offset = new Vector2(0.01034069f, 0.9780799f);
+        }
 
     }
 }
