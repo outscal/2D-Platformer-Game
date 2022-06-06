95c171683513d719204c94c47bf8403a059777bf
Aviral Goel <aviralgoel@rocketmail.com>
2022-06-07 00:08:06 +0400

    player walks and jump
    added code to make player change its position forward and upward.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,23 +1,49 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
     public BoxCollider2D collider2d;
-    //public float speed;
+    public float speed;
+    private Rigidbody2D rb;
+    public float jumpForce;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        rb = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
-    {   
+    {
         float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Jump");
+        MoveAnimation(horizontal);
+        MoveCharacter(horizontal, vertical);
+        CheckForCrouch();
+        CheckForJump();
+
+    }
+
+    private void MoveCharacter(float horizontal, float vertical)
+    {
+        Vector2 _move = transform.position;
+        _move.x += horizontal * speed * Time.deltaTime;
+        transform.position = _move;
+        // In Contrast to the video, this implementation actually work because the player has an RB
+        // and hence it falls back down to the earth.
+        //_move.y += vertical * jumpHeight * Time.deltaTime;
+        // Just for consistency, using the implementation in the tutorial.
+        if(vertical>0)
+        {
+            rb.AddForce(new Vector2(0,jumpForce), ForceMode2D.Force);
+        }
+
+    }
+
+    private void MoveAnimation(float horizontal)
+    {
         
         animator.SetFloat("Speed", Mathf.Abs(horizontal));
         Vector3 scale = transform.localScale;
@@ -25,23 +51,21 @@ public class PlayerController : MonoBehaviour
         {
             scale.x = (-1) * Mathf.Abs(scale.x);
         }
-        else if(horizontal > 0)
+        else if (horizontal > 0)
         {
             scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
-        CheckForCrouch();
-        CheckForJump();
-
     }
 
     private void CheckForJump()
     {
-        float vertical = Input.GetAxisRaw("Vertical");
-       //if(Input.GetKeyDown(KeyCode.Space))
-        if(vertical > 0)
+        
+       if(Input.GetKeyDown(KeyCode.Space))
+       //if(vertical > 0)
         {
             animator.SetTrigger("JumpTrigger");
+            Debug.Log("Jump Triggered");
             //animator.SetBool("Jump", true);
         }
         /*else
