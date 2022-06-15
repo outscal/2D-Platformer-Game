75514a0aea84f7b59a41dca40f8e44d9018602e3
Aviral Goel <aviralgoel@rocketmail.com>
2022-06-15 04:39:31 +0400

    quit button
    added quit button

--- Assets/Scripts/LobbyController.cs
+++ Assets/Scripts/LobbyController.cs
@@ -2,14 +2,25 @@ using TMPro;
 using UnityEngine.UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System;
 
 public class LobbyController : MonoBehaviour
 {
     public Button playButton;
+    public Button quitButton;
     // Start is called before the first frame update
     void Awake()
     {
         playButton.onClick.AddListener(PlayGame);
+        quitButton.onClick.AddListener(QuitGame);
+    }
+
+    private void QuitGame()
+    {
+    #if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+    #endif
+        Application.Quit();
     }
 
     private void PlayGame()
