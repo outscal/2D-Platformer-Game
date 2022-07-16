647d43b601b467c4c972bcd8b5230b631419757e
praksahb <39479839+praksahb@users.noreply.github.com>
2022-07-16 05:59:25 +0530

    added collectable item key
    Player can collect items via OnTrigger methods.
    Item is destroyed onTriggering with playerController.
    Score ui element adds incremental value to score after item collection.

---
+++ Assets/Scripts/KeyController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class KeyController : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+        
+        if(collider.gameObject.GetComponent<PlayerController>() != null)
+        {
+            PlayerController playerController = (PlayerController)collider.gameObject.GetComponent<PlayerController>();
+            playerController.PickUpKey();
+            Destroy(gameObject);
+        }
+    }
+}
