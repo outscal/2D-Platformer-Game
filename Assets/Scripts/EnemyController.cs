d2521c4604f00db65d50a440318554a24c38b5be
praksahb <39479839+praksahb@users.noreply.github.com>
2022-07-17 12:49:48 +0530

    Added enemy gameObject and death animation for player
    enemy gameObject created with simple collider, on collision player is dead.
    
    death animation for player added.
    
    WrapMode for animation allows stop animation at the last image. Can be accessed via debug mode in unity editor.

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>() != null)
+        {
+            PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
+            playerController.KillPlayer();
+        }
+    }
+}
