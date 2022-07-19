4636ac1ae04f9003f281b78211525225df1eed38
praksahb <39479839+praksahb@users.noreply.github.com>
2022-07-19 08:57:14 +0530

    added quit game button after player dies

--- Assets/Scripts/GameOverController.cs
+++ Assets/Scripts/GameOverController.cs
@@ -1,15 +1,25 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System;
 
 public class GameOverController : MonoBehaviour
 {
     public Button restartButton;
+    public Button quitButton;
 
     public void Awake()
     {
         restartButton.onClick.AddListener(ReloadLevel);
+        quitButton.onClick.AddListener(QuitGame);
     }
+    private void QuitGame()
+    {
+        // Application.Quit will not work inside unity editor
+        UnityEditor.EditorApplication.isPlaying = false;
+        Application.Quit();
+    }
+
     public void LoadGameOverUI()
     {
         gameObject.SetActive(true);
