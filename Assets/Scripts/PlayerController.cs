bcd4b34a8df3e985099579b3f3a216b2ecbc109b
Sahil231 <sahilmalhotra231@gmail.com>
2022-07-23 19:41:50 +0530

    Chapter 2 - Mini Assignment 2 - Scripting Animation & Flipping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,8 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class PlayerController : MonoBehaviour
 {
-    
+    public Animator animator;
+    void Update()
+    {
+        float speed= Input.GetAxis("Horizontal");
+        Debug.Log(speed);
+        animator.SetFloat("Speed",Math.Abs(speed));
+        Vector3 scale=transform.localScale;
+        if(Input.GetAxis("Horizontal")<0)
+        {
+            scale.x= -Math.Abs(transform.localScale.x);
+        }
+        if(Input.GetAxis("Horizontal")>0)
+        {
+            scale.x= Math.Abs(transform.localScale.x);
+        }
+        transform.localScale=scale;
+    }
 }
