873dbc6ef3c553041ce163afd62d9f38711aa23e
Sajan Satheesh <64242770+Sajan-Satheesh@users.noreply.github.com>
2022-09-02 12:38:06 +0530

    C2mA1_2_Scripting anim and flip
    1. Created rigid body for player
    2. created box 2d collider for player and the floor.
    3.Created a player controller script.
    4. Scripted with conditions to play animation if the horizontal axis is activated.
    5. Scripted scale flip to change the direction when the horizontal axis changes.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+
+    
+{
+    public Animator In_Anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Starting Player");
+    }
+    private void Awake()
+    {
+       
+        Debug.Log("Player Awake");
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Player hits " + collision.gameObject.name);
+    }
+
+    
+
+    // Update is called once per frame
+    private void Update()
+    {
+        float speed = Input.GetAxisRaw("Horizontal");
+        In_Anim.SetFloat("speed", Mathf.Abs(speed));
+        Vector3 scale = transform.localScale;
+        if (speed < 0)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        else if (speed>0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
+    }
+}
