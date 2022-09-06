6d96d7a98949a198555124d01a60065a1497ed34
Sajan Satheesh <64242770+Sajan-Satheesh@users.noreply.github.com>
2022-09-06 15:22:46 +0530

    Ch2A1
    1.added crouch animation,
    2.added control to crouch using control key,
    3.resized box collider while crouching.
    4.Control jump using vertical axis.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,10 +7,16 @@ public class PlayerController : MonoBehaviour
     
 {
     public Animator In_Anim;
+    public BoxCollider2D playerCol;
+    
+   
+   
     // Start is called before the first frame update
     void Start()
     {
+        playerCol =playerCol.GetComponent<BoxCollider2D>();
         Debug.Log("Starting Player");
+        
     }
     private void Awake()
     {
@@ -27,7 +33,7 @@ public class PlayerController : MonoBehaviour
     
 
     // Update is called once per frame
-    private void Update()
+    public void Update()
     {
         float speed = Input.GetAxisRaw("Horizontal");
         In_Anim.SetFloat("speed", Mathf.Abs(speed));
@@ -41,5 +47,38 @@ public class PlayerController : MonoBehaviour
             scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
+
+        float up = Input.GetAxisRaw("Vertical");
+        bool jump = false;
+        if (up > 0)
+        {
+            jump = true;
+        }
+        In_Anim.SetBool("jump", jump);
+
+        
+        
+
+        
+        bool pressCtrl = Input.GetKeyDown(KeyCode.LeftControl);
+        bool releaseCtrl = Input.GetKeyUp(KeyCode.LeftControl);
+        Vector2 size = playerCol.size;
+        Vector2 offse = playerCol.offset;
+        if (pressCtrl) {
+            
+            In_Anim.SetBool("crouch", true);
+            /*Debug.Log(oldsize);*/
+            playerCol.size = new Vector2(size.x,0.6f*size.y) ;
+            playerCol.offset = new Vector2(offse.x, 0.6f * offse.y);
+        }
+        else if(releaseCtrl)
+        {
+            In_Anim.SetBool("crouch", false);
+            /*Debug.Log(oldsize);*/
+            playerCol.size = new Vector2(size.x, 2.085643f);
+            playerCol.offset = new Vector2(offse.x, 0.9831027f);
+        }
+        
+
     }
 }
