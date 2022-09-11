2bbc2f971c64eb41dd174de0cfd3390a0e25d01c
Sam Blesswin <samblesswinedu@gmail.com>
2022-09-11 21:15:49 +0530

    [Added] Crouch and Jump
    Crouch and Jump animations are played based on User Input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,23 +5,74 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]private Animator animator;
+    [SerializeField]private BoxCollider2D boxCollider;
+
+    public Vector2 standingColliderOffset;
+    public Vector2 standingColliderSize;
+    public Vector2 crouchColliderOffset;
+    public Vector2 crouchColliderSize;
+
+    private void Start()
+    {
+        CrouchHitBox(false);
+    }
 
     void Update()
     {
         float xAxis = Input.GetAxisRaw("Horizontal");
+        PlayRunAnimation(xAxis);
+
+        if(Input.GetKey(KeyCode.LeftControl))
+        {
+            animator.SetBool("Crouch",true);
+            CrouchHitBox(true);
+        }
+        else
+        {
+            animator.SetBool("Crouch", false);
+            CrouchHitBox(false);
+        }
+
+        float yAxis = Input.GetAxisRaw("Vertical");
+        if(yAxis >0)
+        {
+           animator.SetTrigger("Jump"); 
+        }
+        else
+        {
+            animator.ResetTrigger("Jump");
+        }
+    }
+
+    private void PlayRunAnimation(float xAxis)
+    {
         animator.SetFloat("Speed", Mathf.Abs(xAxis));
 
         Vector3 scale = transform.localScale;
 
-        if (xAxis <0)
+        if (xAxis < 0)
         {
             scale.x = -1 * Mathf.Abs(scale.x);
         }
-        else if(xAxis > 0)
+        else if (xAxis > 0)
         {
             scale.x = Mathf.Abs(scale.x);
         }
 
         transform.localScale = scale;
+    }
+
+    private void CrouchHitBox(bool crouch)
+    {
+        if(crouch)
+        {
+            boxCollider.offset = crouchColliderOffset;
+            boxCollider.size = crouchColliderSize;
+        }
+        else
+        {
+            boxCollider.offset = standingColliderOffset;
+            boxCollider.size = standingColliderSize;
+        }
     }
 }
