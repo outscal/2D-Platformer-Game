1ecb76120471a4e006b5185b801dd7a1f30f9261
Sajan Satheesh <64242770+Sajan-Satheesh@users.noreply.github.com>
2022-09-19 17:09:30 +0530

    Ch4A1-Death after falling from platform

---
+++ Assets/Scripts/PlayerDead.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerDead : MonoBehaviour
+{
+    public Transform player;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = player.position.x;
+        transform.position = new Vector2(x, transform.position.y);
+    }
+}
