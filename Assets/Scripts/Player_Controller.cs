7d270b5073406f7654dcc467a3b25c19a85e7ae6
mukundaDev <94097024+mukundaDev@users.noreply.github.com>
2022-09-29 00:24:58 +0530

    PlayerController
    Adding Script and Components to the Player.

---
+++ Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+
+    public Animator animator;
+
+    private void Update()
+    {
+        float Speed =  Input.GetAxis("Horizontal");
+        animator.SetFloat("speed", Speed);
+    }
+
+}
