1bc5e7b9cdfdb0ad11186e5ecf0dbf87481483bf
mukundaDev <94097024+mukundaDev@users.noreply.github.com>
2022-09-29 15:01:15 +0530

    Crouch
    Crouch method Implemented in playercontroller script.

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -6,12 +6,18 @@ public class Player_Controller : MonoBehaviour
 {
 
     public Animator animator;
+    public bool jumpSpeed;
 
     private void Update()
     {
-        float Speed =  Input.GetAxis("Horizontal");
+        float Speed =  Input.GetAxisRaw("Horizontal");
         animator.SetFloat("speed", Mathf.Abs(Speed));
-
+       
+      
+            Crouch();
+        
+        
+        
         Vector3 scale = transform.localScale;
         if (Speed < 0)
         { 
@@ -23,5 +29,16 @@ public class Player_Controller : MonoBehaviour
         }
         transform.localScale = scale;
     }
+    void Crouch()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            animator.SetBool("crouch", true);
 
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            animator.SetBool("crouch", false);
+        }
+    }
 }
