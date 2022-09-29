3a6a470055d3b7264d7bfdbb825dae9214d1f561
mukundaDev <94097024+mukundaDev@users.noreply.github.com>
2022-09-30 01:55:44 +0530

    Jump
    Jump Animation by checking if player is in ground or not.

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -4,37 +4,66 @@ using UnityEngine;
 
 public class Player_Controller : MonoBehaviour
 {
-
+    private Rigidbody2D rb2d;
     public Animator animator;
     
-    public bool jumpSpeed;
+    public float jump;
+    private bool _isGrounded;
     [SerializeField]
-    private float playerSpeed;
+    private float _playerSpeed;
 
+
+    private void Awake()
+    {
+        rb2d = gameObject.GetComponent<Rigidbody2D>();
+    }
     private void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
+        float vertical = Input.GetAxis("Vertical");
+        
         MoveCharecter(horizontal);
         MovementAnimation(horizontal);
-        Jump(verticalInput);
+        MoveVertically(vertical);
         Crouch();
     }
     void MoveCharecter(float horizontal)
     {
         Vector3 position = transform.position;
-        position.x += horizontal * playerSpeed * Time.deltaTime;
+        position.x += horizontal * _playerSpeed * Time.deltaTime;
         transform.position = position;
     }
 
+    private void MoveVertically(float vertical)
+    {
+       
+        if (vertical > 0 && _isGrounded)
+        {
+            animator.SetBool("jump", true);
+            rb2d.AddForce(new Vector2(0f, jump), ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D other)
+    {
+        if (other.transform.tag == "platform")
+        {
+            _isGrounded = true;
+        }
+        
+    }
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if(other.transform.tag == "platform")
+        {
+            _isGrounded = false;
+        }
+    }
     private void MovementAnimation(float horizontal)
     {
         animator.SetFloat("speed", Mathf.Abs(horizontal));
-
-        //Jump(verticalInput);
-        Crouch();
-
+        
+        //move and flip
         Vector3 scale = transform.localScale;
         if (horizontal < 0)
         {
@@ -57,13 +86,6 @@ public class Player_Controller : MonoBehaviour
         else if (Input.GetKeyUp(KeyCode.LeftControl))
         {
             animator.SetBool("crouch", false);
-        }
-    }
-    void Jump(float verticalInput)
-    {
-        if (verticalInput > 0)
-        {
-         animator.SetTrigger("jump");
         }
     }
 }
