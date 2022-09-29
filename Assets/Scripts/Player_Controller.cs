36d40fb3cb061806024b5326b486e038a0c908fc
mukundaDev <94097024+mukundaDev@users.noreply.github.com>
2022-09-30 00:49:25 +0530

    Movement
    Player movement on platform

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -6,29 +6,47 @@ public class Player_Controller : MonoBehaviour
 {
 
     public Animator animator;
+    
     public bool jumpSpeed;
+    [SerializeField]
+    private float playerSpeed;
 
     private void Update()
     {
-        float Speed =  Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("speed", Mathf.Abs(Speed));
-       
-      
-            Crouch();
-        
-        
-        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        MoveCharecter(horizontal);
+        MovementAnimation(horizontal);
+        Jump(verticalInput);
+        Crouch();
+    }
+    void MoveCharecter(float horizontal)
+    {
+        Vector3 position = transform.position;
+        position.x += horizontal * playerSpeed * Time.deltaTime;
+        transform.position = position;
+    }
+
+    private void MovementAnimation(float horizontal)
+    {
+        animator.SetFloat("speed", Mathf.Abs(horizontal));
+
+        //Jump(verticalInput);
+        Crouch();
+
         Vector3 scale = transform.localScale;
-        if (Speed < 0)
-        { 
+        if (horizontal < 0)
+        {
             scale.x = -1f * Mathf.Abs(scale.x);
         }
-        else if (Speed > 0)
+        else if (horizontal > 0)
         {
             scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
     }
+
     void Crouch()
     {
         if (Input.GetKeyDown(KeyCode.LeftControl))
@@ -39,6 +57,13 @@ public class Player_Controller : MonoBehaviour
         else if (Input.GetKeyUp(KeyCode.LeftControl))
         {
             animator.SetBool("crouch", false);
+        }
+    }
+    void Jump(float verticalInput)
+    {
+        if (verticalInput > 0)
+        {
+         animator.SetTrigger("jump");
         }
     }
 }
