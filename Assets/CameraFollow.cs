2a0c6c1b0c90ec04bd438510961ebf083084b1d1
Yash Chaudhari <chaudhariyash723@gmail.com>
2022-10-16 01:47:35 +0530

    commit changes

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Vector3 offset = new Vector3(0f, 0f, -10f);
+    private float smoothTime = 0.25f;
+    private Vector3 velocity = Vector3.zero;
+
+    [SerializeField] private Transform Target;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 targetPosition = transform.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+    }
+    
+}
---
+++ Assets/Script/Charat.cs
@@ -0,0 +1,191 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Charat : MonoBehaviour
+{
+    Rigidbody2D rb;
+    Animator animator;
+    [SerializeField] Collider2D standingCollider;
+    [SerializeField] Transform groundCheckCollider;
+    [SerializeField] Transform overheadCheckCollider;
+    [SerializeField] LayerMask groundLayer;
+
+    const float groundCheckRadius = 0.2f;
+    const float overheadCheckRadius = 0.2f;
+    [SerializeField] float speed = 2;
+    [SerializeField] float jumpPower = 500;
+    [SerializeField] int totalJumps;
+    int availableJumps;
+    float horizontalValue;
+    float runSpeedModifier = 2f;
+    float crouchSpeedModifier = 0.5f;
+
+    [SerializeField] bool isGrounded;
+    bool isRunning;
+    bool facingRight = true;
+    bool crouchPressed;
+    bool multipleJump;
+    bool coyoteJump;
+
+    void Awake()
+    {
+        availableJumps = totalJumps;
+
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        //Store the horizontal value
+        horizontalValue = Input.GetAxisRaw("Horizontal");
+
+        //If LShift is clicked enable isRunning
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+            isRunning = true;
+        //If LShift is released disable isRunning
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+            isRunning = false;
+
+        //If we press Jump button enable jump 
+        if (Input.GetButtonDown("Jump"))
+            Jump();
+
+        //If we press Crouch button enable crouch 
+        if (Input.GetButtonDown("Crouch"))
+        {
+            animator.SetBool("Crouch", true);
+            crouchPressed = true;
+        }
+        
+       
+        //Otherwise disable it
+        else if (Input.GetButtonUp("Crouch"))
+        {
+            crouchPressed = false;
+        animator.SetBool("Crouch", false);
+        }
+            
+
+        //Set the yVelocity Value
+        animator.SetFloat("yVelocity", rb.velocity.y);
+    }
+
+    void FixedUpdate()
+    {
+        GroundCheck();
+        Move(horizontalValue, crouchPressed);
+    }
+
+    void GroundCheck()
+    {
+        bool wasGrounded = isGrounded;
+        isGrounded = false;
+        //Check if the GroundCheckObject is colliding with other
+        //2D Colliders that are in the "Ground" Layer
+        //If yes (isGrounded true) else (isGrounded false)
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheckCollider.position, groundCheckRadius, groundLayer);
+        if (colliders.Length > 0)
+        {
+            isGrounded = true;
+            if (!wasGrounded)
+            {
+                availableJumps = totalJumps;
+                multipleJump = false;
+            }
+        }
+        else
+        {
+            if (wasGrounded)
+                StartCoroutine(CoyoteJumpDelay());
+        }
+
+        //As long as we are grounded the "Jump" bool
+        //in the animator is disabled
+        animator.SetBool("Jump", !isGrounded);
+    }
+
+    IEnumerator CoyoteJumpDelay()
+    {
+        coyoteJump = true;
+        yield return new WaitForSeconds(0.2f);
+        coyoteJump = false;
+    }
+
+    void Jump()
+    {
+        if (isGrounded)
+        {
+            multipleJump = true;
+            availableJumps--;
+
+            rb.velocity = Vector2.up * jumpPower;
+            animator.SetBool("Jump", true);
+        }
+        else
+        {
+            if (coyoteJump)
+            {
+                multipleJump = true;
+                availableJumps--;
+
+                rb.velocity = Vector2.up * jumpPower;
+                animator.SetBool("Jump", true);
+            }
+
+            if (multipleJump && availableJumps > 0)
+            {
+                availableJumps--;
+
+                rb.velocity = Vector2.up * jumpPower;
+                animator.SetBool("Jump", true);
+            }
+        }
+    }
+
+    void Move(float dir, bool crouchFlag)
+    {
+        #region Crouch
+
+        //If we are crouching and disabled crouching
+        //Check overhead for collision with Ground items
+        //If there are any, remain crouched, otherwise un-crouch
+       
+
+        #endregion
+
+        #region Move & Run
+        //Set value of x using dir and speed
+        float xVal = dir * speed * 100 * Time.fixedDeltaTime;
+        //If we are running mulitply with the running modifier (higher)
+        if (isRunning)
+            xVal *= runSpeedModifier;
+        //If we are running mulitply with the running modifier (higher)
+        if (crouchFlag)
+            xVal *= crouchSpeedModifier;
+        //Create Vec2 for the velocity
+        Vector2 targetVelocity = new Vector2(xVal, rb.velocity.y);
+        //Set the player's velocity
+        rb.velocity = targetVelocity;
+
+        //If looking right and clicked left (flip to the left)
+        if (facingRight && dir < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            facingRight = false;
+        }
+        //If looking left and clicked right (flip to rhe right)
+        else if (!facingRight && dir > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1);
+            facingRight = true;
+        }
+
+        //(0 idle , 4 walking , 8 running)
+        //Set the float xVelocity according to the x value 
+        //of the RigidBody2D velocity 
+        animator.SetFloat("xVelocity", Mathf.Abs(rb.velocity.x));
+        #endregion
+    }
+}
