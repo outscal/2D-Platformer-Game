1949f4a4bb412f463ebe8ccaf7f8f9121bf927e9
Annu769 <109024549+Annu769@users.noreply.github.com>
2022-10-20 17:51:57 +0530

    Add Level UI

---
+++ Assets/Scripts/Level Loader/PlayLevel.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayLevel : MonoBehaviour
+{
+   [SerializeField] private Button button;
+    [SerializeField] GameObject LevelSlectionPanel;
+    void Awake()
+    {
+        LevelSlectionPanel.SetActive(false);
+        button.onClick.AddListener(OpenPanel);
+
+    }
+
+   
+    private void OpenPanel()
+    {
+        LevelSlectionPanel.SetActive(true);
+    }
+}
