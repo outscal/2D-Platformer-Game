d9707d73becb842937ebc7041751d6fdc8362ced
Surajn7 <105142430+Surajn7@users.noreply.github.com>
2022-10-27 11:54:50 +0530

    Jump & Crounch Commit
    Jump and Crounch has been added to the player object

--- Assets/SCripts/Player_Script.cs
+++ Assets/SCripts/Player_Script.cs
@@ -11,11 +11,17 @@ public class Player_Script : MonoBehaviour
         float speed = Input.GetAxisRaw("Horizontal");
         animator.SetFloat("SPEED", Mathf.Abs(speed));
 
+        float Jump = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("JUMP", Jump);
+
+        bool Crounch = Input.GetKey(KeyCode.LeftControl);
+        animator.SetBool("CROUNCH", Crounch);
+
         Vector3 scale = transform.localScale;
         if (speed < 0)
-            scale.x = -1f * Mathf.Abs(scale.x);
+        scale.x = -1f * Mathf.Abs(scale.x);  
         else if (speed > 0)
-            scale.x= Mathf.Abs(scale.x);
+        scale.x = Mathf.Abs(scale.x);
 
         transform.localScale = scale;
     }
