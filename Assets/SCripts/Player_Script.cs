e1ad5e655458ada6039b1ea6ad4366b47a400327
Surajn7 <105142430+Surajn7@users.noreply.github.com>
2022-10-31 00:16:21 +0530

    2nd commit

--- Assets/SCripts/Player_Script.cs
+++ Assets/SCripts/Player_Script.cs
@@ -9,14 +9,16 @@ public class Player_Script : MonoBehaviour
     public float speed;
     private void Update()
     {
-        float Horizontal = Input.GetAxisRaw("Horizontal");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        bool Jump = Input.GetKey(KeyCode.Space);
         bool Crouch = Input.GetKey(KeyCode.LeftControl);
-
-        Player_Horizontal(Horizontal);
+       
+        Player_Horizontal(horizontal,Jump);
         Player_Crouch(Crouch);
+        Player_move(horizontal);
     }
 
-    private void Player_Horizontal(float horizontal)
+    private void Player_Horizontal(float horizontal,bool vertical)
     {
         animator.SetFloat("SPEED", Mathf.Abs(horizontal));
         Vector3 scale = transform.localScale;
@@ -24,9 +26,21 @@ public class Player_Script : MonoBehaviour
             scale.x = -1f * Mathf.Abs(scale.x);
         else if (horizontal > 0)
             scale.x = Mathf.Abs(scale.x);
+
         transform.localScale = scale;
+
+        if (vertical == true)
+            animator.SetBool("JUMP", vertical);
+        else
+            animator.SetBool("JUMP", false);
     }
 
+    private void Player_move(float horizontal)
+    {
+        Vector3 position = transform.position;
+        position.x = position.x + horizontal * speed * Time.deltaTime;
+        transform.position = position;
+    }
     private void Player_Crouch(bool Crnch)
     {
         if (Crnch == true)
