7b5440ba6b80469babae5456058997c687703eea
Surajn7 <105142430+Surajn7@users.noreply.github.com>
2022-10-31 14:55:26 +0530

    Vertical Movement
    Vertical Movement

--- Assets/SCripts/Player_Script.cs
+++ Assets/SCripts/Player_Script.cs
@@ -5,20 +5,24 @@ using UnityEngine;
 public class Player_Script : MonoBehaviour
 {
     public Animator animator;
-
+    public Rigidbody2D rbdy;
     public float speed;
+    public float Jump;
+
     private void Update()
     {
         float horizontal = Input.GetAxisRaw("Horizontal");
-        bool Jump = Input.GetKey(KeyCode.Space);
+        float vertical = Input.GetAxisRaw("Jump");
         bool Crouch = Input.GetKey(KeyCode.LeftControl);
        
-        Player_Horizontal(horizontal,Jump);
+        Player_Horizontal(horizontal,vertical);
         Player_Crouch(Crouch);
-        Player_move(horizontal);
+        Player_move(horizontal,vertical);
+
+        rbdy = gameObject.GetComponent<Rigidbody2D>();
     }
 
-    private void Player_Horizontal(float horizontal,bool vertical)
+    private void Player_Horizontal(float horizontal,float vertical)
     {
         animator.SetFloat("SPEED", Mathf.Abs(horizontal));
         Vector3 scale = transform.localScale;
@@ -29,17 +33,22 @@ public class Player_Script : MonoBehaviour
 
         transform.localScale = scale;
 
-        if (vertical == true)
-            animator.SetBool("JUMP", vertical);
+        if (vertical>0)
+            animator.SetBool("JUMP", true);
         else
             animator.SetBool("JUMP", false);
     }
 
-    private void Player_move(float horizontal)
+    private void Player_move(float horizontal,float vertical)
     {
         Vector3 position = transform.position;
         position.x = position.x + horizontal * speed * Time.deltaTime;
         transform.position = position;
+
+        if(vertical>0)
+        {
+            rbdy.AddForce(new Vector2(0f,Jump), ForceMode2D.Force);
+        }
     }
     private void Player_Crouch(bool Crnch)
     {
