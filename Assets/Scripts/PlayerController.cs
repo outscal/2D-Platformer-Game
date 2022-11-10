0fe1b77a811de42c15bf4030c0174d341a3869e5
Abhishek Kadam <kadamabhishek981111@gmail.com>
2022-11-10 23:17:39 +0530

    Scripting Animation & Flipping
    Accept horizontal input from the user, play run animation and flip player according to the horizontal input.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,13 +4,31 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Animator animator;
     private void Awake()
     {
         Debug.Log("Player controller awake");
     }
     
-    private void OnCollisionEnter2D(Collision2D collision)
+    // private void OnCollisionEnter2D(Collision2D collision)
+    // {
+    //     Debug.Log("Collision: " + collision.gameObject.name);
+    // }
+
+    private void Update()
     {
-        Debug.Log("Collision: " + collision.gameObject.name);
+        float speed = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs(speed));
+
+        Vector3 scale = transform.localScale;
+        if(speed < 0)
+        {
+            scale.x = -1 * Mathf.Abs(scale.x);
+        }
+        else if(speed > 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
     }
 }
