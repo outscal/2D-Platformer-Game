44ab9c519010463e1c48789a5f48947756efb818
Abhishek Kadam <kadamabhishek981111@gmail.com>
2022-11-15 12:01:13 +0530

    added crouch and jump animation with animator script
    added crouch and jump animation with animator script for playing respective animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -30,5 +30,41 @@ public class PlayerController : MonoBehaviour
             scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
+
+    //     for jump animation 
+    //    if(Input.GetButtonDown("Jump"))
+    //    {
+    //         float verticalSpeed = Input.GetAxisRaw("Vertical");
+    //         animator.SetBool("Jump", true);
+
+    //         Vector3 verticalPos = transform.position;
+    //         if(verticalSpeed > 0)
+    //         {
+    //             verticalPos.y = Mathf.Abs(verticalPos.y);
+    //         }
+    //         transform.position = verticalPos;
+    //    }
+    //    else if (Input.GetButtonUp("Jump"))
+    //    {
+    //         animator.SetBool("Jump", false);
+    //    }
+    
+
+    //another way for jump 
+        float verticalSpeed = Input.GetAxisRaw("Vertical");
+
+        Vector3 verticalPos = transform.position;
+        if(verticalSpeed > 0)
+        {
+            animator.SetBool("Jump", true);
+            verticalPos.y = Mathf.Abs(verticalPos.y);
+        }  
+        else if (verticalSpeed < 0 )
+        {
+            animator.SetBool("Jump", false);
+            animator.SetBool("Crouch", true);
+        }
+        transform.position = verticalPos;
+        //animator.SetBool("Crouch", false);
     }
 }
