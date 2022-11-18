97322645d782561c10654e4f3919a013a1ac39f3
rahulpargi641 <rahulpargi641@gmail.com>
2022-11-18 12:35:31 +0530

    Added PlayerCrouch and PlayerJump function in the PlayerController script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,9 +5,30 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
-   
+    public BoxCollider2D boxcollider;
+    Vector2 size;
+    Vector2 offset;
+    private void Awake()
+    {
+        size = boxcollider.size;
+        offset = boxcollider.offset;
+    }
+
+
     // Update is called once per frame
     void Update()
+    {
+        PlayerMovement();
+    }
+
+    private void PlayerMovement()
+    {
+        PlayerFlip();
+        PlayerCrouch();
+        PlayerJump();
+    }
+
+    private void PlayerFlip()
     {
         float speed = Input.GetAxisRaw("Horizontal");
         animator.SetFloat("Speed", Mathf.Abs(speed));
@@ -22,5 +43,27 @@ public class PlayerController : MonoBehaviour
         }
 
         transform.localScale = scale;
+    }
+
+    private void PlayerJump()
+    {
+        float speedY = Input.GetAxisRaw("Vertical");
+        animator.SetFloat("Jump", Mathf.Abs(speedY));
+    }
+
+    private void PlayerCrouch()
+    {
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            animator.SetBool("Crouch", true);
+            boxcollider.size = new Vector2(size.x, size.y / 2);
+            boxcollider.offset = new Vector2(offset.x, offset.y - offset.y / 2);
+        }
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            boxcollider.size = size;
+            boxcollider.offset = offset;
+            animator.SetBool("Crouch", false);
+        }
     }
 }
