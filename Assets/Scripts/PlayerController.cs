20b348226f9851e87a3808b6dc6bc9e05fabd5fd
rahulpargi641 <rahulpargi641@gmail.com>
2022-11-18 15:18:41 +0530

    Added PlayerMovement code

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,8 @@ public class PlayerController : MonoBehaviour
 {
     public Animator animator;
     public BoxCollider2D boxcollider;
+    public float speed;
+    public float jump;
     Vector2 size;
     Vector2 offset;
     private void Awake()
@@ -18,26 +21,35 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        PlayerMovement();
+        float speedX = Input.GetAxisRaw("Horizontal");
+        float speedY = Input.GetAxisRaw("Vertical");
+        PlayerMovement(speedX, speedY);
+        PlayerMovementAnimation(speedX, speedY);
     }
 
-    private void PlayerMovement()
+    private void PlayerMovement(float speedX, float speedY)
     {
-        PlayerFlip();
+        PlayerHorizontalMovement(speedX);
+        PlayerJump(speedY);
         PlayerCrouch();
-        PlayerJump();
     }
 
-    private void PlayerFlip()
+    private void PlayerHorizontalMovement(float speedX)
     {
-        float speed = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("Speed", Mathf.Abs(speed));
+        Vector3 position = transform.position;
+        position.x = transform.position.x + speedX * Time.deltaTime;
+        transform.position = position;
+    }
+
+    private void PlayerMovementAnimation(float speedX, float speedY)
+    {
+        animator.SetFloat("Speed", Mathf.Abs(speedX));
         Vector3 scale = transform.localScale;
-        if (speed < 0)
+        if (speedX < 0)
         {
             scale.x = -1f * Mathf.Abs(scale.x);
         }
-        else if (speed > 0)
+        else if (speedX > 0)
         {
             scale.x = Mathf.Abs(scale.x);
         }
@@ -45,10 +57,18 @@ public class PlayerController : MonoBehaviour
         transform.localScale = scale;
     }
 
-    private void PlayerJump()
+    private void PlayerJump(float speedY)
     {
-        float speedY = Input.GetAxisRaw("Vertical");
-        animator.SetFloat("Jump", Mathf.Abs(speedY));
+        if(speedY > 0)
+        {
+            animator.SetBool("Jump", true);
+        }
+        else
+        {
+            animator.SetBool("Jump", false);
+        }
+        
+        
     }
 
     private void PlayerCrouch()
