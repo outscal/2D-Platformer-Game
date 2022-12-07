8742fb5c03892085e8b55acef736f9ed676ef91f
Abhishek Kadam <kadamabhishek981111@gmail.com>
2022-12-07 23:48:53 +0530

    Added key going Up-Fade out animation

--- Assets/Scripts/KeyController.cs
+++ Assets/Scripts/KeyController.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class KeyController : MonoBehaviour
 {
+    public Animator animator;
+    //Oncollision Method to Detect the collision between Key and Player
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.GetComponent<PlayerController>() != null)
         {
+            animator.SetTrigger("KeyFadeTrigger");
             PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
             playerController.PickUpKey();
-            Destroy(gameObject);
+            Destroy(gameObject, 0.6f);
         }
     }
 }
