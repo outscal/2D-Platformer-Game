c8179f36359717f2333a0d97689cb22b37acd02e
shreyashap <107754587+shreyashap@users.noreply.github.com>
2023-01-19 10:50:13 +0530

    Revert "Added multiple levels"
    This reverts commit 4ffae50c80dce19527c2af9e238e0e7a0f4e3f60.

--- Assets/Scripts/LevelLoader.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-
-public class LevelLoader : MonoBehaviour
-{
-   public int levelNumber;
-}
