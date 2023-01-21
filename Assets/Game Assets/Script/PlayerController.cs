f52ab83ffba22308474ecdefe5b7390f0e0d0940
i_rk_jain <svsravikiran.9@gmail.com>
2023-01-22 00:59:40 +0530

    MiniAss2 Done

--- Assets/Game Assets/Script/PlayerController.cs
+++ Assets/Game Assets/Script/PlayerController.cs
@@ -2,4 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour { }
+public class PlayerController : MonoBehaviour {
+
+    public Animator animator;
+    public SpriteRenderer spriteRenderer;
+
+    private void Update() {
+        float speed = Input.GetAxisRaw("Horizontal");
+
+        // joystick works amazingly now
+        animator.SetFloat("speed", Mathf.Abs(speed * 10));
+
+        // flipping the sprite
+        if (speed > 0) {
+            spriteRenderer.flipX = false;
+        } else if (speed < 0) {
+            spriteRenderer.flipX = true;
+        }
+
+
+    }
+
+}
