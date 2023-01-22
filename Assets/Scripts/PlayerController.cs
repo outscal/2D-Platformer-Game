2df424a1dccb3b936f284f231fb256a62f994d8e
RohithKannan <51419779+RohithKKannan@users.noreply.github.com>
2023-01-23 00:04:15 +0530

    Player Animations bound to Input

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,4 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Animator animator;
+    void Update()
+    {
+        float speed = Input.GetAxisRaw("Horizontal");
+        Vector3 scale = transform.localScale;
+        if (speed < 0)
+            scale.x = -1 * Mathf.Abs(scale.x);
+        else if (speed > 0)
+            scale.x = Mathf.Abs(scale.x);
+        animator.SetFloat("speed", Mathf.Abs(speed));
+        transform.localScale = scale;
+    }
 }
