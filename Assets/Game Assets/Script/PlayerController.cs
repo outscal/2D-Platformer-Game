dae5380fb995f5400ae47d3f644639bdc9270003
i_rk_jain <svsravikiran.9@gmail.com>
2023-01-23 21:53:52 +0530

    MainAss1 Done

--- Assets/Game Assets/Script/PlayerController.cs
+++ Assets/Game Assets/Script/PlayerController.cs
@@ -6,18 +6,42 @@ public class PlayerController : MonoBehaviour {
 
     public Animator animator;
     public SpriteRenderer spriteRenderer;
+    public Collider2D collider_stand;
+    public Collider2D collider_crouch;
 
     private void Update() {
-        float speed = Input.GetAxisRaw("Horizontal");
 
-        // joystick works amazingly now
-        animator.SetFloat("speed", Mathf.Abs(speed * 10));
+        // if crouch
+        if (Input.GetButton("Crouch")) {
+            animator.SetBool("crouch", true);
 
-        // flipping the sprite
-        if (speed > 0) {
-            spriteRenderer.flipX = false;
-        } else if (speed < 0) {
-            spriteRenderer.flipX = true;
+            collider_stand.enabled = false;
+            collider_stand.enabled = true;
+
+        } else {
+            animator.SetBool("crouch", false);
+            collider_stand.enabled = true;
+            collider_stand.enabled = false;
+
+            // if jump - works for joystick?
+            if (Input.GetButton("Jump")) {
+                animator.SetBool("jump", true);
+
+            } else {
+                animator.SetBool("jump", false);
+
+                float speed = Input.GetAxisRaw("Horizontal");
+
+                // joystick works amazingly now
+                animator.SetFloat("speed", Mathf.Abs(speed * 10));
+
+                // flipping the sprite
+                if (speed > 0) {
+                    spriteRenderer.flipX = false;
+                } else if (speed < 0) {
+                    spriteRenderer.flipX = true;
+                }
+            }
         }
 
 
