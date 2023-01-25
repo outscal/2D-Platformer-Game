95ed7c8aec152c300fc9dafa9ef19be223a9bb1b
destructodroid <omkaryel@gmail.com>
2023-01-25 18:28:57 +0530

    PlayerController
    Added PlayerController Script

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{   
+    public Animator animator;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        Debug.Log("Collision " + collision.gameObject.name);
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float speed = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs(speed));
+        Vector3 scale = transform.localScale;
+        if (speed < 0)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        else if (speed > 0)
+        {
+          scale.x = Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
+    }
+}
