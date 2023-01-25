63b0569d0e3faea42448f51839b73514142bc051
admiralxeon <sanjit.kulkarni.2018@gmail.com>
2023-01-25 13:15:11 +0530

    Chapter 2 Assignment 2
    Scripting animation
    Flipping Sprite

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Animator animator;
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        float speed = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs (speed));
+        Vector3 scale = transform.localScale;
+        if (speed <0)
+        {
+           
+            scale.x = -1f ;
+        }else if (speed > 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
     }
 }
