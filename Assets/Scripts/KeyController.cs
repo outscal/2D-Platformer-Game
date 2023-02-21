6300dc7788ff7ba4ef2070dc04e6dd152674dac6
Bhawesh Agarwal <bhaweshagarwal02@gmail.com>
2023-02-21 13:59:40 +0530

    Update KeyController.cs

--- Assets/Scripts/KeyController.cs
+++ Assets/Scripts/KeyController.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class KeyController : MonoBehaviour
 {
-    [SerializeField]
-    private PlayerControler playerController;
     
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.GetComponent<PlayerControler>() != null)
         {
             Debug.Log("Player got a key");
-            playerController.IncreaseScore(10);
+            collision.gameObject.GetComponent<PlayerControler>().IncreaseScore(10);
             Destroy(gameObject);
         }
     }
