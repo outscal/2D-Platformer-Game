40537b27c0121ab5293af7e6aa39c57529d7aed2
Ashish Verma <ashv6561@gmail.com>
2023-02-26 20:11:03 +0530

    Fixed Animation Issue by correcting Any State transition Parameters.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -13,33 +13,23 @@ public class PlayerController : MonoBehaviour
         bc2d = GetComponent<BoxCollider2D>();
     }
 
-    private void Update() {
-        // Change HorizontalDirection according to input.
-        float direction = Input.GetAxisRaw("Horizontal");
-        // Set Speed veriable defined in Animator Parameters.
-        animator.SetFloat("speed", Mathf.Abs(direction));
-        if (direction != 0) {
-            sr.flipX = (direction > 0) ? false : true;
-        }
-
-        // Jump Animation on Space
-        if (Input.GetKeyDown(KeyCode.Space)) {
-            if (!animator.GetBool("Jump"))
-                animator.SetBool("Jump", true);
+    private void AnimateJump(float vertical) {
+        if (vertical > 0) {
+            animator.SetBool("Jump", true);
         } else {
             animator.SetBool("Jump", false);
         }
-        
+    }
 
-        // Crouch on C
+    private void AnimateCrouch() {
         if (Input.GetKeyDown(KeyCode.C)) {
             // Constant Values Obtained Manually
             float CrouchOffsetX = 0.002846733f;
             float CrouchOffsetY = 0.5938945f;
             float CrouchSizeX = 0.4671042f;
             float CrouchSizeY = 1.247203f;
-            if (!animator.GetBool("Crouch"))
-                animator.SetBool("Crouch", true);
+
+            animator.SetBool("Crouch", true);
             bc2d.size = new Vector2(CrouchSizeX, CrouchSizeY);
             bc2d.offset = new Vector2(CrouchOffsetX, CrouchOffsetY);
         } else {
@@ -52,5 +42,26 @@ public class PlayerController : MonoBehaviour
             bc2d.size = new Vector2(OriginalSizeX, OriginalSizeY);
             bc2d.offset = new Vector2(OriginalOffsetX, OriginalOffsetY);
         }
+    }
+
+    private void SetPlayerHorizontalMovement(float horizontal) {
+        // Set Speed variable defined in Animator Parameters.
+        animator.SetFloat("speed", Mathf.Abs(horizontal));
+        if (horizontal != 0) {
+            sr.flipX = (horizontal > 0) ? false : true;
+        }
+    }
+
+    private void Update() {
+        // Change HorizontalDirection according to input.
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Jump");
+        SetPlayerHorizontalMovement(horizontal);
+
+        // Jump Animation
+        AnimateJump(vertical);
+
+        // Crouch on C
+        AnimateCrouch();
     }
 }
