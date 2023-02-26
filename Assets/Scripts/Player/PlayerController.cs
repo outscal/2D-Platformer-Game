d06e1c510e91a412fca1097ddb935af52c2df671
Ashish Verma <ashv6561@gmail.com>
2023-02-26 18:05:44 +0530

    Added Crouch + Jump Animations.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,20 +4,53 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    BoxCollider2D bc2d;
     Animator animator;
     SpriteRenderer sr;
     private void Awake() {
         animator = GetComponent<Animator>();
         sr = GetComponent<SpriteRenderer>();
+        bc2d = GetComponent<BoxCollider2D>();
     }
 
     private void Update() {
-        // Change Direction according to input.
+        // Change HorizontalDirection according to input.
         float direction = Input.GetAxisRaw("Horizontal");
         // Set Speed veriable defined in Animator Parameters.
         animator.SetFloat("speed", Mathf.Abs(direction));
         if (direction != 0) {
             sr.flipX = (direction > 0) ? false : true;
+        }
+
+        // Jump Animation on Space
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            if (!animator.GetBool("Jump"))
+                animator.SetBool("Jump", true);
+        } else {
+            animator.SetBool("Jump", false);
+        }
+        
+
+        // Crouch on C
+        if (Input.GetKeyDown(KeyCode.C)) {
+            // Constant Values Obtained Manually
+            float CrouchOffsetX = 0.002846733f;
+            float CrouchOffsetY = 0.5938945f;
+            float CrouchSizeX = 0.4671042f;
+            float CrouchSizeY = 1.247203f;
+            if (!animator.GetBool("Crouch"))
+                animator.SetBool("Crouch", true);
+            bc2d.size = new Vector2(CrouchSizeX, CrouchSizeY);
+            bc2d.offset = new Vector2(CrouchOffsetX, CrouchOffsetY);
+        } else {
+            float OriginalOffsetX = 0.002846733f;
+            float OriginalOffsetY = 0.9867451f;
+            float OriginalSizeX = 0.4671042f;
+            float OriginalSizeY = 2.032904f;
+
+            animator.SetBool("Crouch", false);
+            bc2d.size = new Vector2(OriginalSizeX, OriginalSizeY);
+            bc2d.offset = new Vector2(OriginalOffsetX, OriginalOffsetY);
         }
     }
 }
