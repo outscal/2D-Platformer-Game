a65fe06875d52055cdd252f8c5f6912807ac384d
akshay_manthekar <akky123654@gmail.com>
2023-04-21 11:53:37 +0530

    Crouch Animation Added
    BoxCollider2D component is controlled through script. When player crouches, collider size reduces. It can be setup through the inspector. [SerializeField] is included for easy modification

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,21 +6,58 @@ public class PlayerController : MonoBehaviour
 {
     public Animator animator;
     public SpriteRenderer spriteRenderer;
+    public BoxCollider2D boxCollider2D;
+
+    [SerializeField] Vector2 boxColliderOffset;
+    [SerializeField] Vector2 boxColliderSize;
+
+    private Vector2 initialSize, initialOffset;
 
     private void Start()
     {
         spriteRenderer= GetComponent<SpriteRenderer>();
+        initialOffset = gameObject.GetComponent<BoxCollider2D>().offset;
+        initialSize = gameObject.GetComponent<BoxCollider2D>().size;
     }
 
     private void Update()
     {
         float speed = Input.GetAxisRaw("Horizontal");
+        bool isCrouch = Input.GetKey(KeyCode.LeftControl);
+        bool isJump = Input.GetKeyDown(KeyCode.Space);
+
+        // Player Crouch Animation trigger
+        if (isCrouch)
+        {
+            // If Left Control is Pressed, then player will crouch
+            // Also need to resize the box collider
+            animator.SetBool("Crouch", isCrouch);
+            gameObject.GetComponent<BoxCollider2D>().offset = boxColliderOffset;
+            gameObject.GetComponent<BoxCollider2D>().size = boxColliderSize;
+        } else
+        {
+            animator.SetBool("Crouch", isCrouch);
+            gameObject.GetComponent<BoxCollider2D>().offset = initialOffset;
+            gameObject.GetComponent<BoxCollider2D>().size = initialSize;
+        }
+
         animator.SetFloat("Speed", Mathf.Abs(speed));
+
         if (speed < 0)
         {
             spriteRenderer.flipX = true;
-        } else if (speed > 0) { 
+        } else if (speed > 0) 
+        { 
             spriteRenderer.flipX= false;
+        }
+        // Player Jump
+        if(isJump)
+        {
+            animator.SetBool("Jump", isJump);
+            Debug.Log("Space was pressed\n");
+        } else
+        {
+            animator.SetBool("Jump", false);
         }
     }
 }
