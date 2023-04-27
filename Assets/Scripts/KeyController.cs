b9ae86570c92f484beff82a7ead501487a534a3b
akshay_manthekar <akky123654@gmail.com>
2023-04-27 20:45:32 +0530

    Key Fade Animation Added
    When player collects the key, key rises up a few distance after that it fades out.

--- Assets/Scripts/KeyController.cs
+++ Assets/Scripts/KeyController.cs
@@ -4,13 +4,27 @@ using UnityEngine;
 
 public class KeyController : MonoBehaviour
 {
+    public Animator keyAnimator;
+    public float keyFadeDuration = 1f;
+
+    private void Start()
+    {
+        keyAnimator = GetComponent<Animator>();
+        keyAnimator.SetBool("KeyCollected", false);
+    }
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if(collision.gameObject.GetComponent<PlayerController>() != null)
         {
             PlayerController playerController = collision.gameObject.GetComponent<PlayerController>();
             playerController.CollectKey();
-            Destroy(gameObject);
+            keyAnimator.SetBool("KeyCollected", true);
+            Invoke(nameof(DestroyGameObject), keyFadeDuration);
         }
+    }
+
+    void DestroyGameObject()
+    {
+        Destroy(gameObject);
     }
 }
