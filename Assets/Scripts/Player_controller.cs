2e070ad84fce48579ee0d86f6267b9382790b6aa
orthos-abuser <chouremalhar@gmail.com>
2023-04-27 17:22:51 +0530

    Added crouch functionality

--- Assets/Scripts/Player_controller.cs
+++ Assets/Scripts/Player_controller.cs
@@ -7,7 +7,7 @@ public class Player_controller : MonoBehaviour
 
     public GameObject player;
     public float player_speed;
-    public Animator animatior;
+    public Animator animator;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,7 +19,7 @@ public class Player_controller : MonoBehaviour
     {
         float speed = Input.GetAxisRaw("Horizontal");
         Debug.Log(speed);
-        animatior.SetFloat("Speed",Mathf.Abs(speed));
+        animator.SetFloat("Speed",Mathf.Abs(speed));
         if(speed<0)
         {
             Vector2 Scale = transform.localScale;
@@ -31,6 +31,14 @@ public class Player_controller : MonoBehaviour
             Vector2 Scale = transform.localScale;
             Scale.x = 1;
             transform.localScale = Scale;
+        }
+        if(Input.GetKey("left ctrl"))
+        {
+            animator.SetBool("Crouched",true);
+        }
+        else
+        {
+            animator.SetBool("Crouched", false);
         }
         //player.transform.localScale = new Vector3(speed, player_speed, 0);
     }
