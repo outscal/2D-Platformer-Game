8cb966725adf6872e41e8ab1e30784eb71014335
ShoumoPal <arka.pal21@gmail.com>
2023-05-02 16:11:15 +0530

    Added crouch and jump animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,13 +6,38 @@ using UnityEngine.UIElements;
 public class PlayerController : MonoBehaviour
 {
     public Animator anim;
-
+    private bool crouch = false;
+    private bool jump = false;
     // Update is called once per frame
     void Update()
     {
         float speed = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
         anim.SetFloat("Speed", Mathf.Abs(speed));
 
+        //Jump Logic
+        if(vertical > 0 )
+        {
+            jump= true;
+        }
+        else
+        {
+            jump= false;
+        }
+        anim.SetBool("Jump", jump);
+        //Crouch logic
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            crouch = true;
+        }
+        else
+        {
+            crouch = false;
+        }
+        anim.SetBool("Crouch", crouch);
+
+        //Running logic
         Vector3 scale = transform.localScale;
         if (speed < 0)
         {   
