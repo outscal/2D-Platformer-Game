28d1946c49a9192c6bd449b90b3f5742eacfca5d
P D Vivekanand <103555518+viv1nd@users.noreply.github.com>
2023-05-05 19:15:55 +0530

    Added horizontal input to walk the player and move it forward.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,8 +7,9 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
-    
+     public float speed;
    
+    
     
    
     
@@ -15,25 +17,50 @@ public class PlayerController : MonoBehaviour
    
     void Update()
     {
+        float vertical = Input.GetAxisRaw("Jump");
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        Debug.Log("speed= " + horizontal);
 
-        float speed = Input.GetAxisRaw("Horizontal");
+        MoveCharacter(horizontal);
+        PlayerMovementAnimation(horizontal);
+       
+
+
+        if(Input.GetButtonDown("Fire1")){
+            GetComponent<Animator>().Play("Player_Crouch");
+        }
+
+    
+        if(Input.GetKeyDown(KeyCode.W)){
+            animator.SetBool("Jump",true);
+        }
+    }
+    
+    private void MoveCharacter(float horizontal){
+
+        Vector3 position=transform.position;
+        position.x = position.x + horizontal * speed * Time.deltaTime;
+        transform.position =  position;
+    }
+
+    private void PlayerMovementAnimation(float horizontal){
+
+       
+       animator.SetInteger("Speed",(int)horizontal);
+
         
-        animator.SetInteger("Speed", (int) speed);
-
-        Debug.Log("speed= " + speed);
 
        Vector3 scale = transform.localScale;
-        if  (speed < 0)
+        if  (horizontal < 0)
         {
             scale.x = -1f * Mathf.Abs(scale.x);
         }
 
-        else if ( speed > 0)
+        else if ( horizontal > 0)
         {
             scale.x = Mathf.Abs(scale.x);
         }
        
         transform.localScale = scale;
-        
     }
 }
