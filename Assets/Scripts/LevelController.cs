6b6eee47e136c4658499201df9756fa4500a9276
ShoumoPal <arka.pal21@gmail.com>
2023-05-05 20:27:44 +0530

    Added basic level setup and Level end logic

---
+++ Assets/Scripts/LevelController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.GetComponent<PlayerController>() != null)
+        {
+            Debug.Log("Level completed!");
+        }
+    }
+}
