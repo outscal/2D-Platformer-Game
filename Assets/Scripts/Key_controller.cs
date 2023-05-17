ef4462bf700c18ec270a51c25b8d483adba20bd0
orthos-abuser <chouremalhar@gmail.com>
2023-05-17 22:42:58 +0530

    Added key animation and ui update

---
+++ Assets/Scripts/Key_controller.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using TMPro;
+
+public class Key_controller : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.GetComponent<Player_controller>()!=null)
+        {
+            Increase_score score_text = GameObject.Find("Score_text").GetComponent<Increase_score>();
+            score_text.update_score();
+            Destroy(this.gameObject);
+        }
+    }
+}
