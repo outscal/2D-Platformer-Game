92c491021b64418d0d3d70850ff224429cf92255
shaan1757 <62057164+shaan1757@users.noreply.github.com>
2023-05-20 01:58:19 +0530

    binded crouch animation with left cntrl key  on keyboard binded jump animation with the vertical input

--- Assets/Scripts/PlayerControllerScript.cs
+++ Assets/Scripts/PlayerControllerScript.cs
@@ -9,19 +9,31 @@ public class PlayerControllerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       float  speed = Input.GetAxisRaw("Horizontal");
-       animator.SetFloat("Speed",Mathf.Abs(speed));
+       float  hrzntl = Input.GetAxisRaw("Horizontal");
+       float  vrtcl = Input.GetAxisRaw("Vertical");
+
+       animator.SetFloat("Speed",Mathf.Abs(hrzntl));
 
        Vector3 resize = transform.localScale;
-       if(speed < 0)
+       if(hrzntl < 0)
        {
         resize.x = -1f * Mathf.Abs(resize.x);
        }
-       else if(speed > 0)
+       else if(hrzntl > 0)
        {
-        resize.x =Mathf.Abs(resize.x);
+        resize.x = Mathf.Abs(resize.x);
        }
        transform.localScale = resize;
+
+//crouch
+       bool crouch = Input.GetKey("left ctrl");
+       animator.SetBool("Crouch",crouch);
+
+//jump
+       if(vrtcl > 0)
+       {
+        animator.SetFloat("Jump", Mathf.Abs(vrtcl));
+       }
    
     }
 };    
