56adbbabcfe0407755917f36a26e841191dfb4c5
orthos-abuser <chouremalhar@gmail.com>
2023-05-24 16:13:14 +0530

    removed enemy_enemy collision

--- Assets/Scripts/enemy_movement.cs
+++ Assets/Scripts/enemy_movement.cs
@@ -14,28 +14,49 @@ public class enemy_movement : MonoBehaviour
     private float speed=10;
     private float reverse_speed;
 
+    public bool start_a=true;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        transform.position = point_a.position;
-        reverse_speed = -speed;
-        flip();
+        if (start_a == true)
+        {
+            transform.position = point_a.position;
+            reverse_speed = -speed;
+            Vector2 localScale = transform.localScale;
+            localScale.x =-1;
+            transform.localScale = localScale;
+        }
+        else
+        {
+            transform.position = point_b.position;
+            reverse_speed = speed;
+            Vector2 localScale = transform.localScale;
+            localScale.x = -1;
+            transform.localScale = localScale;
+        }
+  
     }
 
     // Update is called once per frame
     void Update()
     {
         step = step + speed* Time.deltaTime;
+        Debug.Log(step);
         if(step>=1)
         {
             speed = reverse_speed;
-            flip();
+            Vector2 localScale = transform.localScale;
+            localScale.x = 1;
+            transform.localScale = localScale;
         }
         if(step<=0)
         {
             speed = -reverse_speed;
-            flip();
+            Vector2 localScale = transform.localScale;
+            localScale.x = -1;
+            transform.localScale = localScale;
         }
         transform.position = Vector2.Lerp(point_a.position, point_b.position, step);
     }
