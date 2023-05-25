5ca1dc91d24cfbc305b8d0cce92dba4040c907b9
orthos-abuser <chouremalhar@gmail.com>
2023-05-25 20:57:05 +0530

    Commit changes

---
+++ Assets/Scripts/Menu_to_play.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu_to_play : MonoBehaviour
+{
+    public void play()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
