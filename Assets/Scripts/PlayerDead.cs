4b1450dc8267ac5410085499c4d02e373219e421
Pranay Anumandla <pranay7293@gmail.com>
2023-05-28 09:52:41 +0530

    Player Death condition has been added.

--- Assets/Scripts/PlayerDead.cs
+++ Assets/Scripts/PlayerDead.cs
@@ -4,14 +4,23 @@ using UnityEngine;
 
 public class PlayerDead : MonoBehaviour
 {
+    public Animator animator;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.GetComponent<PlayerController>() != null)
         {
-            Debug.Log("Player is Dead");
+            Debug.Log("Player Dead"); 
+            animator.SetBool("Death", true);
         }
-    }
+        else
+        {
+            animator.SetBool("Death", false);
+        }
+
+    }    
+
+
 
 
 }
