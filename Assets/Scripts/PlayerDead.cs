aadd688658c07abf9da33c8ea9bd3661c0f4cf65
Pranay Anumandla <pranay7293@gmail.com>
2023-05-28 08:18:18 +0530

    Player Level Finish Trigger added on Collision.

---
+++ Assets/Scripts/PlayerDead.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDead : MonoBehaviour
+{
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>() != null)
+        {
+            Debug.Log("Player is Dead");
+        }
+    }
+
+
+}
