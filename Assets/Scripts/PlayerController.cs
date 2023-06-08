71ce456f8c26aa8d61fe5fee0778c8aebffdc1f4
Zarrickisdea <mohammedraz45@yahoo.com>
2023-06-08 22:56:54 +0530

    Jump added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     private void Update ()
     {
         float speed = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
 
         Animator.SetFloat("Speed", Mathf.Abs(speed));
 
@@ -22,6 +23,17 @@ public class PlayerController : MonoBehaviour
         {
             scale.x = Mathf.Abs(scale.x);
         }
+
+        if (vertical > 0)
+        {
+            Animator.SetTrigger("JumpTrigger");
+            Animator.SetBool("Grounded", false);
+        }
+        else
+        {
+            Animator.SetBool("Grounded", true);
+        }
+
         transform.localScale = scale;
 
         if (Input.GetKeyDown("left ctrl") || Input.GetKeyDown("right ctrl"))
