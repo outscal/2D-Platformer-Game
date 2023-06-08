d09cc6fe4d07d7d8d67f01a6b488f01d82b7033b
Zarrickisdea <mohammedraz45@yahoo.com>
2023-06-08 16:08:59 +0530

    Running and turning

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,5 +4,22 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public Animator Animator;
 
+    private void Update ()
+    {
+        float speed = Input.GetAxisRaw("Horizontal");
+        Animator.SetFloat("Speed", Mathf.Abs(speed));
+
+        Vector3 scale = transform.localScale;
+        if (speed < 0)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        else if (speed > 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
+    }
 }
