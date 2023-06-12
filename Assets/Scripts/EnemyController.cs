30d4f0135252d8305c5c4e20bc0dd9c35257c9fc
P D Vivekanand <103555518+viv1nd@users.noreply.github.com>
2023-06-12 12:09:17 +0530

    Added multiple enemiea on single platform
    I added multiple enemies using raycast and layer collison matrix with which the enemies know where the platform ends and also they are not colliding with each other.

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -4,6 +4,36 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+
+    public float speed;
+    private bool movingCorrect;
+    public float rayDist;
+
+    public Transform groundDetect;
+
+     /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    private void Update()
+    {
+        transform.Translate(Vector2.right * speed * Time.deltaTime);
+        RaycastHit2D groundCheck = Physics2D.Raycast(groundDetect.position,Vector2.down , rayDist);
+
+        if(groundCheck.collider == false)
+        {
+            if(movingCorrect == true)
+            {
+                transform.eulerAngles = new Vector3 ( 0 , -180 , 0);
+                movingCorrect = false;
+            }
+
+            else
+            {
+                transform.eulerAngles = new Vector3 (0,0,0);
+                movingCorrect = true;
+            }
+        }     
+    }
     void OnCollisionEnter2D(Collision2D other)
     {
         if(other.gameObject.GetComponent<PlayerController>() != null)
