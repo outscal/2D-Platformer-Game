6117c3d975df7bf3da4d3b4c854f79cc34569e40
Saurabh bhamare <ankaramessi@mail.com>
2023-06-19 18:35:58 +0530

    multiple keys

---
+++ Assets/Scripts/FadeKey.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class FadeKey : MonoBehaviour
+{
+    private void Start()
+    {
+
+    }
+    private void Update()
+    {
+        
+    }
+  
+}
