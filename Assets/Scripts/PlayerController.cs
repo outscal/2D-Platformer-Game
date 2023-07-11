2d60ebdc42f6fbec55a7a3cf2560ced4225129e1
Jatin Kumar <106999612+jatinkumar-creator@users.noreply.github.com>
2023-07-12 00:51:20 +0530

    Player controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public Animator animator;
+   
+    
+    private void Awake() 
     {
-        
+        Debug.Log("Player controller awake");
     }
-
-    // Update is called once per frame
-    void Update()
+    //private void OnCollisionEnter2D(Collision2D collision)
+    //{
+      // Debug.Log("Collision: " + collision.gameObject.name);
+    //}
+    private void Update() 
     {
-        
+        float speed = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs(speed));
+         Vector3 scale = transform.localScale;
+        if (speed < 0)
+
+        {
+            
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+         else if (speed > 0) 
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+       transform.localScale = scale;
     }
 }
