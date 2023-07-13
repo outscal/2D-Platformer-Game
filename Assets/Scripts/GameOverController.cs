2502acf78400988b7353c6d091208e54634eacb2
Abhishek Kadam <kadamabhishek981111@gmail.com>
2023-07-13 23:08:05 +0530

    Added MainMenu button
    Added MainMenu button to go back to Lobby scene(0).

--- Assets/Scripts/GameOverController.cs
+++ Assets/Scripts/GameOverController.cs
@@ -7,10 +7,12 @@ using UnityEngine.UI;
 public class GameOverController : MonoBehaviour
 {
     public Button buttonRestart;
+    public Button buttonMainMenu;
     
     public void Awake()
     {
         buttonRestart.onClick.AddListener(ReloadLevel);
+        buttonMainMenu.onClick.AddListener(MainMenu);
     }
     public void PlayerDied()
     {
@@ -20,5 +22,10 @@ public class GameOverController : MonoBehaviour
     {
         Debug.Log("Reloading Scene 1");
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);      //Can also do with SceneManager.LoadScene(0);
+    }
+    public void MainMenu()
+    {
+        Debug.Log("Loading scene 0");
+        SceneManager.LoadScene(0);  
     }
 }
