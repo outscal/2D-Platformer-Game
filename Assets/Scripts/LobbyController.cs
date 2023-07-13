7ae4d13556382ce48e0d54152f1c072bcb2461af
Abhishek Kadam <kadamabhishek981111@gmail.com>
2023-07-13 22:19:33 +0530

    Quit button in lobby scene
    Added Quit button in lobby scene canvas UI.

--- Assets/Scripts/LobbyController.cs
+++ Assets/Scripts/LobbyController.cs
@@ -7,14 +7,22 @@ using UnityEngine.SceneManagement;
 public class LobbyController : MonoBehaviour
 {
     public Button buttonPlay;
+    public Button buttonQuit;
 
     private void Awake()
     {
         buttonPlay.onClick.AddListener(PlayGame);
+        buttonQuit.onClick.AddListener(QuitGame);
     }
 
     private void PlayGame()
     {
         SceneManager.LoadScene(1);
+    }
+
+    private void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit!");
     }
 }
