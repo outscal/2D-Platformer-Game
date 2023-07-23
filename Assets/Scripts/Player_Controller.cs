d2af16993a85c3680bdc2f2f557776efc49a99a9
Sujit Reddy <Sujitreddy97@users.noreply.github.com>
2023-07-23 11:41:57 +0530

    Added code to accept horizontal input from the user.
    Added code to play run animation and flip player according to the horizontal input.

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class Player_Controller : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Animator animator;
 
     // Update is called once per frame
     void Update()
     {
-        
+        //speed of the player by player input
+        float speed = Input.GetAxisRaw("Horizontal");
+
+        //setting the speed parameter to animator
+        animator.SetFloat("Speed", Mathf.Abs(speed));
+
+        //Accessing the scale of the player
+        Vector3 scale = transform.localScale;
+
+        //if speed < 0 flip the image in x-axis 
+        if(speed < 0)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        else if(speed > 0) //If speed > 0 then keep the x-axis value positive
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+        //Set the scale value
+        transform.localScale = scale;
     }
 }
