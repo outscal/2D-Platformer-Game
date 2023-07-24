27dc076dc0a08480b215a7ed4a770c1559c1c990
Sujit Reddy <Sujitreddy97@users.noreply.github.com>
2023-07-24 10:06:33 +0530

    Player horizontal Movement Added
    Horizontal movement added using transform.position

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -7,25 +7,38 @@ public class Player_Controller : MonoBehaviour
 {
     public Animator animator;
     public BoxCollider2D collider1;
+    public float speed;
     
     // Update is called once per frame
     void Update()
     {
-        //speed of the player by player input
-        float speed = Input.GetAxisRaw("Horizontal");
+        //horizontalInput(speed) of the player by player input
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        MovieCharacter(horizontal);
+        PlayMovementAnimation(horizontal);
 
+    }
+    private void MovieCharacter(float horizontal)
+    {
+        Vector3 position = transform.position;
+        position.x += horizontal * speed * Time.deltaTime;
+        transform.position = position;
+    }
+    //Player Animation Movement function
+    private void PlayMovementAnimation(float horizontal)
+    {
         //setting the speed parameter to animator
-        animator.SetFloat("Speed", Mathf.Abs(speed));
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
 
         //Accessing the scale of the player
         Vector3 scale = transform.localScale;
 
         //if speed < 0 flip the image in x-axis 
-        if(speed < 0)
+        if (horizontal < 0)
         {
             scale.x = -1f * Mathf.Abs(scale.x);
         }
-        else if(speed > 0) //If speed > 0 then keep the x-axis value positive
+        else if (horizontal > 0) //If speed > 0 then keep the x-axis value positive
         {
             scale.x = Mathf.Abs(scale.x);
         }
@@ -36,21 +49,25 @@ public class Player_Controller : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.LeftControl))
         {
             //Set crouch animation
-            animator.SetBool("Crouch", true);
-            collider1.size = new Vector2(collider1.size.x, 1.4f);
-            collider1.offset = new Vector2(collider1.offset.x, 0.6f);
+            Crouch(true, 1.4f, 0.6f);
         }
-        else if(Input.GetKeyUp(KeyCode.LeftControl))
+        else if (Input.GetKeyUp(KeyCode.LeftControl))
         {
             //Set crouch animation
-            animator.SetBool("Crouch", false);
-            collider1.size = new Vector2(collider1.size.x, 2.1f);
-            collider1.offset = new Vector2(collider1.offset.x, 1);
+            Crouch(false, 2.1f, 1);
         }
+
 
         //Jump
         float verticalSpeed = Input.GetAxisRaw("Vertical");
         animator.SetFloat("VerticalSpeed", verticalSpeed);
-       
+    }
+
+    //Crouch function
+    public void Crouch(bool crouch, float sizeY, float offsetY)
+    {
+        animator.SetBool("Crouch", crouch);
+        collider1.size = new Vector2(collider1.size.x, sizeY);
+        collider1.offset = new Vector2(collider1.offset.x, offsetY);
     }
 }
