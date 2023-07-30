9e829918aa17aee7c7cf55d7ff44896b1b2cb578
AKSHAY R <akshay.renjith.1@gmail.com>
2023-07-31 03:02:24 +0530

    Binded horizontal input to walk/run the player and move it forward & Binded Vertical input to jump the player up

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -8,13 +9,17 @@ public class PlayerController : MonoBehaviour
     private BoxCollider2D playerCollider;
     private Rigidbody2D playerBody;
 
+    public float speed;
+    public float jump;
+
+    private bool shiftPressed = false;
     private bool ctrlPressed = false;
-   
+    private bool isGrounded = false;
+
     private void Awake()
     {
         Debug.Log("Player controller awake");
     }
-
     private void Start()
     {
         playerCollider = gameObject.GetComponent<BoxCollider2D>();
@@ -22,27 +27,77 @@ public class PlayerController : MonoBehaviour
         playerBody = gameObject.GetComponent<Rigidbody2D>();
     }
 
+    private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Ground"))
+            {
+                isGrounded = true;
+            }
+        }
+
     // Update is called once per frame
     private void Update()
     {
-        float speed = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("Speed", Mathf.Abs(speed));
+         float horizontal = Input.GetAxisRaw("Horizontal");
+         float vertical = Input.GetAxis("Jump");
+         shiftPressed = Input.GetKey(KeyCode.LeftShift);
+         ctrlPressed = (Input.GetKey(KeyCode.LeftControl)) || (Input.GetKey(KeyCode.RightControl)) ;
 
-        ctrlPressed = (Input.GetKey(KeyCode.LeftControl)) || (Input.GetKey(KeyCode.RightControl)) ;
-        animator.SetBool("IsCrouching", ctrlPressed);
+        MoveCharacter(horizontal,vertical);
+        PlayMovementAnimation(horizontal,vertical);
 
-        float verticalInput = Input.GetAxis("Vertical");
-        animator.SetFloat("Vertical", Mathf.Abs(verticalInput));
+    }
+
+    private void MoveCharacter(float horizontal, float vertical)
+        {
+            // move character horizontally
+            //walk and run
+            Vector3 position = transform.position;
+            if(shiftPressed)                          
+            {
+                position.x += horizontal * speed * Time.deltaTime * 2;  //Run
+            }
+            else
+            {
+                position.x += horizontal * speed * Time.deltaTime;     //Walk
+            }
+            transform.position = position; 
+
+            if(vertical > 0 && isGrounded)
+            {
+                isGrounded = false;
+                // move character vertically
+                if(shiftPressed){
+                    playerBody.AddForce(new Vector2(0f, 1.3f * jump), ForceMode2D.Impulse);
+                }
+                else{
+                    playerBody.AddForce(new Vector2(0f, jump), ForceMode2D.Impulse);
+                }
+
+            }
+        }
+
+    private void PlayMovementAnimation(float horizontal, float vertical)
+    {
+        if (shiftPressed)                          // For Run animation, I make speed variable > 1.99, Pressing L.Shift
+        {
+            horizontal = 2 * horizontal;
+        }
+         animator.SetBool("IsWalking", (horizontal != 0 ? true : false));
+         animator.SetFloat("Speed", Mathf.Abs(horizontal));
+
         //Flipping the player 
         Vector3 scale = transform.localScale;
-        if(speed < 0)
+        if(horizontal < 0)
         {
             scale.x = -1f * Mathf.Abs(scale.x);
-        } else if(speed > 0)
+        } else if(horizontal > 0)
         {
             scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
+
+        animator.SetBool("IsCrouching", ctrlPressed);
 
         //Crouch Animation
         if (ctrlPressed)
@@ -57,15 +112,14 @@ public class PlayerController : MonoBehaviour
         }
 
         //Jump Animation
-        if( verticalInput > 0 )
+        if( vertical > 0 )
         {
            animator.SetBool("IsJumping", true);
-
-        } else if(verticalInput<=0)
+        } else 
         {
             animator.SetBool("IsJumping", false);
         }
-        
+
     }
 
 }
