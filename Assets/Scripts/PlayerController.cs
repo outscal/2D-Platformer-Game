8784065069ebeae39071765b9e3df718e42c7eef
AKSHAY R <akshay.renjith.1@gmail.com>
2023-07-30 23:15:32 +0530

    Revert "Binded horizontal input to walk/run the player and move it forward"
    This reverts commit 64be17ad1a768e605551af02cf51a79ee1ad27f0.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,7 +8,6 @@ public class PlayerController : MonoBehaviour
     private BoxCollider2D playerCollider;
     private Rigidbody2D playerBody;
 
-    public float speed;
     private bool ctrlPressed = false;
    
     private void Awake()
@@ -26,16 +25,24 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-
-        MoveCharacter(horizontal);
-        PlayMovementAnimation(horizontal);
+        float speed = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs(speed));
 
         ctrlPressed = (Input.GetKey(KeyCode.LeftControl)) || (Input.GetKey(KeyCode.RightControl)) ;
         animator.SetBool("IsCrouching", ctrlPressed);
 
         float verticalInput = Input.GetAxis("Vertical");
         animator.SetFloat("Vertical", Mathf.Abs(verticalInput));
+        //Flipping the player 
+        Vector3 scale = transform.localScale;
+        if(speed < 0)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        } else if(speed > 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
 
         //Crouch Animation
         if (ctrlPressed)
@@ -59,28 +66,6 @@ public class PlayerController : MonoBehaviour
             animator.SetBool("IsJumping", false);
         }
         
-    }
-
-    private void MoveCharacter(float horizontal)
-    {
-        Vector3 position = transform.position;
-        position.x += horizontal * speed * Time.deltaTime;
-        transform.position = position; 
-    }
-
-    private void PlayMovementAnimation(float horizontal)
-    {
-         animator.SetFloat("Speed", Mathf.Abs(horizontal));
-        //Flipping the player 
-        Vector3 scale = transform.localScale;
-        if(horizontal < 0)
-        {
-            scale.x = -1f * Mathf.Abs(scale.x);
-        } else if(horizontal > 0)
-        {
-            scale.x = Mathf.Abs(scale.x);
-        }
-        transform.localScale = scale;
     }
 
 }
