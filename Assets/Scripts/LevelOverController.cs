3c9d1dc6722c447a464aeac9a4afd56a73972620
AKSHAY R <akshay.renjith.1@gmail.com>
2023-08-02 01:19:00 +0530

    Made an empty game object to detect the end of the level and go to the next level

---
+++ Assets/Scripts/LevelOverController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelOverController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>() != null)
+        {
+            Debug.Log(" Level Finished by the Player ");
+            Debug.Log(" Load New Level ");
+        }
+    }
+}
