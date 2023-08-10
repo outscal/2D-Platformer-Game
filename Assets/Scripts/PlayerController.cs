ddc7f0f50d87678be14f5d1081cabc570b6ee6df
MoonJSonic <abhijeet.munje@gmail.com>
2023-08-10 18:50:38 +0530

    Added Horizontal Player Movement
    With the horizontal input > 0 the code translates the player across x axis with all other controls and animations in place.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
+    public float speed;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,28 +15,44 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float speed = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("Speed",Mathf.Abs(speed));
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        MoveCharachtar(horizontal);
+        PlayMovementAnimation(horizontal);
+
+        
+    }
+
+    private void MoveCharachtar(float horizontal)
+    {
+        Vector3 position = transform.position;
+        position.x += horizontal * speed * Time.deltaTime;
+        transform.position = position;
+    }
+
+    private void PlayMovementAnimation(float horizontal)
+    {
+        animator.SetFloat("Speed", Mathf.Abs(horizontal));
 
         Vector3 scale = transform.localScale;
-        if(speed<0)
+        if (horizontal < 0)
         {
             scale.x = -1f * Mathf.Abs(scale.x);
         }
-        else if(speed>0)
+        else if (horizontal > 0)
         {
             scale.x = Mathf.Abs(scale.x);
         }
         transform.localScale = scale;
 
-       bool crouch = Input.GetKey(KeyCode.LeftControl);
+        bool crouch = Input.GetKey(KeyCode.LeftControl);
         animator.SetBool("Crouch", crouch);
 
         float jump = Input.GetAxisRaw("Vertical");
-        animator.SetFloat("Jump",jump);
+        animator.SetFloat("Jump", jump);
 
 
         //bool jump = Input.GetKey(KeyCode.Space);
         //animator.SetBool("JumpTriggered", jump);
     }
+
 }
