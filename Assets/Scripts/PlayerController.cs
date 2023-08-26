af59ce57bf019012a628f945d50e68dcfd1867bb
kiran2701 <gaurdian.kiran@gmail.com>
2023-08-26 16:42:50 +0100

    Added horizontal Input and animation flipping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private Animator animator;
+
+    private float speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        speed = 0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        CheckDirection();
+        speed = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs(speed));
+    }
+
+    void CheckDirection()
+    {
+        if (speed < 0f)
+            transform.eulerAngles = new Vector3(0, 180, 0);
+        else
+            transform.eulerAngles = new Vector3(0, 0, 0);
     }
 }
