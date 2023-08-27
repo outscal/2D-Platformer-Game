377a464b3b910221e1e84d9c8d26f21c4b8881dd
kiran2701 <gaurdian.kiran@gmail.com>
2023-08-27 11:13:01 +0100

    Added crouch and jump animations with collider resizing

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,27 +5,86 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Animator animator;
+    [SerializeField] private Vector2 newCrouchColliderOffset;
+    [SerializeField] protected Vector2 newCrouchColliderSize;
 
+    private enum PlayerState
+    {
+        idle, 
+        walking,
+        running,
+        crouching,
+        jumping
+    };
+
+    private Vector2 originalColliderOffset;
+    private Vector2 originalColliderSize;
+    private PlayerState state;
+    private BoxCollider2D collider;
     private float speed;
     // Start is called before the first frame update
     void Start()
     {
         speed = 0f;
+        state = PlayerState.idle;
+        collider = GetComponent<BoxCollider2D>();
+        originalColliderOffset = collider.offset;
+        originalColliderSize = collider.size;
     }
 
     // Update is called once per frame
     void Update()
     {
+        GetPlayerInput();
         CheckDirection();
+    }
+
+    void GetPlayerInput()
+    {
         speed = Input.GetAxisRaw("Horizontal");
         animator.SetFloat("Speed", Mathf.Abs(speed));
+
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            state = PlayerState.crouching;
+            ResizeCollider();
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            state = PlayerState.idle;
+            RestoreColliderSize();
+        }
+
+        if (Input.GetAxisRaw("Vertical") > 0f)
+            animator.SetTrigger("PlayerJumped");
+
+        animator.SetInteger("PlayerState", (int)state);
+    }
+
+    void RestoreColliderSize()
+    {
+        collider.offset = originalColliderOffset;
+        collider.size = originalColliderSize;
+    }
+
+    void ResizeCollider()
+    {
+        collider.offset = newCrouchColliderOffset;
+        collider.size = newCrouchColliderSize;
     }
 
     void CheckDirection()
     {
         if (speed < 0f)
+        {
             transform.eulerAngles = new Vector3(0, 180, 0);
-        else
+            state = PlayerState.walking;
+        }
+        else if (speed > 0.1f)
+        {
             transform.eulerAngles = new Vector3(0, 0, 0);
+            state = PlayerState.walking;
+        }
     }
 }
