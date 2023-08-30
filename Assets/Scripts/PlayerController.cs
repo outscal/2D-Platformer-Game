dfb5b2bf281882a18c863b7efaf326ff33545b89
kiran2701 <gaurdian.kiran@gmail.com>
2023-08-30 13:43:28 +0100

    Added Movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,8 +6,12 @@ public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Animator animator;
     [SerializeField] private Vector2 newCrouchColliderOffset;
-    [SerializeField] protected Vector2 newCrouchColliderSize;
+    [SerializeField] private Vector2 newCrouchColliderSize;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float jumpPower;
 
+
+    private Rigidbody2D rb;
     private enum PlayerState
     {
         idle, 
@@ -20,16 +24,17 @@ public class PlayerController : MonoBehaviour
     private Vector2 originalColliderOffset;
     private Vector2 originalColliderSize;
     private PlayerState state;
-    private BoxCollider2D collider;
-    private float speed;
+    private CapsuleCollider2D playerCollider;
+    private float horizontalInput;
     // Start is called before the first frame update
     void Start()
     {
-        speed = 0f;
+        horizontalInput = 0f;
         state = PlayerState.idle;
-        collider = GetComponent<BoxCollider2D>();
-        originalColliderOffset = collider.offset;
-        originalColliderSize = collider.size;
+        playerCollider = GetComponent<CapsuleCollider2D>();
+        rb = GetComponent<Rigidbody2D>();
+        originalColliderOffset = playerCollider.offset;
+        originalColliderSize = playerCollider.size;
     }
 
     // Update is called once per frame
@@ -41,8 +46,9 @@ public class PlayerController : MonoBehaviour
 
     void GetPlayerInput()
     {
-        speed = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("Speed", Mathf.Abs(speed));
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs(horizontalInput));
+        MovePlayer(horizontalInput);
 
         if (Input.GetKey(KeyCode.LeftControl))
         {
@@ -56,35 +62,63 @@ public class PlayerController : MonoBehaviour
             RestoreColliderSize();
         }
 
-        if (Input.GetAxisRaw("Vertical") > 0f)
-            animator.SetTrigger("PlayerJumped");
+        //if (Input.GetAxisRaw("Vertical") > 0f)
+        //    PlayerJump();
 
         animator.SetInteger("PlayerState", (int)state);
     }
 
+    void MovePlayer(float input)
+    {
+        Vector3 position = transform.position;
+
+        position.x += input * moveSpeed * Time.deltaTime;
+        transform.position = position;
+    }
     void RestoreColliderSize()
     {
-        collider.offset = originalColliderOffset;
-        collider.size = originalColliderSize;
+        playerCollider.offset = originalColliderOffset;
+        playerCollider.size = originalColliderSize;
     }
 
     void ResizeCollider()
     {
-        collider.offset = newCrouchColliderOffset;
-        collider.size = newCrouchColliderSize;
+        playerCollider.offset = newCrouchColliderOffset;
+        playerCollider.size = newCrouchColliderSize;
     }
-
     void CheckDirection()
     {
-        if (speed < 0f)
+        if (horizontalInput < 0f)
         {
             transform.eulerAngles = new Vector3(0, 180, 0);
             state = PlayerState.walking;
         }
-        else if (speed > 0.1f)
+        else if (horizontalInput > 0.1f)
         {
             transform.eulerAngles = new Vector3(0, 0, 0);
             state = PlayerState.walking;
         }
+    }
+
+    void CheckForFalling()
+    {
+        if (IsGrounded())
+        {
+            state = PlayerState.idle;
+            animator.ResetTrigger("PlayerJumped");
+        }
+    }
+
+    void PlayerJump()
+    {
+        animator.SetTrigger("PlayerJumped");
+        rb.velocity = new Vector2(rb.velocity.x, jumpPower * Time.deltaTime);  
+    }
+    bool IsGrounded()
+    {
+        if (Physics2D.Raycast(playerCollider.bounds.center, -transform.up, 1.3f, LayerMask.GetMask("Platform")))
+            return true;
+
+        return false;
     }
 }
