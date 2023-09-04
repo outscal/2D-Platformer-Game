09c8e65d6b39e7dda814e00bf1664ebcc66053eb
Dev <devmaheswari2017@gmail.com>
2023-09-04 16:00:29 +0530

    fliping animation

--- Assets/Scripts/playerController.cs
+++ Assets/Scripts/playerController.cs
@@ -4,15 +4,35 @@ using UnityEngine;
 
 public class playerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
+     public Animator Animator;
     // Update is called once per frame
     void Update()
     {
-        
+        float speed = Input.GetAxisRaw("Horizontal");
+        Animator.SetFloat("Speed", Mathf.Abs(speed));
+
+        //if (Input.GetKeyDown(KeyCode.Space))
+        //{
+        //    Animator.SetBool("IsJumping", true);
+        //    Animator.SetBool("IsJumping", false);
+
+        //}
+        //if (Input.GetKeyUp(KeyCode.Space))
+        //{
+        //    Animator.SetBool("IsCrouching", true);
+        //    Animator.SetBool("IsCrouching", false);
+
+        //}
+
+        Vector3 scale = transform.localScale;
+        if (speed<0) // pressed A/ right arrow 
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        else if(speed > 0)  // pressed D/left arrow
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+            transform.localScale = scale;
     }
 }
