3756f350e73a46a01e4a241a9a626e6dd2c121c4
VastavPansuriya <vastavpansuriya4444@gmail.com>
2023-09-17 00:00:39 +0530

    PlayerController
    Add Player animations with using Collider and Rigidbody on it.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour 
+{
+    [SerializeField] private float speed = 0;
+    [SerializeField] private float moveValue = 0;
+
+    private SpriteRenderer playerSprite_R;
+    private Animator playerAnimator;
+    // Start is called before the first frame update
+    private void Start()
+    {
+        playerSprite_R = GetComponent<SpriteRenderer>();
+        playerAnimator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+
+        //input approach 1 smooth movement 
+        /*moveValue = Input.GetAxis("Horizontal");*/
+
+        //input approach 2 raw Movement and time.deltatime 
+        moveValue = Input.GetAxisRaw("Horizontal");     
+        if (moveValue > 0)
+        {
+
+            //sprite flip approach1 scale 
+            transform.localScale = Vector3.one;
+            //sprite flip approach2 flip value
+            //playerSprite_R.flipX = false;
+        }
+
+        if (moveValue < 0)
+        {
+            //sprite flip approach1 scale 
+            transform.localScale = new Vector3(-1,1,1);
+            //sprite flip approach2 flip value
+            //playerSprite_R.flipX = true;
+        }
+
+        playerAnimator.SetFloat("Speed",Mathf.Abs(moveValue));
+
+    }
+}
