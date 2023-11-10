f6db518fec9c44d265bf7fcc727af09db52e388b
Roopesh Verma <roopeshv.30@gmail.com>
2023-11-10 22:15:17 +0530

    Add Player controller and set animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,44 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private Animator playerAnim;
+    private float speed;
+    Vector2 scale;
+    private float jumpSpeed;
+
+    private void Start() {
+        playerAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        speed = Input.GetAxisRaw("Horizontal");
+
+        playerAnim.SetFloat("Speed", speed);
+        Vector2 scale = transform.localScale;
+        if(speed < 0)
+        {
+            playerAnim.SetFloat("Speed", Mathf.Abs(speed));
+            scale.x = -1f * scale.x;
+        }
+        else
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+        transform.localScale = scale;
+
+        if(Input.GetKeyDown(KeyCode.LeftControl))
+        {
+            playerAnim.SetBool("canCrouch", true);
+        }
+
+        jumpSpeed = Input.GetAxisRaw("Vertical");
+
+        if(jumpSpeed>0)
+        {
+            playerAnim.SetBool("canJump", true);
+        }
+
     }
 }
