daba78c101298e0249da7bc93a72dadc322acd9c
Roopesh Verma <roopeshv.30@gmail.com>
2023-11-13 22:15:51 +0530

    Add key rotation animation

--- Assets/Scripts/KeyController.cs
+++ Assets/Scripts/KeyController.cs
@@ -7,16 +7,6 @@ public class KeyController : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D other) {
         if(other.tag == "Player")
         {
-            print("h1");
-            playerController.UpdateScore();
-            gameObject.SetActive(false);
-        }
-    }
-
-    private void OnCollisionEnter2D(Collision2D other) {
-        if(other.gameObject.tag == "Player")
-        {
-            print("h2");
             playerController.UpdateScore();
             gameObject.SetActive(false);
         }
