63ffd1b4183d97f7fb3e87ebea4f4e80b52200ae
SibasisRath <sibasisrath2000@gmail.com>
2023-12-18 23:40:37 +0530

    controling character animation using keyboard inputs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Animator animator;
+    [SerializeField] float horizontalInput;
+
+    bool flip;
+
+    private void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        flip = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        animator.SetBool("IsRunning", false);
+
+
+        if (horizontalInput != 0)
+        {
+            animator.SetBool("IsRunning", true);
+        }
+        if (horizontalInput < 0)
+        {
+            flip = true;
+        }
+        else if (horizontalInput > 0)
+        {
+            flip = false;
+        }
+
+       /* if (Mathf.Approximately(horizontalInput, 0f))
+        {
+            flip = false;
+        }*/
+
+        GetComponent<SpriteRenderer>().flipX = flip;
+
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Jump");
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            animator.SetBool("IsCrouching", true);
+        }
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            animator.SetBool("IsCrouching", false);
+        }
     }
 }
