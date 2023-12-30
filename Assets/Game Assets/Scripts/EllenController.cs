69aac459c49a021e8fa80378633b4b52362f4385
DhillonHarsh <107165909+DHillon01@users.noreply.github.com>
2023-12-30 13:14:48 -0400

    added player controlls
    , jump walk and run

--- Assets/Game Assets/Scripts/EllenController.cs
+++ Assets/Game Assets/Scripts/EllenController.cs
@@ -4,15 +4,77 @@ using UnityEngine;
 
 public class EllenController : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Animator animator;
+    private Rigidbody2D rb;
+    private bool isGrounded = false;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        // Handle player movement and jumping input
+        HandleMovement();
+        HandleJumping();
+    }
+
+    void HandleMovement()
+    {
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("speed", Mathf.Abs(moveInput));
+
+        // Move player horizontally
+        rb.velocity = new Vector2(moveInput * moveSpeed, rb.velocity.y);
+
+        // Flip character if moving left/right
+        if (moveInput != 0)
+        {
+            transform.localScale = new Vector3(moveInput, 1, 1);
+        }
+    }
+
+    void HandleJumping()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        animator.SetBool("IsGrounded", isGrounded);
+
+        // Jumping
+        if (isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        // Handle jumping animation states
+        HandleJumpAnimations();
+    }
+
+    void HandleJumpAnimations()
+    {
+        if (rb.velocity.y > 0.1f)
+        {
+            animator.SetBool("IsJumping", true);
+            animator.SetBool("IsFalling", false);
+        }
+        else if (rb.velocity.y < -0.1f)
+        {
+            animator.SetBool("IsJumping", false);
+            animator.SetBool("IsFalling", true);
+        }
+        else
+        {
+            animator.SetBool("IsJumping", false);
+            animator.SetBool("IsFalling", false);
+        }
     }
 }
