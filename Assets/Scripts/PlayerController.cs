9edade0ead14d2437c79d0ab1085be6f17fdf90e
Reyansh <aayushsoni92@gmail.com>
2024-01-05 00:10:17 +0530

    Added Horizontal Movement and Jumping

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,26 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 { 
-[SerializeField] Animator animator;
+[SerializeField] private Animator animator;
+[SerializeField] private float Speed = 0f;
+[SerializeField] private float jump = 0f;
+private Rigidbody2D rb;
 
-private void Update()
-    { 
-float sp = Input.GetAxisRaw("Horizontal");
-float jump = Input.GetAxisRaw("Vertical");
-        animator.SetFloat("Speed", Mathf.Abs(sp));
+    void Awake()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
 
-Vector3 scale = transform.localScale;
+    private void Update()
+    {
 
-if(sp<0) {
-            scale.x = -1f * Mathf.Abs(scale.x);
-} else if (sp > 0)
-        {
-            scale.x = Mathf.Abs(scale.x);
-        }
-//for crouch
+        float vert = Input.GetAxisRaw("Vertical");
+        float horiz = Input.GetAxisRaw("Horizontal");
+
+        PlayerMove(horiz, vert);
+        PlayerMoveAnim(horiz, vert);
+
+        //for crouch
         if (Input.GetKey(KeyCode.LeftControl))
         {
             animator.SetBool("Crouch", true);
@@ -30,8 +34,39 @@ if(sp<0) {
             animator.SetBool("Crouch", false);
         }
 
+    }
+
+    private void PlayerMove(float horiz, float vert)
+    {
+        Vector3 position = transform.position;
+
+        position.x = position.x + (horiz * Speed * Time.deltaTime);
+        transform.position = position;
+
         //for jump
-        if (jump > 0)
+        if(vert >0)
+        {
+            rb.AddForce(new Vector2(0f, jump), ForceMode2D.Impulse);
+        }
+    }
+
+    private void PlayerMoveAnim(float horiz, float vert)
+    {
+        animator.SetFloat("Speed", Mathf.Abs(horiz));
+        Vector3 scale = transform.localScale;
+
+
+        if (horiz < 0)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        }
+        else if (horiz > 0)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+
+        //for Jump
+        if (vert > 0)
         {
             animator.SetBool("Jump", true);
         }
@@ -40,7 +75,6 @@ if(sp<0) {
             animator.SetBool("Jump", false);
         }
 
-
         transform.localScale = scale;
-}
+    }
 }
