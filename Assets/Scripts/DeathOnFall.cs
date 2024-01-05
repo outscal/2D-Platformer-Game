64c8970dab13d098ca93f9f1d6fd91d0664725a1
Reyansh <aayushsoni92@gmail.com>
2024-01-06 01:36:34 +0530

    Added Death After falling from the platform and going outside the screen downwards

---
+++ Assets/Scripts/DeathOnFall.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathOnFall : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.GetComponent<PlayerController>() != null)
+        {
+            Debug.Log("Died! Restarting Level");
+            SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
