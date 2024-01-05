090c05f22d579fed383d36a9ccabf02b64799968
Reyansh <aayushsoni92@gmail.com>
2024-01-06 01:24:17 +0530

    - Performed all the content tasks -Place / move the Level End game object to the level end position. -When the player collides with the empty game object run the code to load the next level.

---
+++ Assets/Scripts/LevelFinish.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class LevelFinish : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if(collider.gameObject.GetComponent<PlayerController>() != null)
+        {
+            Debug.Log("Level is Over");
+            SceneManager.LoadSceneAsync(1);
+        }
+    }
+}
