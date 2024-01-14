b938c1034c564722d2509c4b9422a19e2db5bba1
Reyansh <aayushsoni92@gmail.com>
2024-01-15 00:16:25 +0530

    Chapter 7 - Mini Assignment 1 - Lobby Screen

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void Quitter()
+    {
+        Application.Quit();
+    }
+}
