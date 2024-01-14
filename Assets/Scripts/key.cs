35d6086a1558b3f7bec80bf1eba4271330d0dfdb
Reyansh <aayushsoni92@gmail.com>
2024-01-14 16:36:52 +0530

    Chapter 5 - Assignment - Implementation of Animated Keys

--- Assets/Scripts/key.cs
+++ Assets/Scripts/key.cs
@@ -5,13 +5,25 @@ using TMPro;
 public class key : MonoBehaviour
 {
     [SerializeField] private PlayerController player;
+    [SerializeField] private Animator anim;
+
+    private void Start()
+    {
+        anim.SetBool("Collected", false);
+    }
     private void OnTriggerEnter2D(Collider2D col)
     {
         if(col.gameObject.tag == "Player")
         {
-
+            anim.SetBool("Collected", true);
             player.KeyPickUp();
-            Destroy(this.gameObject);
+            StartCoroutine(destroyer());
         }
+    }
+
+    IEnumerator destroyer()
+    {
+        yield return new WaitForSeconds(1);        
+        Destroy(this.gameObject);
     }
 }
