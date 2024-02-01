a9fcb78dfafbba8e9f3cb3512e94d70878611e46
Chetan Rautela <chetanrautela20@gmail.com>
2024-02-01 19:05:10 +0530

    jumping added , flipping sprite for reverse

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,16 +5,38 @@ using UnityEngine;
 public class NewBehaviourScript : MonoBehaviour
 {
     public Animator animator;
+    SpriteRenderer spriteRenderer;
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float speed = Input.GetAxisRaw("Horizontal");
-        animator.SetFloat("Speed", speed);
+        float horispeed = Input.GetAxisRaw("Horizontal");
+        float vertispeed = Input.GetAxisRaw("Vertical");
+        if (horispeed < 0)
+        {
+            spriteRenderer.flipX = true;
+            animator.SetBool("IsJumping", false);
+        }
+        if(horispeed > 0)
+        {
+            spriteRenderer.flipX = false; 
+            animator.SetBool("IsJumping", false);
+        }
+        animator.SetFloat("Speed", horispeed);
+        
+        if (vertispeed>0)
+        {
+            animator.SetBool("IsJumping", true);
+           
+        }
+        if(vertispeed < 0)
+        {
+            animator.SetBool("IsJumping", false);
+        }
     }
 }
