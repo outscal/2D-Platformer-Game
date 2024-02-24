5fff4ee0961fdefeb41ef58071fc605dc93eeb7c
Surajn7 <105142430+Surajn7@users.noreply.github.com>
2024-02-24 15:35:37 +0530

    Level Complete

--- Assets/Scripts/Player_Controller.cs
+++ Assets/Scripts/Player_Controller.cs
@@ -7,37 +7,43 @@ public class Player_Controller : MonoBehaviour
     float horizontal,vertical;
     private Animator Animator;
     public float speed;
+    public GameObject UI_object;
+    public float jumpForce;
+
+    private Rigidbody2D rgbody;
     // Start is called before the first frame update
     void Start()
     {
-        Animator   = GetComponent<Animator>();   
+        Animator   = GetComponent<Animator>();
+        rgbody = gameObject.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
-        vertical = Input.GetAxisRaw("Vertical");
 
         if (horizontal > 0)
-        {                      
+        {
+            Animator.SetBool("IsMoving", true);
             Vector3 Scale = transform.localScale;
             Scale.x = Mathf.Abs(Scale.x);
-            transform.localScale = Scale;
-            Animator.SetBool("IsMoving", true);
+            transform.localScale = Scale;           
             PlayerMovement(horizontal);            
         }
         else if(horizontal < 0)
-        {            
+        {
+            Animator.SetBool("IsMoving", true);
             Vector3 Scale = transform.localScale;
             Scale.x = -1f * Mathf.Abs(Scale.x);
-            transform.localScale = Scale;
-            Animator.SetBool("IsMoving", true);
+            transform.localScale = Scale;            
             PlayerMovement(horizontal);
         }
-        else if(Input.GetAxisRaw("Vertical")>0)
+        else if(Input.GetKey(KeyCode.Space))
         {
             Animator.SetBool("IsJumping", true);
+            rgbody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Force);
+
         }
         else if(Input.GetKey(KeyCode.C))
         {            
@@ -56,5 +62,13 @@ public class Player_Controller : MonoBehaviour
         Vector3 Position = transform.position;
         Position.x = Position.x + horizontal * speed * Time.deltaTime;
         transform.position = Position;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag == "Finish")
+        {
+            UI_object.SetActive(true);
+        }
     }
 }
