7d0bcd50f98a9e5e323b86e27e0105fe2900d8a9
Aryan khera <105739020+AryAnKhera23@users.noreply.github.com>
2024-03-04 04:34:25 +0530

    Run animation

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] Animator animator;
+   
+    void Update()
+    {
+        float speed = Input.GetAxisRaw("Horizontal");
+        animator.SetFloat("Speed", Mathf.Abs(speed));
+
+        Vector3 scale = gameObject.transform.localScale;
+        if (speed < 0f)
+        {
+            scale.x = -1f * Mathf.Abs(scale.x);
+        } else if(speed > 0f)
+        {
+            scale.x = Mathf.Abs(scale.x);
+        }
+        gameObject.transform.localScale = scale;
+    }
+}
